60fa08d17f8c260ed80b11289c50cd50c8742b71
Emmanuel Briot <briot@adacore.com>
1998-07-02 04:46:44 +0000

    new widget

--- src/gtk-box.adb
+++ src/gtk-box.adb
@@ -1,20 +1,150 @@
 
 package body Gtk.Box is
 
-   procedure Pack_Start (In_Box  : in Box'Class;
-                         Widget  : in Gtk.Widget.Widget'Class;
-                         Expand  : in Boolean;
-                         Fill    : in Boolean;
-                         Padding : in GInt) is
+   ----------------
+   -- Pack_Start --
+   ----------------
+
+   procedure Pack_Start
+     (In_Box  : in Box'Class;
+      Child   : in Gtk.Widget.Widget'Class;
+      Expand  : in Boolean := True;
+      Fill    : in Boolean := True;
+      Padding : in GInt    := 0)
+   is
       procedure Internal (In_Box  : System.Address;
-                          Widget  : System.Address;
+                          Child   : System.Address;
                           Expand  : GInt;
                           Fill    : GInt;
                           Padding : GInt);
       pragma Import (C, Internal, "gtk_box_pack_start");
    begin
-      Internal (Get_Object (In_Box), Get_Object (Widget),
+      Internal (Get_Object (In_Box), Get_Object (Child),
                 Boolean'Pos (Expand), Boolean'Pos (Fill), Padding);
    end Pack_Start;
+
+   --------------
+   -- Pack_End --
+   --------------
+
+   procedure Pack_End
+     (In_Box  : in Box'Class;
+      Child   : in Gtk.Widget.Widget'Class;
+      Expand  : in Boolean := True;
+      Fill    : in Boolean := True;
+      Padding : in GInt    := 0)
+   is
+      procedure Internal (In_Box  : in System.Address;
+                          Child   : in System.Address;
+                          Expand  : in GInt;
+                          Fill    : in GInt;
+                          Padding : in GInt);
+      pragma Import (C, Internal, "gtk_box_pack_end");
+   begin
+      Internal (Get_Object (In_Box), Get_Object (Child),
+                Boolean'Pos (Expand), Boolean'Pos (Fill), Padding);
+   end Pack_End;
+
+   ---------------------
+   -- Set_Homogeneous --
+   ---------------------
+
+   procedure Set_Homogeneous (In_Box      : in Box'Class;
+                              Homogeneous : in Boolean)
+   is
+      procedure Internal (In_Box      : in System.Address;
+                          Homogeneous : in GInt);
+      pragma Import (C, Internal, "gtk_box_set_homogeneous");
+   begin
+      Internal (Get_Object (In_Box), Boolean'Pos (Homogeneous));
+   end Set_Homogeneous;
+
+   -----------------
+   -- Set_Spacing --
+   -----------------
+
+   procedure Set_Spacing (In_Box  : in Box'Class;
+                          Spacing : in GInt)
+   is
+      procedure Internal (In_Box  : in System.Address;
+                          Spacing : in GInt);
+      pragma Import (C, Internal, "gtk_box_set_spacing");
+   begin
+      Internal (Get_Object (In_Box), Spacing);
+   end Set_Spacing;
+
+   -------------------
+   -- Reorder_Child --
+   -------------------
+
+   procedure Reorder_Child
+     (In_Box : in Box'Class;
+      Child  : in Gtk.Widget.Widget'Class;
+      Pos    : in GUint)
+   is
+      procedure Internal (In_Box : in System.Address;
+                          Child  : in System.Address;
+                          Pos    : in GUint);
+      pragma Import (C, Internal, "gtk_box_reorder_child");
+   begin
+      Internal (Get_Object (In_Box), Get_Object (Child), Pos);
+   end Reorder_Child;
+
+   -------------------------
+   -- Query_Child_Packing --
+   -------------------------
+
+   procedure Query_Child_Packing
+     (In_Box   : in Box'Class;
+      Child    : in Gtk.Widget.Widget'Class;
+      Expand   : out Boolean;
+      Fill     : out Boolean;
+      Padding  : out GInt;
+      PackType : out Pack_Type)
+   is
+      procedure Internal (In_Box   : in System.Address;
+                          Child    : in System.Address;
+                          Expand   : out GInt;
+                          Fill     : out GInt;
+                          Padding  : out GInt;
+                          PackType : out GInt);
+      pragma Import (C, Internal, "gtk_box_query_child_packing");
+
+      Expand_Ptr  : GInt;
+      Fill_Ptr    : GInt;
+      PackT_Ptr   : GInt;
+   begin
+      Internal (Get_Object (In_Box), Get_Object (Child),
+                Expand_Ptr, Fill_Ptr, Padding, PackT_Ptr);
+      Expand   := Expand_Ptr /= 0;
+      Fill     := Fill_Ptr /= 0;
+      PackType := Pack_Type'Val (PackT_Ptr);
+   end Query_Child_Packing;
+
+   -----------------------
+   -- Set_Child_Packing --
+   -----------------------
+
+   procedure Set_Child_Packing
+     (In_Box    : in Box'Class;
+      Child     : in Gtk.Widget.Widget'Class;
+      Expand    : in Boolean;
+      Fill      : in Boolean;
+      Padding   : in GInt;
+      PackType  : in Pack_Type)
+   is
+      procedure Internal (In_Box   : in System.Address;
+                          Child    : in System.Address;
+                          Expand   : in GInt;
+                          Fill     : in GInt;
+                          Padding  : in GInt;
+                          PackType : in GInt);
+      pragma Import (C, Internal, "gtk_box_set_child_packing");
+   begin
+      Internal (Get_Object (In_Box), Get_Object (Child),
+                Boolean'Pos (Expand), Boolean'Pos (Fill), Padding,
+                Pack_Type'Pos (PackType));
+   end Set_Child_Packing;
+
 
 end Gtk.Box;
--- src/gtk-box.ads
+++ src/gtk-box.ads
@@ -6,13 +6,63 @@ package Gtk.Box is
 
    type Box is new Gtk.Container.Container with private;
 
-   procedure Pack_Start (In_Box  : in Box'Class;
-                         Widget  : in Gtk.Widget.Widget'Class;
-                         Expand  : in Boolean;
-                         Fill    : in Boolean;
-                         Padding : in GInt);
+   type Pack_Type is (Pack_Start, Pack_End);
+   --  mapping: Pack_Type gtkenums.h GtkPackType
 
+   procedure Pack_Start
+     (In_Box  : in Box'Class;
+      Child   : in Gtk.Widget.Widget'Class;
+      Expand  : in Boolean := True;
+      Fill    : in Boolean := True;
+      Padding : in GInt    := 0);
    --  mapping: Pack_Start gtkbox.h gtk_box_pack_start
+   --  mapping: Pack_Start gtkbox.h gtk_box_pack_start_defaults
+
+   procedure Pack_End
+     (In_Box  : in Box'Class;
+      Child   : in Gtk.Widget.Widget'Class;
+      Expand  : in Boolean := True;
+      Fill    : in Boolean := True;
+      Padding : in GInt    := 0);
+   --  mapping: Pack_End gtkbox.h gtk_box_pack_end
+   --  mapping: Pack_End gtkbox.h gtk_box_pack_end_defaults
+
+   procedure Set_Homogeneous
+     (In_Box      : in Box'Class;
+      Homogeneous : in Boolean);
+   --  mapping: Set_Homogeneous gtkbox.h gtk_box_set_homogeneous
+
+   procedure Set_Spacing
+     (In_Box  : in Box'Class;
+      Spacing : in GInt);
+   --  mapping: Set_Spacing gtkbox.h gtk_box_set_spacing
+
+   procedure Reorder_Child
+     (In_Box : in Box'Class;
+      Child  : in Gtk.Widget.Widget'Class;
+      Pos    : in GUint);
+   --  mapping: Box_Reorder_Child gtkbox.h gtk_box_reorder_child
+
+   procedure Query_Child_Packing
+     (In_Box   : in Box'Class;
+      Child    : in Gtk.Widget.Widget'Class;
+      Expand   : out Boolean;
+      Fill     : out Boolean;
+      Padding  : out GInt;
+      PackType : out Pack_Type);
+   --  mapping: Query_Child_Packing gtkbox.h gtk_box_query_child_packing
+
+   procedure Set_Child_Packing
+     (In_Box    : in Box'Class;
+      Child     : in Gtk.Widget.Widget'Class;
+      Expand    : in Boolean;
+      Fill      : in Boolean;
+      Padding   : in GInt;
+      PackType  : in Pack_Type);
+   --  mapping: Set_Child_Packing gtkbox.h gtk_box_set_child_packing
+
+
+   --  mapping: NOT_IMPLEMENTED gtkbox.h gtk_box_get_type
 
 private
 
