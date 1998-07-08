9c03e08fc0c88e7ca294b959c45d79f63d86b8f7
Emmanuel Briot <briot@adacore.com>
1998-07-08 14:45:50 +0000

    Modified to use Status Bar and Color_Selection_Dialog

--- src/test.adb
+++ src/test.adb
@@ -6,15 +6,29 @@ with Gtk.Signal; use Gtk.Signal;
 with Gtk.Button; use Gtk.Button;
 with Gtk.Box;    use Gtk.Box;
 with Gtk.Hbox;   use Gtk.Hbox;
+with Gtk.Vbox;   use Gtk.Vbox;
+with Gtk.Combo;  use Gtk.Combo;
 with Ada.Text_IO;
+with Gtk.Status_Bar; use Gtk.Status_Bar;
+with Gtk.Color_Selection; use Gtk.Color_Selection;
+with Gtk.Color_Selection_Dialog; use Gtk.Color_Selection_Dialog;
 
 package body Test is
 
    subtype String7 is String (1 .. 7);
+   package String_Cb is new Callback (String7, Gtk.Button.Gtk_Button);
+   package Void_Cb   is new Void_Callback (Gtk.Window.Gtk_Window);
+   package Void_Cb_Button is new Void_Callback (Gtk.Button.Gtk_Button);
+   package ColSel_Cb is new Object_Callback (Gtk_Color_Selection_Dialog);
+   use Gtk.Combo.String_List;
+
+   Status   : Gtk_Status_Bar;
 
    procedure Hello (Widget : in out Gtk.Button.Gtk_Button;
                     S      : in     String7);
    procedure Destroy (Object : in out Gtk.Window.Gtk_Window);
+   procedure Launch_Dialog (Object : in out Gtk.Button.Gtk_Button);
+   procedure Print_Color (Dialog : in out Gtk_Color_Selection_Dialog'Class);
 
    -----------
    -- Hello --
@@ -22,9 +36,43 @@ package body Test is
 
    procedure Hello (Widget : in out Gtk.Button.Gtk_Button;
                     S      : in     String7) is
+      Message : Message_Id;
    begin
       Ada.Text_IO.Put_Line ("Hello World  => String was=" & S);
+      Message := Push (Status, 1, S);
    end Hello;
+
+   -----------------
+   -- Print_Color --
+   -----------------
+
+   procedure Print_Color (Dialog : in out Gtk_Color_Selection_Dialog'Class) is
+      Color : Color_Array;
+   begin
+      Get_Color (Get_Colorsel (Dialog), Color);
+      for I in Red .. Opacity loop
+         Ada.Text_IO.Put_Line (Color_Index'Image (I)
+                               & " => "
+                               & Gdouble'Image (Color (I)));
+      end loop;
+   end Print_Color;
+
+   -------------------
+   -- Launch_Dialog --
+   -------------------
+
+   procedure Launch_Dialog (Object : in out Gtk.Button.Gtk_Button) is
+      Dialog : aliased Gtk_Color_Selection_Dialog;
+      Id     : ColSel_Cb.Callback_Id;
+   begin
+      Gtk_New (Dialog);
+      Hide (Get_Help_Button (Dialog));
+      Show (Dialog);
+      Id := ColSel_Cb.Connect (Get_OK_Button (Dialog), "clicked",
+                               Print_Color'Access, Dialog);
+--       Id := ColSel_Cb.Connect (Get_Cancel_Button (Dialog), "clicked",
+--                                Destroy'Access, Dialog);
+   end Launch_Dialog;
 
    -------------
    -- Destroy --
@@ -42,14 +90,18 @@ package body Test is
 
    procedure Main is
 
-      package String_Cb is new Callback (String7, Gtk.Button.Gtk_Button);
-      package Void_Cb   is new Void_Callback (Gtk.Window.Gtk_Window);
+      function CS (S : String) return Gtk.Combo.String_Access
+        renames Gtk.Combo.To_String_Access;
 
       A_Window : aliased Gtk.Window.Gtk_Window;
       A_Button : Gtk.Button.Gtk_Button;
-      A_Box    : Gtk.Hbox.Gtk_Hbox;
+      A_Box    : Gtk_Hbox;
+      V_Box    : Gtk_Vbox;
       Void_Id  : Void_Cb.Callback_Id;
+      Void_B_Id : Void_Cb_Button.Callback_Id;
       Str_Id   : String_Cb.Callback_Id;
+      Combo    : Gtk_Combo;
+      List     : Glist;
 
    begin
       Init;
@@ -62,13 +114,16 @@ package body Test is
       Void_Id := Void_Cb.Connect (A_Window, "destroy", Destroy'Access);
 
       --  Create the box to store the buttons
+      Gtk_New (V_Box, False, 0);
+      Add (A_Window, V_Box);
+
       Gtk_New (A_Box, False, 0);
-      Add (A_Window, A_Box);
+      Pack_Start (V_Box, A_Box);
 
       --  Create the first button
       Gtk_New (A_Button, Label => "Button1");
       Str_Id := String_Cb.Connect (A_Button, "clicked",
-                                  Hello'Access, "Button1");
+                                   Hello'Access, "Button1");
       Pack_Start (A_Box, A_Button, True, True, 0);
       Show (A_Button);
 
@@ -79,8 +134,38 @@ package body Test is
       Pack_Start (A_Box, A_Button, True, True, 0);
       Show (A_Button);
 
+      --  Combo Box
+      Append (List, CS ("item0"));
+      Append (List, CS ("item1 item1"));
+      Append (List, CS ("item2 item2 item2"));
+      Append (List, CS ("item3 item3 item3 item3"));
+      Append (List, CS ("item4 item4 item4 item4 item4"));
+      Append (List, CS ("item5 item5 item5 item5 item5 item5"));
+      Append (List, CS ("item6 item6 item6 item6 item6"));
+      Append (List, CS ("item7 item7 item7 item7"));
+      Append (List, CS ("item8 item8 item8"));
+      Append (List, CS ("item9 item9"));
+
+      Gtk_New (Combo);
+      Set_Popdown_Strings (Combo, List);
+      Pack_Start (V_Box, Combo, True, True, 5);
+      Show (Combo);
+
+      --  Status Bar
+      Gtk_New (Status);
+      Pack_Start (V_Box, Status, True, True, 10);
+      Show (Status);
+
+      --  Launch dialog
+      Gtk_New (A_Button, Label => "Color Selection");
+      Void_B_Id := Void_Cb_Button.Connect (A_Button, "clicked",
+                                           Launch_Dialog'Access);
+      Pack_Start (V_Box, A_Button, True, True, 5);
+      Show (A_Button);
+
       --  Show the box
       Show (A_Box);
+      Show (V_Box);
 
       Show (A_Window);
 
