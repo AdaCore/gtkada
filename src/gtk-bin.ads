45efb5c2abc7a838fd7aafa25e0208a4de6327aa
Joël Brobecker <brobecke@adacore.com>
1998-07-08 15:55:44 +0000

    Added the --  mapping: comments. A few cosmetic arrangements.

--- src/gtk-bin.ads
+++ src/gtk-bin.ads
@@ -1,12 +1,13 @@
-
 with Gtk.Container;
 
 package Gtk.Bin is
 
-   type Gtk_Bin is new Gtk.Container.Gtk_Container with private;
+   type Gtk_Bin is new Container.Gtk_Container with private;
 
 private
 
-   type Gtk_Bin is new Gtk.Container.Gtk_Container with null record;
+   type Gtk_Bin is new Container.Gtk_Container with null record;
+
+   --  mapping: USE_OBJECT_ORIENTED gtkbin.h gtk_bin_get_type
 
 end Gtk.Bin;
