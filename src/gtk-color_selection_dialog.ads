dbacb6b62222061b10ac1707f0cafa17f7d395f5
Emmanuel Briot <briot@adacore.com>
1998-07-10 05:34:29 +0000

    inherit from Gtk_Window instead of Gtk_Vbox

--- src/gtk-color_selection_dialog.ads
+++ src/gtk-color_selection_dialog.ads
@@ -1,12 +1,12 @@
 
-with Gtk.Vbox;
+with Gtk.Window;
 with Gtk.Button;
 with Gtk.Color_Selection;
 
 package Gtk.Color_Selection_Dialog is
 
    type Gtk_Color_Selection_Dialog is new
-     Gtk.Vbox.Gtk_Vbox with private;
+     Gtk.Window.Gtk_Window with private;
 
    procedure Gtk_New (Widget : out Gtk_Color_Selection_Dialog);
    --  mapping: Gtk_New gtkcolorsel.h gtk_color_selection_dialog_new
@@ -30,6 +30,6 @@ package Gtk.Color_Selection_Dialog is
 private
 
    type Gtk_Color_Selection_Dialog is new
-     Gtk.Vbox.Gtk_Vbox with null record;
+     Gtk.Window.Gtk_Window with null record;
 
 end Gtk.Color_Selection_Dialog;
