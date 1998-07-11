43594ab63cf4ce34e1716f570888fd55ab6a174e
Joël Brobecker <brobecke@adacore.com>
1998-07-11 12:00:49 +0000

    Moved the type Gtk_Object from Gtk to Gtk.Object.

---
+++ src/gtk-object.adb
@@ -0,0 +1,25 @@
+with Gtk.Signal;
+
+package body Gtk.Object is
+
+
+   -----------------
+   -- Destroy --
+   -----------------
+
+   procedure Destroy (Object : in Gtk_Object'Class)
+   is
+      procedure Internal  (Object : in System.Address);
+      pragma Import (C, Internal, "gtk_object_destroy");
+   begin
+      --  When the widget is destroyed, the callbacks are automatically
+      --  destroyed too.
+      --  In this binding, we have to make sure the memory allocated for
+      --  the data is freed. The simple way is to call
+      Gtk.Signal.Handlers_Destroy (Object);
+
+      Internal (Get_Object (Object));
+   end Destroy;
+
+
+end Gtk.Object;
---
+++ src/gtk-object.ads
@@ -0,0 +1,49 @@
+package Gtk.Object is
+
+   type Gtk_Object is new Root_Type with private;
+
+   procedure Destroy (Object : in Gtk_Object'Class);
+   --  mapping: Destroy gtkobject.h gtk_object_destroy
+
+private
+
+   type Gtk_Object is new Root_Type with null record;
+
+   --  Functions which are not implemented because they are probably not needed
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_add_arg_type
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_check_cast
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_check_class_cast
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_class_add_signals
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_class_add_user_signal
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_get_arg_type
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_get_type
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_getv
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_new
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_newv
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_query_args
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_ref
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_set
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_setv
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_sink
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_unref
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_weakref
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_object_weakunref
+   --  mapping: NOT_IMPLEMENTED gtkobject.h gtk_trace_referencing
+
+   --  Functions useless because of the object oriented binding
+   --  mapping: USE_OBJECT_ORIENTED gtkobject.h gtk_object_get_data
+   --  mapping: USE_OBJECT_ORIENTED gtkobject.h gtk_object_get_user_data
+   --  mapping: USE_OBJECT_ORIENTED gtkobject.h gtk_object_remove_data
+   --  mapping: USE_OBJECT_ORIENTED gtkobject.h gtk_object_set_data
+   --  mapping: USE_OBJECT_ORIENTED gtkobject.h gtk_object_set_data_full
+   --  mapping: USE_OBJECT_ORIENTED gtkobject.h gtk_object_set_user_data
+
+   --  Functions *internal* to gtk, not mapped
+   --  mapping: INTERNAL gtkobject.h gtk_object_data_force_id
+   --  mapping: INTERNAL gtkobject.h gtk_object_data_try_key
+   --  mapping: INTERNAL gtkobject.h gtk_object_get_data_by_id
+   --  mapping: INTERNAL gtkobject.h gtk_object_remove_data_by_id
+   --  mapping: INTERNAL gtkobject.h gtk_object_set_data_by_id
+   --  mapping: INTERNAL gtkobject.h gtk_object_set_data_by_id_full
+
+end Gtk.Object;
