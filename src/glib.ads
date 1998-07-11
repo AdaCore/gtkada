145daf348551b3ba5ec3024b4544af4f28f683dc
Joël Brobecker <brobecke@adacore.com>
1998-07-11 12:09:16 +0000

    Moved the gtk "basic" types here for package Gtk to be coherent with the gtk architecture.

--- src/glib.ads
+++ src/glib.ads
@@ -1,133 +1,44 @@
+with Interfaces.C;
+with System;
 
 package Glib is
 
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_free
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_free_1
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_append
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_prepend
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_insert
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_concat
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_remove
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_remove_link
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_reverse
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_nth
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_find
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_position
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_index
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_last
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_length
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_foreach
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_nth_data
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_next(list)
-   --  mapping: NOT_IMPLEMENTED glib.h g_list_allocator_free
-   --  mapping: NOT_IMPLEMENTED glib.h g_slist_set_allocator
-   --  mapping: NOT_IMPLEMENTED glib.h g_list_set_allocator
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_destroy
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_insert
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_remove
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_lookup
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_freeze
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_thaw
-   --  mapping: NOT_IMPLEMENTED glib.h g_hash_table_foreach
-   --  mapping: NOT_IMPLEMENTED glib.h g_cache_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_cache_destroy
-   --  mapping: NOT_IMPLEMENTED glib.h g_cache_insert
-   --  mapping: NOT_IMPLEMENTED glib.h g_cache_remove
-   --  mapping: NOT_IMPLEMENTED glib.h g_cache_key_foreach
-   --  mapping: NOT_IMPLEMENTED glib.h g_cache_value_foreach
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_destroy
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_insert
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_remove
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_lookup
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_traverse
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_search
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_height
-   --  mapping: NOT_IMPLEMENTED glib.h g_tree_nnodes
-   --  mapping: NOT_IMPLEMENTED glib.h g_malloc(size)
-   --  mapping: NOT_IMPLEMENTED glib.h g_malloc0
-   --  mapping: NOT_IMPLEMENTED glib.h g_realloc
-   --  mapping: NOT_IMPLEMENTED glib.h g_free
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_profile
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_check
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_destroy
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_alloc
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_free
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_clean
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_reset
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_print
-   --  mapping: NOT_IMPLEMENTED glib.h g_mem_chunk_info
-   --  mapping: NOT_IMPLEMENTED glib.h g_blow_chunks
-   --  mapping: NOT_IMPLEMENTED glib.h g_timer_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_timer_destroy
-   --  mapping: NOT_IMPLEMENTED glib.h g_timer_start
-   --  mapping: NOT_IMPLEMENTED glib.h g_timer_stop
-   --  mapping: NOT_IMPLEMENTED glib.h g_timer_reset
-   --  mapping: NOT_IMPLEMENTED glib.h g_timer_elapsed
-   --  mapping: NOT_IMPLEMENTED glib.h g_error
-   --  mapping: NOT_IMPLEMENTED glib.h g_warning
-   --  mapping: NOT_IMPLEMENTED glib.h g_message
-   --  mapping: NOT_IMPLEMENTED glib.h g_print
-   --  mapping: NOT_IMPLEMENTED glib.h g_strdelimit
-   --  mapping: NOT_IMPLEMENTED glib.h g_strdup
-   --  mapping: NOT_IMPLEMENTED glib.h g_strconcat
-   --  mapping: NOT_IMPLEMENTED glib.h g_strtod
-   --  mapping: NOT_IMPLEMENTED glib.h g_strerror
-   --  mapping: NOT_IMPLEMENTED glib.h g_strsignal
-   --  mapping: NOT_IMPLEMENTED glib.h g_strcasecmp
-   --  mapping: NOT_IMPLEMENTED glib.h g_strdown
-   --  mapping: NOT_IMPLEMENTED glib.h g_strup
-   --  mapping: NOT_IMPLEMENTED glib.h g_parse_debug_string
-   --  mapping: NOT_IMPLEMENTED glib.h g_snprintf
-   --  mapping: NOT_IMPLEMENTED glib.h g_set_error_handler
-   --  mapping: NOT_IMPLEMENTED glib.h g_set_warning_handler
-   --  mapping: NOT_IMPLEMENTED glib.h g_set_message_handler
-   --  mapping: NOT_IMPLEMENTED glib.h g_set_print_handler
-   --  mapping: NOT_IMPLEMENTED glib.h g_debug
-   --  mapping: NOT_IMPLEMENTED glib.h g_attach_process
-   --  mapping: NOT_IMPLEMENTED glib.h g_stack_trace
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_chunk_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_chunk_free
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_chunk_insert
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_chunk_insert_const
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_free
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_assign
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_truncate
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_append
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_append_c
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_prepend
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_prepend_c
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_insert
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_insert_c
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_erase
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_down
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_up
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_sprintf
-   --  mapping: NOT_IMPLEMENTED glib.h g_string_sprintfa
-   --  mapping: NOT_IMPLEMENTED glib.h g_array_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_array_free
-   --  mapping: NOT_IMPLEMENTED glib.h g_rarray_append
-   --  mapping: NOT_IMPLEMENTED glib.h g_rarray_prepend
-   --  mapping: NOT_IMPLEMENTED glib.h g_rarray_truncate
-   --  mapping: NOT_IMPLEMENTED glib.h g_str_equal
-   --  mapping: NOT_IMPLEMENTED glib.h g_str_hash
-   --  mapping: NOT_IMPLEMENTED glib.h g_direct_hash
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_new
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_destroy
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_input_file
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_input_text
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_get_next_token
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_peek_next_token
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_cur_token
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_cur_value
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_cur_line
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_cur_position
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_eof
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_add_symbol
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_lookup_symbol
-   --  mapping: NOT_IMPLEMENTED glib.h g_scanner_remove_symbol
+   package C renames Interfaces.C;
+   use type C.Int;
+   use type C.Unsigned;
+
+   ----------------------------------------
+   --  The basic types  defined by glib  --
+   ----------------------------------------
+
+   type Gshort is new C.Short;
+   type Glong  is new C.Long;
+   type Gint   is new C.Int;
+
+   type Gushort is new C.Unsigned_Short;
+   type Gulong  is new C.Unsigned_Long;
+   type Guint   is new C.Unsigned;
+
+   type Gfloat  is new C.C_Float;
+   type Gdouble is new C.Double;
+
+   subtype Gint32 is Gint range -(2 ** 16) .. (2 ** 16 - 1);
+
+   subtype Guint8  is Guint range Guint'First .. (2 ** 8 - 1);
+   subtype Guint32 is Guint range Guint'First .. (2 ** 32 - 1);
+
+   -------------------------------------------------
+   --  The more evoluated types defined by glib   --
+   -------------------------------------------------
+   --
+   --  Note : The services related to those types should be implemented in
+   --         different packages.  Ex: Services related to Gdk_Window should
+   --         be implemented in the package Gdk.Window just like it has been
+   --         implemented in gtkwindow.c in the C version.
+   --
+   --         Note also that some of those type can not be declared here since
+   --         they are "generic" types.
+
+
 
 end Glib;
