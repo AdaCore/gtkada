e705be767bce9c6c6654d6e1d9e38de48a3d8b27
Joël Brobecker <brobecke@adacore.com>
1998-07-11 12:12:55 +0000

    the Gtk_Object type is now located in package Gtk.Object. Made the changes accordingly.

--- src/gtk-data.ads
+++ src/gtk-data.ads
@@ -1,11 +1,12 @@
+with Gtk.Object;
 
 package Gtk.Data is
 
-   type Gtk_Data is new Gtk.Gtk_Object with private;
+   type Gtk_Data is new Object.Gtk_Object with private;
 
 private
 
-   type Gtk_Data is new Gtk.Gtk_Object with null record;
+   type Gtk_Data is new Object.Gtk_Object with null record;
 
    --  mapping: USE_OBJECT_ORIENTED gtkdata.h gtk_data_get_type
 
