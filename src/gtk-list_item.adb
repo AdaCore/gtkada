42e9064ca9c4f75f34f44db94ad441015c00fd04
Emmanuel Briot <briot@adacore.com>
1998-07-13 01:09:23 +0000

    minor modifications so that -gnatg does not complain

--- src/gtk-list_item.adb
+++ src/gtk-list_item.adb
@@ -1,48 +1,51 @@
 
+
 package body Gtk.List_Item is
 
-   ----------------
-   --  Deselect  --
-   ----------------
+   --------------
+   -- Deselect --
+   --------------
 
-   procedure Deselect (List_Item : in out Gtk_List_Item'Class) is
+   procedure Deselect (List_Item : in Gtk_List_Item'Class)
+   is
       procedure Internal (List_Item : in System.Address);
       pragma Import (C, Internal, "gtk_list_item_deselect");
    begin
       Internal (Get_Object (List_Item));
    end Deselect;
 
+   -------------
+   -- Gtk_New --
+   -------------
 
-   ---------------
-   --  Gtk_New  --
-   ---------------
+   procedure Gtk_New (Widget : out Gtk_List_Item;
+                      Label  : in String)
+   is
+      function Internal (Label  : in String)
+                         return      System.Address;
+      pragma Import (C, Internal, "gtk_list_item_new_with_label");
+   begin
+      Set_Object (Widget, Internal (Label & Ascii.NUL));
+   end Gtk_New;
 
-   procedure Gtk_New (List_Item : out Gtk_List_Item) is
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Widget : out Gtk_List_Item)
+   is
       function Internal return System.Address;
       pragma Import (C, Internal, "gtk_list_item_new");
    begin
-      Set_Object (List_Item, Internal);
+      Set_Object (Widget, Internal);
    end Gtk_New;
 
+   ----------------
+   -- Gtk_Select --
+   ----------------
 
-   ---------------
-   --  Gtk_New  --
-   ---------------
-
-   procedure Gtk_New (List_Item : out Gtk_List_Item;
-                      Label     : in String) is
-      function Internal (Label : in String) return System.Address;
-      pragma Import (C, Internal, "gtk_list_item_new_with_label");
-   begin
-      Set_Object (List_Item, Internal (Label & ASCII.NUL));
-   end Gtk_New;
-
-
-   ------------------
-   --  Gtk_Select  --
-   ------------------
-
-   procedure Gtk_Select (List_Item : in out Gtk_List_Item'Class) is
+   procedure Gtk_Select (List_Item : in Gtk_List_Item'Class)
+   is
       procedure Internal (List_Item : in System.Address);
       pragma Import (C, Internal, "gtk_list_item_select");
    begin
--- src/gtk-list_item.ads
+++ src/gtk-list_item.ads
@@ -1,26 +1,22 @@
+
 with Gtk.Item;
 
 package Gtk.List_Item is
 
-   type Gtk_List_Item is new Item.Gtk_Item with private;
+   type Gtk_List_Item is new Gtk.Item.Gtk_Item with private;
 
-   procedure Gtk_New (List_Item : out Gtk_List_Item);
-   --  mapping: Gtk_New gtklistitem.h gtk_list_item_new
-
-   procedure Gtk_New (List_Item : out Gtk_List_Item;
-                      Label     : in String);
-   --  mapping: Gtk_New gtklistitem.h gtk_list_item_new_with_label
-
-   procedure Gtk_Select (List_Item : in out Gtk_List_Item'Class);
-   --  mapping: Gtk_Select gtklistitem.h gtk_list_item_select
-
-   procedure Deselect (List_Item : in out Gtk_List_Item'Class);
-   --  mapping: Deselect gtklistitem.h gtk_list_item_deselect
+   procedure Deselect (List_Item : in Gtk_List_Item'Class);
+   procedure Gtk_New (Widget : out Gtk_List_Item;
+                      Label  : in String);
+   procedure Gtk_New (Widget : out Gtk_List_Item);
+   procedure Gtk_Select (List_Item : in Gtk_List_Item'Class);
 
 private
+   type Gtk_List_Item is new Gtk.Item.Gtk_Item with null record;
 
-   type Gtk_List_Item is new Item.Gtk_Item with null record;
-
-   --  mapping: USE_OBJECT_ORIENTED gtklistitem.h gtk_list_item_get_type
-
+   --  mapping: Deselect gtklistitem.h gtk_list_item_deselect
+   --  mapping: NOT_IMPLEMENTED gtklistitem.h gtk_list_item_get_type
+   --  mapping: Gtk_New gtklistitem.h gtk_list_item_new_with_label
+   --  mapping: Gtk_New gtklistitem.h gtk_list_item_new
+   --  mapping: Gtk_Select gtklistitem.h gtk_list_item_select
 end Gtk.List_Item;
