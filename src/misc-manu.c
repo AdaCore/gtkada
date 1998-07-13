b708fb904b729b9e8186bdc87c2cc313d5975ef0
Emmanuel Briot <briot@adacore.com>
1998-07-13 01:11:55 +0000

    Lots of new functions add for new widgets

--- src/misc-manu.c
+++ src/misc-manu.c
@@ -78,3 +78,117 @@ ada_gamma_curve_get_gamma (GtkGammaCurve* widget)
 {
   return widget->gamma;
 }
+
+/******************************************
+ ** Functions for Alignment
+ ******************************************/
+
+gfloat
+ada_alignment_get_xalign (GtkAlignment* widget)
+{
+   return widget->xalign;
+}
+
+gfloat
+ada_alignment_get_xscale (GtkAlignment* widget)
+{
+   return widget->xscale;
+}
+
+gfloat
+ada_alignment_get_yalign (GtkAlignment* widget)
+{
+   return widget->yalign;
+}
+
+gfloat
+ada_alignment_get_yscale (GtkAlignment* widget)
+{
+   return widget->yscale;
+}
+
+
+/******************************************
+ ** Functions for Ruler
+ ******************************************/
+
+gfloat
+ada_ruler_get_lower (GtkRuler* widget)
+{
+   return widget->lower;
+}
+
+gfloat
+ada_ruler_get_max_size (GtkRuler* widget)
+{
+   return widget->max_size;
+}
+
+gfloat
+ada_ruler_get_position (GtkRuler* widget)
+{
+   return widget->position;
+}
+
+gfloat
+ada_ruler_get_upper (GtkRuler* widget)
+{
+   return widget->upper;
+}
+
+/******************************************
+ ** Functions for Editable
+ ******************************************/
+
+gchar*
+ada_editable_get_clipboard_text (GtkEditable* widget)
+{
+   return widget->clipboard_text;
+}
+
+guint
+ada_editable_get_current_pos (GtkEditable* widget)
+{
+   return widget->current_pos;
+}
+
+guint
+ada_editable_get_has_selection (GtkEditable* widget)
+{
+   return widget->has_selection;
+}
+
+guint
+ada_editable_get_selection_end_pos (GtkEditable* widget)
+{
+   return widget->selection_end_pos;
+}
+
+guint
+ada_editable_get_selection_start_pos (GtkEditable* widget)
+{
+   return widget->selection_start_pos;
+}
+
+/******************************************
+ ** Functions for Aspect_Frame
+ ******************************************/
+
+gfloat
+ada_aspect_frame_get_ratio (GtkAspectFrame* widget)
+{
+   return widget->ratio;
+}
+
+gfloat
+ada_aspect_frame_get_xalign (GtkAspectFrame* widget)
+{
+   return widget->xalign;
+}
+
+gfloat
+ada_aspect_frame_get_yalign (GtkAspectFrame* widget)
+{
+   return widget->yalign;
+}
+
