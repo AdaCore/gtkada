463acfb64932d9c29fd9f0d7b8e530ebaa266b20
Emmanuel Briot <briot@adacore.com>
1998-07-13 01:00:39 +0000

    Modified the way we search for names (so that -gnatg does not complain)

--- src/create_test_link.pl
+++ src/create_test_link.pl
@@ -4,12 +4,14 @@ foreach (<g*.ads>)
 {
     if (/g(lib|dk|tk)/)
     {
-	s/\.ads$//;                # remove the .ads extension
-	s/-/\./g;                  # change the dashes into dots
-	s/(\w+)/\u$1/g;            # Uppercase the first letters of all words.
-	s/_(\w)/_\u$1/g;           # Uppercase the first letters after _
-
-	print "with " . $_ . ";\n";    # output the result
+       open (FILE, $_);
+       while ($line = <FILE>)
+       {
+           last if ($line =~ /package\s+(\S+)/);
+       }
+       close (FILE);
+       $line =~ /package\s+(\S+)/;
+       print "with $1;\n";
     }
 }
 
