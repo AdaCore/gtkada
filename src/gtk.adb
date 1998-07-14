1d127fed82215d3a0f90f36685e8d31bbb15a097
Joël Brobecker <brobecke@adacore.com>
1998-07-14 21:44:06 +0000

    Moved To_Boolean and To_Gint to the Glib package.

--- src/gtk.adb
+++ src/gtk.adb
@@ -70,27 +70,4 @@ package body Gtk is
    end Set_Object;
 
 
-   ----------------
-   -- To_Boolean --
-   ----------------
-
-   function To_Boolean (Value : in Gint) return Boolean is
-   begin
-      return Value /= 0;
-   end To_Boolean;
-
-
-   -------------
-   -- To_Gint --
-   -------------
-
-   function To_Gint (Bool : in Boolean) return Gint is
-   begin
-      if Bool then
-         return 1;
-      else
-         return 0;
-      end if;
-   end To_Gint;
-
 end Gtk;
