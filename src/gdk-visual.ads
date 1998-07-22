f82369cb713ae23efa7c635ce1511f8f0628408e
Joël Brobecker <brobecke@adacore.com>
1998-07-22 16:49:15 +0000

    Added the "--  mapping:" comments. Next step was to declare the services, but I have been short of time.

--- src/gdk-visual.ads
+++ src/gdk-visual.ads
@@ -2,6 +2,18 @@ package Gdk.Visual is
 
    type Gdk_Visual is new Root_Type with private;
 
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_depth
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_type
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_system
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_with_depth
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_with_type
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_with_both
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_ref
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_unref
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_query_depths
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_query_visual_types
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_list_visuals
 
 
 private
