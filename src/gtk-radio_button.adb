850e6616b70228e45f1d1a7fcc460610c8ff386a
Emmanuel Briot <briot@adacore.com>
1998-07-26 17:20:30 +0000

    Rewritten Function names changed to Gtk_New New functions added

--- src/gtk-radio_button.adb
+++ src/gtk-radio_button.adb
@@ -1,48 +1,99 @@
 
 package body Gtk.Radio_Button is
 
-   -----------------------
-   --  New_From_Widget  --
-   -----------------------
+   use Widget_SList;
 
-   procedure New_From_Widget
-     (Widget : out Gtk_Radio_Button;
-      Group : in Gtk_Radio_Button := Null_Gtk_Radio_Button) is
+   -----------
+   -- Group --
+   -----------
+
+   function Group (Button : in Gtk_Radio_Button) return Widget_SList.GSlist is
+      function Internal (Button : in System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_radio_button_group");
+      Group : Widget_SList.GSlist;
+   begin
+      Set_Object (Group, Internal (Get_Object (Button)));
+      return Group;
+   end Group;
+
+   -------------
+   -- GtK_New --
+   -------------
+
+   procedure Gtk_New
+     (Button : out Gtk_Radio_Button;
+      Group  : in Widget_SList.GSlist)
+   is
+      function Internal (Group : in System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_radio_button_new");
+   begin
+      Set_Object (Button, Internal (Get_Object (Group)));
+   end Gtk_New;
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New
+     (Button : out Gtk_Radio_Button;
+      Group  : in  Widget_SList.GSlist;
+      Label  : in String)
+   is
+      function Internal (Group : in System.Address;
+                         Label : in String) return System.Address;
+      pragma Import (C, Internal,
+                     "gtk_radio_button_new_with_label");
+   begin
+      Set_Object (Button, Internal (Get_Object (Group),
+                                    Label & ASCII.NUL));
+   end Gtk_New;
+
+   -------------
+   -- GtK_New --
+   -------------
+
+   procedure Gtk_New
+     (Button : out Gtk_Radio_Button;
+      Group  : in Gtk_Radio_Button)
+   is
       function Internal (Group : in System.Address) return System.Address;
       pragma Import (C, Internal, "gtk_radio_button_new_from_widget");
    begin
-      Set_Object (Widget, Internal (Get_Object (Group)));
-   end New_From_Widget;
+      Set_Object (Button, Internal (Get_Object (Group)));
+   end Gtk_New;
 
+   -------------
+   -- Gtk_New --
+   -------------
 
-   -----------------------
-   --  New_From_Widget  --
-   -----------------------
-
-   procedure New_From_Widget
-     (Widget : out Gtk_Radio_Button;
-      With_Label : in String;
-      Group : in Gtk_Radio_Button := Null_Gtk_Radio_Button) is
+   procedure Gtk_New
+     (Button : out Gtk_Radio_Button;
+      Group  : in Gtk_Radio_Button;
+      Label  : in String)
+   is
       function Internal (Group : in System.Address;
                          Label : in String) return System.Address;
       pragma Import (C, Internal,
                      "gtk_radio_button_new_with_label_from_widget");
    begin
-      Set_Object (Widget, Internal (Get_Object (Group),
-                                    With_Label & ASCII.NUL));
-   end New_From_Widget;
+      Set_Object (Button, Internal (Get_Object (Group),
+                                    Label & ASCII.NUL));
+   end Gtk_New;
 
+   ---------------
+   -- Set_Group --
+   ---------------
 
-   -----------------------------
-   --  Null_Gtk_Radio_Button  --
-   -----------------------------
-
-   function Null_Gtk_Radio_Button return Gtk_Radio_Button is
-      Result : Gtk_Radio_Button;
+   procedure Set_Group (Button : in Gtk_Radio_Button;
+                        Group  : in Widget_SList.GSlist)
+   is
+      procedure Internal (Button : in System.Address;
+                          Group  : in System.Address);
+      pragma Import (C, Internal, "gtk_radio_button_set_group");
    begin
-      Set_Object (Result, System.Null_Address);
-      return Result;
-   end Null_Gtk_Radio_Button;
+      Internal (Get_Object (Button), Get_Object (Group));
+   end Set_Group;
 
 
 end Gtk.Radio_Button;
+
--- src/gtk-radio_button.ads
+++ src/gtk-radio_button.ads
@@ -1,40 +1,47 @@
 with Gtk.Check_Button;
+with Glib.GSlist; use Glib.GSlist;
+with Gtk.Widget;
 
 package Gtk.Radio_Button is
 
    type Gtk_Radio_Button is new Check_Button.Gtk_Check_Button with private;
+   package Widget_SList is new Glib.GSlist.Generic_SList
+     (Gtk.Widget.Gtk_Widget'Class);
 
-   function Null_Gtk_Radio_Button return Gtk_Radio_Button;
+   function Group (Button : in Gtk_Radio_Button) return Widget_SList.GSlist;
+   --  mapping: Set_Group gtkradiobutton.h gtk_radio_button_group
 
-   procedure New_From_Widget
-     (Widget : out Gtk_Radio_Button;
-      Group : in Gtk_Radio_Button := Null_Gtk_Radio_Button);
+   procedure Gtk_New (Button : out Gtk_Radio_Button;
+                      Group  : in Widget_SList.GSlist);
+   --  mapping: Gtk_New gtkradiobutton.h gtk_radio_button_new
+
+   procedure Gtk_New (Button : out Gtk_Radio_Button;
+                      Group  : in Widget_SList.GSlist;
+                      Label  : in String);
+   --  mapping: Gtk_New gtkradiobutton.h gtk_radio_button_new_with_label
+
+   procedure Gtk_New
+     (Button : out Gtk_Radio_Button;
+      Group  : in Gtk_Radio_Button);
    --  mapping: New_From_Widget gtkradiobutton.h \
    --  mapping:                 gtk_radio_button_new_from_widget
 
-   procedure New_From_Widget
-     (Widget : out Gtk_Radio_Button;
-      With_Label : in String;
-      Group : in Gtk_Radio_Button := Null_Gtk_Radio_Button);
+   procedure Gtk_New
+     (Button : out Gtk_Radio_Button;
+      Group  : in Gtk_Radio_Button;
+      Label  : in String);
    --  mapping: New_With_Label_From_Widget  gtkradiobutton.h \
    --  mapping: gtk_radio_button_new_with_label_from_widget
 
+   procedure Set_Group (Button : in Gtk_Radio_Button;
+                        Group  : in Widget_SList.GSlist);
+   --  mapping: Set_Group gtkradiobutton.h gtk_radio_button_set_group
 
 private
 
    type Gtk_Radio_Button is new Check_Button.Gtk_Check_Button with null record;
 
-   --  Services not mapped...
-   --
-   --  mapping: OBJECT_ORIENTED gtkradiobutton.h gtk_radio_button_get_type
-
-   --  Services not mapped because they are not needed.
-   --
-   --  mapping: NOT_IMPLEMENTED gtkradiobutton.h gtk_radio_button_new
-   --  mapping: NOT_IMPLEMENTED gtkradiobutton.h \
-   --  mapping:                 gtk_radio_button_new_with_label
-   --  mapping: NOT_IMPLEMENTED gtkradiobutton.h gtk_radio_button_group
-   --  mapping: NOT_IMPLEMENTED gtkradiobutton.h gtk_radio_button_set_group
-
+   --  mapping: NOT_IMPLEMENTED gtkradiobutton.h gtk_radio_button_get_type
 
 end Gtk.Radio_Button;
+
