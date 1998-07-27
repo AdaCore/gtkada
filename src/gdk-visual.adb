dde21bde03a1085e8986d279278a907b92daeb08
Joël Brobecker <brobecke@adacore.com>
1998-07-27 09:39:16 +0000

    Implemented the GdkVisual services (except gdk_list_visuals).

---
+++ src/gdk-visual.adb
@@ -0,0 +1,156 @@
+with Interfaces.C.Pointers;
+
+package body Gdk.Visual is
+
+
+   type Aliased_Gint_Array is array (Natural range <>) of aliased Gint;
+
+   package Gint_Ptr is new Interfaces.C.Pointers
+     (Index => Natural, Element => Gint, Element_Array => Aliased_Gint_Array,
+      Default_Terminator => 0);
+
+
+   type Aliased_Visual_Type_Array is array (Natural range <>)
+     of aliased Types.Gdk_Visual_Type;
+
+   package Visual_Type_Ptr is new Interfaces.C.Pointers
+     (Index => Natural, Element => Types.Gdk_Visual_Type,
+      Element_Array => Aliased_Visual_Type_Array,
+      Default_Terminator => Types.Static_Gray);
+
+
+   ----------------
+   --  Get_Best  --
+   ----------------
+
+   procedure Get_Best (Visual : out Gdk_Visual'Class) is
+      function Internal return System.Address;
+      pragma Import (C, Internal, "gdk_visual_get_system");
+   begin
+      Set_Object (Visual, Internal);
+   end Get_Best;
+
+
+   ----------------
+   --  Get_Best  --
+   ----------------
+
+   procedure Get_Best (Visual :    out Gdk_Visual'Class;
+                       Depth  : in     Gint) is
+      function Internal (Depth : in Gint) return System.Address;
+      pragma Import (C, Internal, "gdk_visual_get_best_with_depth");
+   begin
+      Set_Object (Visual, Internal (Depth));
+   end Get_Best;
+
+
+   ----------------
+   --  Get_Best  --
+   ----------------
+
+   procedure Get_Best (Visual      :    out Gdk_Visual'Class;
+                       Visual_Type : in     Types.Gdk_Visual_Type) is
+      function Internal (Visual_Type : in Types.Gdk_Visual_Type)
+                         return System.Address;
+      pragma Import (C, Internal, "gdk_visual_get_best_with_type");
+   begin
+      Set_Object (Visual, Internal (Visual_Type));
+   end Get_Best;
+
+
+   ----------------
+   --  Get_Best  --
+   ----------------
+
+   procedure Get_Best (Visual      :    out Gdk_Visual'Class;
+                       Depth       : in     Gint;
+                       Visual_Type : in     Types.Gdk_Visual_Type) is
+      function Internal (Depth       : in Gint;
+                         Visual_Type : in Types.Gdk_Visual_Type)
+                         return System.Address;
+      pragma Import (C, Internal, "gdk_visual_get_best_with_both");
+   begin
+      Set_Object (Visual, Internal (Depth, Visual_Type));
+   end Get_Best;
+
+
+   ------------------
+   --  Get_System  --
+   ------------------
+
+   procedure Get_System (Visual : out Gdk_Visual'Class) is
+      function Internal return System.Address;
+      pragma Import (C, Internal, "gdk_visual_get_system");
+   begin
+      Set_Object (Visual, Internal);
+   end Get_System;
+
+
+   --------------------
+   --  Query_Depths  --
+   --------------------
+
+   function Query_Depths return Gint_Array is
+      procedure Internal (Depths : out Gint_Ptr.Pointer; Count : out Gint);
+      pragma Import (C, Internal, "gdk_query_depths");
+
+      Internal_Result : Gint_Ptr.Pointer;
+      Count : Gint;
+
+   begin
+
+      Internal (Internal_Result, Count);
+
+      declare
+         Temp : constant Aliased_Gint_Array :=
+           Gint_Ptr.Value (Ref => Internal_Result,
+                           Length => Interfaces.C.ptrdiff_t (Count));
+         Result : Gint_Array (1 .. Temp'Length);
+      begin
+
+         for Index in Temp'Range loop
+            Result (Result'First + Index - Temp'First) := Temp (Index);
+         end loop;
+
+         return Result;
+
+      end;
+
+   end Query_Depths;
+
+
+   --------------------------
+   --  Query_Visual_Types  --
+   --------------------------
+
+   function Query_Visual_Types return Gdk_Visual_Type_Array is
+      procedure Internal (Visual_Type : out Visual_Type_Ptr.Pointer;
+                          Count : out Gint);
+      pragma Import (C, Internal, "gdk_query_visual_types");
+
+      Internal_Result : Visual_Type_Ptr.Pointer;
+      Count : Gint;
+
+   begin
+
+      Internal (Internal_Result, Count);
+
+      declare
+         Temp : constant Aliased_Visual_Type_Array :=
+           Visual_Type_Ptr.Value (Ref => Internal_Result,
+                                  Length => Interfaces.C.ptrdiff_t (Count));
+         Result : Gdk_Visual_Type_Array (1 .. Temp'Length);
+      begin
+
+         for Index in Temp'Range loop
+            Result (Result'First + Index - Temp'First) := Temp (Index);
+         end loop;
+
+         return Result;
+
+      end;
+
+   end Query_Visual_Types;
+
+
+end Gdk.Visual;
--- src/gdk-visual.ads
+++ src/gdk-visual.ads
@@ -1,23 +1,61 @@
+with Glib; use Glib;
+with Gdk.Types;
+
+
 package Gdk.Visual is
 
    type Gdk_Visual is new Root_Type with private;
 
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_depth
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_type
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_system
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_with_depth
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_with_type
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_get_best_with_both
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_ref
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_unref
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_query_depths
-   --  mapping: NOT_IMPLEMENTED gdk.h gdk_query_visual_types
+   type Gdk_Visual_Type_Array is array (Natural range <>)
+     of Types.Gdk_Visual_Type;
+
+
+   function Get_Best_Depth return Gint;
+   --  mapping: Get_Best_Depth gdk.h gdk_visual_get_best_depth
+
+   function Get_Best_Type return Types.Gdk_Visual_Type;
+   --  mapping: Get_Best_Type gdk.h gdk_visual_get_best_type
+
+   procedure Get_System (Visual : out Gdk_Visual'Class);
+   --  mapping: Get_System gdk.h gdk_visual_get_system
+
+   procedure Get_Best (Visual : out Gdk_Visual'Class);
+   --  mapping: Get_Best gdk.h gdk_visual_get_best
+
+   procedure Get_Best (Visual :    out Gdk_Visual'Class;
+                       Depth  : in     Gint);
+   --  mapping: Get_Best gdk.h gdk_visual_get_best_with_depth
+
+   procedure Get_Best (Visual      :    out Gdk_Visual'Class;
+                       Visual_Type : in     Types.Gdk_Visual_Type);
+   --  mapping: Get_Best gdk.h gdk_visual_get_best_with_type
+
+   procedure Get_Best (Visual      :    out Gdk_Visual'Class;
+                       Depth       : in     Gint;
+                       Visual_Type : in     Types.Gdk_Visual_Type);
+   --  mapping: Get_Best gdk.h gdk_visual_get_best_with_both
+
+   function Query_Depths return Gint_Array;
+   --  mapping: Query_Depths gdk.h gdk_query_depths
+
+   function Query_Visual_Types return Gdk_Visual_Type_Array;
+   --  mapping: Query_Visual_Types gdk.h gdk_query_visual_types
+
+
    --  mapping: NOT_IMPLEMENTED gdk.h gdk_list_visuals
 
 
 private
 
    type Gdk_Visual is new Root_Type with null record;
+
+
+   pragma Import (C, Get_Best_Depth, "gdk_visual_get_best_depth");
+   pragma Import (C, Get_Best_Type, "gdk_visual_get_best_type");
+
+   --  probably not needed.
+   --
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_ref
+   --  mapping: NOT_IMPLEMENTED gdk.h gdk_visual_unref
 
 end Gdk.Visual;
