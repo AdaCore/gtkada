888bec1327c13a59ad703252a902f47970aba8de
Joël Brobecker <brobecke@adacore.com>
1998-08-03 19:51:29 +0000

    Added Draw_Pixmap (Hello Manu :-)

--- src/gdk-drawable.adb
+++ src/gdk-drawable.adb
@@ -24,4 +24,33 @@ package body Gdk.Drawable is
                 X, Y, Width, Height);
    end Draw_Rectangle;
 
+
+   -----------------
+   -- Draw_Pixmap --
+   -----------------
+
+   procedure Draw_Pixmap (Drawable : in Gdk.Drawable.Gdk_Drawable'Class;
+                          Gc       : in Gdk.GC.Gdk_GC'Class;
+                          Src      : in Gdk.Drawable.Gdk_Drawable'Class;
+                          Xsrc     : in Gint;
+                          Ysrc     : in Gint;
+                          Xdest    : in Gint;
+                          Ydest    : in Gint;
+                          Width    : in Gint;
+                          Height   : in Gint) is
+      procedure Internal (Drawable : in System.Address;
+                          Gc       : in System.Address;
+                          Src      : in System.Address;
+                          Xsrc     : in Gint;
+                          Ysrc     : in Gint;
+                          Xdest    : in Gint;
+                          Ydest    : in Gint;
+                          Width    : in Gint;
+                          Height   : in Gint);
+      pragma Import (C, Internal, "gdk_draw_pixmap");
+   begin
+      Internal (Get_Object (Drawable), Get_Object (Gc), Get_Object (Src),
+                Xsrc, Ysrc, Xdest, Ydest, Width, Height);
+   end Draw_Pixmap;
+
 end Gdk.Drawable;
