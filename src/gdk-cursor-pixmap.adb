5fed8c6a83bc0f5abb6541d448515e4ca0b0c05a
Emmanuel Briot <briot@adacore.com>
1998-11-03 05:23:14 +0000

    Checkin for Joel New file, for circular dependencies reasons

---
+++ src/gdk-cursor-pixmap.adb
@@ -0,0 +1,62 @@
+-----------------------------------------------------------------------
+--          GtkAda - Ada95 binding for the Gimp Toolkit              --
+--                                                                   --
+-- Copyright (C) 1998 Emmanuel Briot and Joel Brobecker              --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+package body Gdk.Cursor.Pixmap is
+
+   -------------
+   -- Gdk_New --
+   -------------
+
+   procedure Gdk_New
+      (Widget : out Gdk_Cursor;
+       Source : in Gdk.Pixmap.Gdk_Pixmap'Class;
+       Mask   : in Gdk.Pixmap.Gdk_Pixmap'Class;
+       Fg     : in Gdk.Color.Gdk_Color;
+       Bg     : in Gdk.Color.Gdk_Color;
+       X      : in Gint;
+       Y      : in Gint)
+   is
+      function Internal
+         (Source : in System.Address;
+          Mask   : in System.Address;
+          Fg     : in System.Address;
+          Bg     : in System.Address;
+          X      : in Gint;
+          Y      : in Gint)
+          return      System.Address;
+      pragma Import (C, Internal, "gdk_cursor_new_from_pixmap");
+   begin
+      Set_Object (Widget, Internal (Get_Object (Source),
+                                    Get_Object (Mask),
+                                    Fg'Address,
+                                    Bg'Address,
+                                    X,
+                                    Y));
+   end Gdk_New;
+
+end Gdk.Cursor.Pixmap;
