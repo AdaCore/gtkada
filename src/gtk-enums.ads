575b91c030c63e5c8856e56eececb68523b381b3
Emmanuel Briot <briot@adacore.com>
1999-02-19 13:47:35 +0000

    Do the unchecked conversions from Gint to System.Address directly in Ada, don't try to use C for that. (Actually, protect them with pragma Warnings (Off)).

--- src/gtk-enums.ads
+++ src/gtk-enums.ads
@@ -178,14 +178,10 @@ package Gtk.Enums is
 
    function Convert (S : String) return System.Address;
    function Convert (S : System.Address) return String;
-   function Convert_I (I : Gint) return System.Address;
-   function Convert_A (S : System.Address) return Gint;
-   pragma Import (C, Convert_I, "convert_i");
-   pragma Import (C, Convert_A, "convert_a");
+   function Convert (I : Gint) return System.Address;
+   function Convert (S : System.Address) return Gint;
 
    package String_List is new Glib.Glist.Generic_List (String);
-   package Gint_List is new Glib.Glist.Generic_List (Gint,
-                                                     Convert_I,
-                                                     Convert_A);
+   package Gint_List is new Glib.Glist.Generic_List (Gint);
 
 end Gtk.Enums;
