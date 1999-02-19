ecbc475f5d03cd712abd0ea51794712109fbad45
Arnaud Charlet <charlet@adacore.com>
1999-02-19 10:30:31 +0000

    (Reparent_Label): Work around a bug in GNAT 3.12w

--- testgtk/create_reparent.adb
+++ testgtk/create_reparent.adb
@@ -44,7 +44,7 @@ with Ada.Text_IO;
 
 package body Create_Reparent is
 
-   package Box_Cb is new Signal.Callback (Gtk_Button, Gtk_Box'Class);
+   package Box_Cb is new Signal.Callback (Gtk_Button, Gtk_Box);
    package Int_Cb is new Signal.Two_Callback (Gtk_Label, Gint, Gtk_Widget);
    package Label_User is new User_Data (Gtk_Label);
 
@@ -77,7 +77,7 @@ package body Create_Reparent is
    end Set_Parent_Signal;
 
    procedure Reparent_Label (Widget     : in out Gtk_Button;
-                             New_Parent : in out Gtk_Box'Class)
+                             New_Parent : in out Gtk_Box)
    is
       Label : Gtk_Label;
    begin
