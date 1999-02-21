183f8c896dd56ae48627baf03ebca1802c080334
Arnaud Charlet <charlet@adacore.com>
1999-02-21 11:05:05 +0000

    (Generate): Correct handling of the label parameter of Gtk_New.  Add call to Container.Add when needed. Add handling of Gtk_File_Selection  children (e.g Ok_Button, Cancel_Button).

--- src/gtk-button.adb
+++ src/gtk-button.adb
@@ -30,6 +30,8 @@
 with System;
 with Gdk; use Gdk;
 with Gtk.Util; use Gtk.Util;
+with Gtk.Container; use Gtk.Container;
+with Gtk.File_Selection; use Gtk.File_Selection;
 
 package body Gtk.Button is
 
@@ -145,29 +147,57 @@ package body Gtk.Button is
                        File   : in File_Type) is
       use Container;
 
-      P : Node_Ptr := Find_Tag (N.Child, "label");
+      Child_Name : Node_Ptr   := Find_Tag (N.Child, "child_name");
+      Label      : String_Ptr := Get_Field (N, "label");
+
    begin
-      if P = null then
-         Gen_New (N, "Button", File => File);
+      if Child_Name = null then
+         if Label = null then
+            Gen_New (N, "Button", File => File);
+         else
+            Gen_New (N, "Button", Label.all, File => File, Delim => '"');
+         end if;
       else
-         Gen_New (N, "Button", P.Value.all, File => File, Delim => '"');
+         Gen_Child (N, Child_Name, File);
       end if;
 
       Generate (Gtk_Container (Button), N, File);
       Gen_Set (N, "Button", "relief", File);
+
+      if Child_Name = null then
+         Gen_Call_Child (N, null, "Container", "Add", File => File);
+      end if;
    end Generate;
 
    procedure Generate (Button : in out Gtk_Button;
                        N      : in Node_Ptr) is
       use Container;
 
-      S : String_Ptr := Get_Field (N, "label");
+      Child_Name     : String_Ptr := Get_Field (N, "child_name");
+      S              : String_Ptr := Get_Field (N, "label");
+      File_Selection : Gtk_File_Selection;
+
    begin
       if not N.Specific_Data.Created then
-         if S = null then
-            Gtk_New (Button);
+         if Child_Name = null then
+            if S = null then
+               Gtk_New (Button);
+            else
+               Gtk_New (Button, S.all);
+            end if;
          else
-            Gtk_New (Button, S.all);
+            File_Selection :=
+              Gtk_File_Selection (Get_Object (Find_Tag
+                (Find_Parent (N.Parent, Get_Part (Child_Name.all, 1)),
+                 "name").Value).all);
+
+            if Get_Part (Child_Name.all, 2) = "ok_button" then
+               Button := Get_Ok_Button (File_Selection);
+            elsif Get_Part (Child_Name.all, 2) = "cancel_button" then
+               Button := Get_Cancel_Button (File_Selection);
+            elsif Get_Part (Child_Name.all, 2) = "help_button" then
+               Button := Get_Help_Button (File_Selection);
+            end if;
          end if;
 
          Set_Object (Get_Field (N, "name"), Button'Unchecked_Access);
@@ -180,6 +210,11 @@ package body Gtk.Button is
 
       if S /= null then
          Set_Relief (Button, Gtk.Enums.Gtk_Relief_Style'Value (S.all));
+      end if;
+
+      if Child_Name = null then
+         Container.Add (Gtk_Container (Get_Object
+           (Get_Field (N.Parent, "name")).all), Button);
       end if;
    end Generate;
 
