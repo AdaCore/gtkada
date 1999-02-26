e3197e53a613e02769c938fc69e5176d9087f13b
Emmanuel Briot <briot@adacore.com>
1999-02-26 17:50:18 +0000

    Declare some icons required for some tests. These are embedded icons

--- testgtk/common.ads
+++ testgtk/common.ads
@@ -34,6 +34,7 @@ with Gtk.Signal;
 with Gtk.Label; use Gtk.Label;
 with Gtk.Widget; use Gtk.Widget;
 with Gtk.Window; use Gtk.Window;
+with Interfaces.C.Strings;
 
 package Common is
 
@@ -54,4 +55,116 @@ package Common is
                                               Gtk_Window_Access);
    procedure Destroy_Window (Win : access Gtk.Window.Gtk_Window_Record;
                              Ptr : in Gtk_Window_Access);
+
+   package ICS renames Interfaces.C.Strings;
+   Book_Open_Xpm    : ICS.chars_ptr_array :=
+     (ICS.New_String ("16 16 4 1"),
+      ICS.New_String ("       c None s None"),
+      ICS.New_String (".      c black"),
+      ICS.New_String ("X      c #808080"),
+      ICS.New_String ("o      c white"),
+      ICS.New_String ("                "),
+      ICS.New_String ("  ..            "),
+      ICS.New_String (" .Xo.    ...    "),
+      ICS.New_String (" .Xoo. ..oo.    "),
+      ICS.New_String (" .Xooo.Xooo...  "),
+      ICS.New_String (" .Xooo.oooo.X.  "),
+      ICS.New_String (" .Xooo.Xooo.X.  "),
+      ICS.New_String (" .Xooo.oooo.X.  "),
+      ICS.New_String (" .Xooo.Xooo.X.  "),
+      ICS.New_String (" .Xooo.oooo.X.  "),
+      ICS.New_String ("  .Xoo.Xoo..X.  "),
+      ICS.New_String ("   .Xo.o..ooX.  "),
+      ICS.New_String ("    .X..XXXXX.  "),
+      ICS.New_String ("    ..X.......  "),
+      ICS.New_String ("     ..         "),
+      ICS.New_String ("                "));
+   Book_Closed_Xpm  : ICS.chars_ptr_array :=
+     (ICS.New_String ("16 16 6 1"),
+      ICS.New_String ("       c None s None"),
+      ICS.New_String (".      c black"),
+      ICS.New_String ("X      c red"),
+      ICS.New_String ("o      c yellow"),
+      ICS.New_String ("O      c #808080"),
+      ICS.New_String ("#      c white"),
+      ICS.New_String ("                "),
+      ICS.New_String ("       ..       "),
+      ICS.New_String ("     ..XX.      "),
+      ICS.New_String ("   ..XXXXX.     "),
+      ICS.New_String (" ..XXXXXXXX.    "),
+      ICS.New_String (".ooXXXXXXXXX.   "),
+      ICS.New_String ("..ooXXXXXXXXX.  "),
+      ICS.New_String (".X.ooXXXXXXXXX. "),
+      ICS.New_String (".XX.ooXXXXXX..  "),
+      ICS.New_String (" .XX.ooXXX..#O  "),
+      ICS.New_String ("  .XX.oo..##OO. "),
+      ICS.New_String ("   .XX..##OO..  "),
+      ICS.New_String ("    .X.#OO..    "),
+      ICS.New_String ("     ..O..      "),
+      ICS.New_String ("      ..        "),
+      ICS.New_String ("                "));
+
+   Mini_Page_Xpm  : ICS.chars_ptr_array :=
+     (ICS.New_String ("16 16 4 1"),
+      ICS.New_String ("       c None s None"),
+      ICS.New_String (".      c black"),
+      ICS.New_String ("X      c white"),
+      ICS.New_String ("O      c #808080"),
+      ICS.New_String ("                "),
+      ICS.New_String ("   .......      "),
+      ICS.New_String ("   .XXXXX..     "),
+      ICS.New_String ("   .XoooX.X.    "),
+      ICS.New_String ("   .XXXXX....   "),
+      ICS.New_String ("   .XooooXoo.o  "),
+      ICS.New_String ("   .XXXXXXXX.o  "),
+      ICS.New_String ("   .XooooooX.o  "),
+      ICS.New_String ("   .XXXXXXXX.o  "),
+      ICS.New_String ("   .XooooooX.o  "),
+      ICS.New_String ("   .XXXXXXXX.o  "),
+      ICS.New_String ("   .XooooooX.o  "),
+      ICS.New_String ("   .XXXXXXXX.o  "),
+      ICS.New_String ("   ..........o  "),
+      ICS.New_String ("    oooooooooo  "),
+      ICS.New_String ("                "));
+
+   Gtk_Mini_Xpm  : ICS.chars_ptr_array :=
+     (ICS.New_String ("15 20 17 1"),
+      ICS.New_String ("       c None"),
+      ICS.New_String (".      c #14121F"),
+      ICS.New_String ("+      c #278828"),
+      ICS.New_String ("@      c #9B3334"),
+      ICS.New_String ("#      c #284C72"),
+      ICS.New_String ("$      c #24692A"),
+      ICS.New_String ("%      c #69282E"),
+      ICS.New_String ("&      c #37C539"),
+      ICS.New_String ("*      c #1D2F4D"),
+      ICS.New_String ("=      c #6D7076"),
+      ICS.New_String ("-      c #7D8482"),
+      ICS.New_String (";      c #E24A49"),
+      ICS.New_String (">      c #515357"),
+      ICS.New_String (",      c #9B9C9B"),
+      ICS.New_String ("'      c #2FA232"),
+      ICS.New_String (")      c #3CE23D"),
+      ICS.New_String ("!      c #3B6CCB"),
+      ICS.New_String ("               "),
+      ICS.New_String ("      ***>     "),
+      ICS.New_String ("    >.*!!!*    "),
+      ICS.New_String ("   ***....#*=  "),
+      ICS.New_String ("  *!*.!!!**!!# "),
+      ICS.New_String (" .!!#*!#*!!!!# "),
+      ICS.New_String (" @%#!.##.*!!$& "),
+      ICS.New_String (" @;%*!*.#!#')) "),
+      ICS.New_String (" @;;@%!!*$&)'' "),
+      ICS.New_String (" @%.%@%$'&)$+' "),
+      ICS.New_String (" @;...@$'*'*)+ "),
+      ICS.New_String (" @;%..@$+*.')$ "),
+      ICS.New_String (" @;%%;;$+..$)# "),
+      ICS.New_String (" @;%%;@$$$'.$# "),
+      ICS.New_String (" %;@@;;$$+))&* "),
+      ICS.New_String ("  %;;;@+$&)&*  "),
+      ICS.New_String ("   %;;@'))+>   "),
+      ICS.New_String ("    %;@'&#     "),
+      ICS.New_String ("     >%$$      "),
+      ICS.New_String ("      >=       "));
+
 end Common;
