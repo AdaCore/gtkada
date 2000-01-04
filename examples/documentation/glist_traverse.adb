c67ec7590453af89209656b6a0586b1b4935b2b0
Emmanuel Briot <briot@adacore.com>
2000-01-04 17:49:13 +0000

    New example on how to traverse a list.

---
+++ examples/documentation/glist_traverse.adb
@@ -0,0 +1,30 @@
+with Glib, Glib.Glist, Gtk.Enums, Ada.Text_IO;
+use  Glib, Ada.Text_IO;
+
+procedure Glist_Traverse is
+   use Gtk.Enums.Gint_List;
+   List : Gtk.Enums.Gint_List.Glist;
+   Temp : Gtk.Enums.Gint_List.Glist;
+begin
+
+   --  First step: create a new list.
+
+   Prepend (List, 2);                       --  add a the beginning of the list
+   Append (List, 3);                        --  add a the end of the list
+   Insert (List, Data => 1, Position => 1); --  in the middle of the list
+
+   --  Traverse the list (first way)
+
+   Temp := First (List);
+   while Temp /= Null_List loop
+      Put_Line (Gint'Image (Get_Data (Temp)));
+      Temp := Next (Temp);
+   end loop;
+
+   --  Traverse the list (second way)
+
+   for I in 1 .. Length (List) loop
+      Put_Line (Gint'Image (Nth_Data (List, I - 1)));
+   end loop;
+
+end Glist_Traverse;
