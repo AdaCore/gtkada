78229d24525ee6617751eb1cdee85866217baf53
Arnaud Charlet <charlet@adacore.com>
2000-01-20 11:00:41 +0000

    Fix documentation layout and style.

--- src/gdk-gc.ads
+++ src/gdk-gc.ads
@@ -65,20 +65,20 @@ package Gdk.GC is
 
    procedure Gdk_New (GC     :    out Gdk_GC;
                       Window : in     Gdk.Window.Gdk_Window'Class);
-   --  Creates a new graphic context.
+   --  Create a new graphic context.
    --  The window must have been realized first (so that it is associated
    --  with some ressources on the Xserver).
    --  The GC can then be used for any window that has the same root window,
-   --  and same color depth a WINDOW.
+   --  and same color depth as Window.
    --  See the manual page for XCreateGC on Unix systems for more information.
 
    procedure Gdk_New (GC          :    out Gdk_GC;
                       Window      : in     Gdk.Window.Gdk_Window'Class;
                       Values      : in     Gdk_GC_Values'Class;
                       Values_Mask : in     Types.Gdk_GC_Values_Mask);
-   --  Creates a new graphic context.
-   --  It is directly created with the values set in VALUES, and whose
-   --  associated field has been set in VALUES_MASK.
+   --  Create a new graphic context.
+   --  It is directly created with the values set in Values, and whose
+   --  associated field has been set in Values_Mask.
    --  This is faster than calling the simple Gdk_New function and each of
    --  other functions in this package, since each of them requires a call
    --  to the server.
@@ -89,8 +89,8 @@ package Gdk.GC is
    --  the user responsability to do so.
 
    procedure Ref (GC : in out Gdk_GC);
-   --  Increments the reference counting for the graphic context.
-   --  You should not normally have to use it.
+   --  Increment the reference counting for the graphic context.
+   --  You should usually not have to use it.
 
    procedure Unref (GC : in out Gdk_GC);
    --  Decrement the reference counting for the graphic context.
@@ -100,26 +100,26 @@ package Gdk.GC is
                          Values :    out Gdk_GC_Values);
    --  Get the values set in the GC.
    --  This copies the values from the server to client, allowing faster
-   --  modifications. VALUES can then be copied back to the server by
-   --  created a new graphic context with the function Gdk_New above.
+   --  modifications. Values can then be copied back to the server by
+   --  creating a new graphic context with the function Gdk_New above.
 
    procedure Set_Foreground (GC    : in Gdk_GC;
                              Color : in Gdk.Color.Gdk_Color);
-   --  Sets the foreground color for the graphic context.
+   --  Set the foreground color for the graphic context.
    --  This color is the one that is used by most drawing functions.
 
    procedure Set_Background (GC     : in Gdk_GC;
                              Color  : in Gdk.Color.Gdk_Color);
-   --  Sets the background color for the graphic context.
+   --  Set the background color for the graphic context.
 
    procedure Set_Font (GC   : in Gdk_GC;
                        Font : in Gdk.Font.Gdk_Font'Class);
-   --  Sets the font used by the graphic context.
+   --  Set the font used by the graphic context.
    --  This font is used by the function Gdk.Drawable.Draw_Text.
 
    procedure Set_Function (GC   : in Gdk_GC;
                            Func : in Types.Gdk_Function);
-   --  Sets the function in the graphic context.
+   --  Set the function in the graphic context.
    --  This function specifies how the points are put on the screen, ie
    --  if GtkAda how GtkAda should mix the point already on the screen
    --  and the new point being put.
@@ -135,7 +135,7 @@ package Gdk.GC is
 
    procedure Set_Fill (GC   : in Gdk_GC;
                        Fill : in Types.Gdk_Fill);
-   --  Sets that pattern used for filling the polygons.
+   --  Set the pattern used for filling the polygons.
 
    --  procedure Set_Tile
    --  procedure Set_Stipple
@@ -146,25 +146,25 @@ package Gdk.GC is
 
    procedure Set_Ts_Origin (GC   : in Gdk_GC;
                             X, Y : in Gint);
-   --  Sets the Tile and Stiple origin in the graphic context.
+   --  Set the Tile and Stiple origin in the graphic context.
 
    procedure Set_Clip_Origin (GC   : in Gdk_GC;
                               X, Y : in Gint);
-   --  Sets the origin of the clip mask. See the functions
+   --  Set the origin of the clip mask.
    --  See the functions Set_Clip_Rectangle, Set_Clip_Region and
    --  Set_Clip_Mask for more explanation.
 
    procedure Set_Clip_Rectangle
      (GC        : in Gdk_GC;
       Rectangle : in Gdk.Rectangle.Gdk_Rectangle);
-   --  Sets the clip rectangle.
+   --  Set the clip rectangle.
    --  Only the points that are drawn inside this rectangle will be displayed
    --  on the screen. Note that you might have to modify the Clip origin first
    --  with Set_Clip_Origin.
 
    procedure Set_Clip_Region (GC     : in Gdk_GC;
                               Region : in Gdk.Region.Gdk_Region'Class);
-   --  Defines a clip region on the screen.
+   --  Define a clip region on the screen.
    --  This is just like Set_Clip_Rectangle, except that a region is a more
    --  complex region, that can be the intersection or union of multiple
    --  rectangles. Note that the Clip_Origin can have an influence on this
@@ -172,14 +172,14 @@ package Gdk.GC is
 
    procedure Set_Subwindow (GC   : in Gdk_GC;
                             Mode : in Types.Gdk_Subwindow_Mode);
-   --  Sets the subwindow mode for the graphic context.
+   --  Set the subwindow mode for the graphic context.
    --  This specifies whether the drawing routines should be clipped to
    --  the specific window they are drawn into, or if they should extend
    --  to subwindows as well.
 
    procedure Set_Exposures (GC        : in Gdk_GC;
                             Exposures : in Boolean);
-   --  EXPOSURES indicates  whether you want "expose" and "noexpose" events to
+   --  Exposures indicates whether you want "expose" and "noexpose" events to
    --  be reported when calling Copy_Area and Copy_Plane with this GC.
    --  You should disable this if you don't need the event and want to optimize
    --  your application.
@@ -190,34 +190,33 @@ package Gdk.GC is
                                   Cap_Style  : in Types.Gdk_Cap_Style;
                                   Join_Style : in Types.Gdk_Join_Style);
    --  Set the line attributes for this GC.
-   --  LINE_WIDTH is the width of the line. If its value is 0, the line is as
+   --  Line_Width is the width of the line. If its value is 0, the line is as
    --  thin as possible, possibly even more so than if the width is 1. It is
    --  also faster to draw a line with width 0 than any other line width.
    --
-   --  LINE_STYLE specifies whether the line should be solid or dashed. If its
+   --  Line_Style specifies whether the line should be solid or dashed. If its
    --  value is Line_On_Off_Dash, the colors are alternatively the foreground
    --  color, and blank. If the value is Line_Double_Dash, the colors are
    --  alternatively the foreground and background colors.
    --
-   --  CAP_STYLE specifies how the line should end, either flat or rounded.
+   --  Cap_Style specifies how the line should end, either flat or rounded.
    --
-   --  JOIN_STYLE specifies how to consecutive lines drawn by Draw_Lines are
+   --  Join_Style specifies how two consecutive lines drawn by Draw_Lines are
    --  connected.
 
    procedure Set_Dashes (Gc          : in Gdk_GC;
                          Dash_Offset : in Gint;
                          Dash_List   : in Guchar_Array);
-   --  Specifies the dash pattern when the style of the line is anything but
-   --  solid.
+   --  Specify the dash pattern when the line's style is anything but solid.
    --  The values in the array alternatively give the length (in pixels) of
    --  the plain dash, the empty dash, the second plain dash, ... None of
-   --  these values can be 0. If there is an odd number of items in DASH_LIST,
+   --  these values can be 0. If there is an odd number of items in Dash_List,
    --  this is equivalent to giving the array concatenated with itself.
-   --  DASH_OFFSET specifies of the phase of the pattern to start with.
+   --  Dash_Offset specifies the phase of the pattern to start with.
 
    procedure Copy (Dst_GC :    out Gdk_GC;
                    Src_GC : in     Gdk_GC);
-   --  Copies a SRC_GC to DST_GC.
+   --  Copy a Src_GC to Dst_GC.
 
 
    ----------------------
@@ -225,12 +224,12 @@ package Gdk.GC is
    ----------------------
 
    function Gdk_New return Gdk_GC_Values;
-   --  Allocates a new Values structure on the client.
+   --  Allocate a new Values structure on the client.
    --  Note that this function allocates a C structure, and thus needs to
    --  be freed with a call to Free below.
 
    procedure Free (Values : in out Gdk_GC_Values);
-   --  Frees the C structure associated with Values.
+   --  Free the C structure associated with Values.
 
    procedure Set_Foreground (Values : in Gdk_GC_Values;
                              Color  : in Gdk.Color.Gdk_Color);
