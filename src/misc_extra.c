8bd1dab282ecc8396eeb4072f27198e66e3873c5
Emmanuel Briot <briot@adacore.com>
2000-02-21 08:58:43 +0000

    Add some support functions for gtk-extra-plot_layout

--- src/misc_extra.c
+++ src/misc_extra.c
@@ -1,6 +1,7 @@
 
 #include <gtk/gtk.h>
 #include <gtkextra/gtkplot.h>
+#include <gtkextra/gtkplotcanvas.h>
 
 /********************************************************************
  **
@@ -18,7 +19,56 @@ ada_gtk_extra_plot_set_flags (GtkPlot* plot, guint8 flags) {
   GTK_PLOT_SET_FLAGS (plot, flags);
 }
 
+void
+ada_gtk_extra_plot_unset_flags (GtkPlot* plot, guint8 flags) {
+  GTK_PLOT_UNSET_FLAGS (plot, flags);
+}
+
 char*
 ada_gtk_dataset_get_name (GtkPlotData* data) {
   return data->name;
+}
+
+GList*
+ada_gtk_plot_get_datasets (GtkPlot* plot) {
+  return plot->data_sets;
+}
+
+GList*
+ada_gtk_plot_get_texts (GtkPlot* plot) {
+  return plot->text;
+}
+
+gchar*
+ada_gtk_plot_get_text_string (GtkPlotText* text) {
+  return text->text;
+}
+
+void
+ada_gtk_plot_get_text_position (GtkPlotText* text,
+				gdouble* x,
+				gdouble* y) {
+  *x = text->x;
+  *y = text->y;
+}
+
+/********************************************************************
+ **
+ **  Gtk_Plot_Canvas widget
+ **
+ ********************************************************************/
+
+guint
+ada_gtk_plot_canvas_flag_is_set (GtkPlotCanvas* canvas, guint16 flag) {
+  return ((GTK_PLOT_CANVAS_FLAGS (canvas) & flag) != 0);
+}
+
+void
+ada_gtk_plot_canvas_set_flags (GtkPlotCanvas* canvas, guint16 flags) {
+  GTK_PLOT_CANVAS_SET_FLAGS (canvas, flags);
+}
+
+void
+ada_gtk_plot_canvas_unset_flags (GtkPlotCanvas* canvas, guint16 flags) {
+  GTK_PLOT_CANVAS_UNSET_FLAGS (canvas, flags);
 }
