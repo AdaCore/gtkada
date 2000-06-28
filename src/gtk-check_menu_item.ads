b85f54d27dd43a257bde75de1a483318c9944869
Arnaud Charlet <charlet@adacore.com>
2000-06-28 15:49:45 +0000

    Add full documentation.

--- src/gtk-check_menu_item.ads
+++ src/gtk-check_menu_item.ads
@@ -27,6 +27,17 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
+--  <description>
+--  A Gtk_Check_Menu_Item is a menu item that maintains the state of a boolean
+--  value in addition to a Gtk_Menu_Item's usual role in activating application
+--  code.
+--
+--  A check box indicating the state of the boolean value is displayed at the
+--  left side of the Gtk_Menu_Item. Activating the Gtk_Menu_Item toggles the
+--  value.
+--  </description>
+--  <c_version>1.2.8</c_version>
+
 with Gtk.Object; use Gtk.Object;
 with Gtk.Menu_Item;
 
@@ -36,37 +47,72 @@ package Gtk.Check_Menu_Item is
      with private;
    type Gtk_Check_Menu_Item is access all Gtk_Check_Menu_Item_Record'Class;
 
-   procedure Gtk_New (Check_Menu_Item : out Gtk_Check_Menu_Item;
-                      Label  : in String := "");
+   procedure Gtk_New
+     (Check_Menu_Item : out Gtk_Check_Menu_Item;
+      Label  : in String := "");
+   --  Create a new Gtk_Check_Menu_Item with a label, if label isn't null.
+
    procedure Initialize
      (Check_Menu_Item : access Gtk_Check_Menu_Item_Record'Class;
       Label  : in String := "");
+   --  Internal initialization function.
+   --  See the section "Creating your own widgets" in the documentation.
+
+   function Get_Type return Gtk.Gtk_Type;
+   --  Return the internal value associated with a Gtk_Calendar.
+
    procedure Set_Show_Toggle
      (Check_Menu_Item : access Gtk_Check_Menu_Item_Record;
       Always          : in Boolean);
+   --  Control whether the check box is shown at all times.
+   --  Normally the check box is shown only when it is active or while the
+   --  menu item is selected.
+
    procedure Set_Always_Show_Toggle
      (Check_Menu_Item : access Gtk_Check_Menu_Item_Record;
       Always          : in Boolean);
+   --  Same as Set_Show_Toggle.
+   --  Provided to simplify automated tools, such as Gate.
 
-   procedure Set_Active (Check_Menu_Item : access Gtk_Check_Menu_Item_Record;
-                         Is_active       : in Boolean);
+   procedure Set_Active
+     (Check_Menu_Item : access Gtk_Check_Menu_Item_Record;
+      Is_active       : in Boolean);
+   --  Set the active state of the menu item's check box.
+
    procedure Toggled (Check_Menu_Item : access Gtk_Check_Menu_Item_Record);
+   --  Emit the "toggled" signal.
 
-   function Get_Active (Check_Menu_Item : access Gtk_Check_Menu_Item_Record)
-                        return Boolean;
-   --   Returns True if the Item is active
+   function Get_Active
+     (Check_Menu_Item : access Gtk_Check_Menu_Item_Record) return Boolean;
+   --  Return True if the Item is active
 
-   --  The two following procedures are used to generate and create widgets
-   --  from a Node.
+   ----------------------------
+   -- Support for Gate/Dgate --
+   ----------------------------
 
-   procedure Generate (N               : in Node_Ptr;
-                       File            : in File_Type);
+   procedure Generate (N : in Node_Ptr; File : in File_Type);
+   --  Gate internal function
 
-   procedure Generate (Check_Menu_Item : in out Gtk_Object;
-                       N               : in Node_Ptr);
+   procedure Generate (Check_Menu_Item : in out Gtk_Object; N : in Node_Ptr);
+   --  Dgate internal function
+
+   -------------
+   -- Signals --
+   -------------
+
+   --  <signals>
+   --  The following new signals are defined for this widget:
+   --  - "toggled"
+   --    procedure Handler
+   --      (Check_Menu_Item : access Gtk_Check_Menu_Item_Record'Class);
+   --
+   --  Emitted when the state of the check box is changed.
+   --  A signal handler can call Get_Active to discover the new state.
+   --  </signals>
 
 private
    type Gtk_Check_Menu_Item_Record is new Gtk.Menu_Item.Gtk_Menu_Item_Record
      with null record;
 
+   pragma Import (C, Get_Type, "gtk_check_menu_item_get_type");
 end Gtk.Check_Menu_Item;
