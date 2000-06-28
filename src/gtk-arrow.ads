e47111cdc0aad2ff75bcc861e06394c3f8a488a2
Arnaud Charlet <charlet@adacore.com>
2000-06-28 15:14:39 +0000

    Add full documentation.

--- src/gtk-arrow.ads
+++ src/gtk-arrow.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --          GtkAda - Ada95 binding for the Gimp Toolkit              --
 --                                                                   --
---                     Copyright (C) 1998-1999                       --
+--                     Copyright (C) 1998-2000                       --
 --        Emmanuel Briot, Joel Brobecker and Arnaud Charlet          --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
@@ -27,6 +27,21 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
+--  <description>
+--  Gtk_Arrow should be used to draw simple arrows that need to point in one
+--  of the four cardinal directions (up, down, left, or right). The style of
+--  the arrow can be one of shadow in, shadow out, etched in, or etched out.
+--  Note that these directions and style types may be ammended in versions of
+--  Gtk to come.
+--
+--  Gtk_Arrow will fill any space alloted to it, but since it is inherited from
+--  Gtk_Misc, it can be padded and/or aligned, to fill exactly the space you
+--  desire.
+--
+--  Arrows are created with a call to Gtk_New. The direction or style of an
+--  arrow can be changed after creation by using Set.
+--  </description>
+--  <c_version>1.2.8</c_version>
 
 with Gtk.Enums; use Gtk.Enums;
 with Gtk.Object;
@@ -38,27 +53,47 @@ package Gtk.Arrow is
    type Gtk_Arrow is access all Gtk_Arrow_Record'Class;
 
    procedure Gtk_New
-      (Arrow       : out Gtk_Arrow;
-       Arrow_Type  : in Gtk_Arrow_Type;
-       Shadow_Type : in Gtk_Shadow_Type);
+     (Arrow       : out Gtk_Arrow;
+      Arrow_Type  : in Gtk_Arrow_Type;
+      Shadow_Type : in Gtk_Shadow_Type);
+   --  Create a new arrow widget.
+
    procedure Initialize
-      (Arrow       : access Gtk_Arrow_Record'Class;
-       Arrow_Type  : in Gtk_Arrow_Type;
-       Shadow_Type : in Gtk_Shadow_Type);
+     (Arrow       : access Gtk_Arrow_Record'Class;
+      Arrow_Type  : in Gtk_Arrow_Type;
+      Shadow_Type : in Gtk_Shadow_Type);
+   --  Internal initialization function.
+   --  See the section "Creating your own widgets" in the documentation.
+
+   function Get_Type return Gtk.Gtk_Type;
+   --  Return the internal value associated with a Gtk_Arrow.
+
    procedure Set
-      (Arrow       : access Gtk_Arrow_Record;
-       Arrow_Type  : in Gtk_Arrow_Type;
-       Shadow_Type : in Gtk_Shadow_Type);
+     (Arrow       : access Gtk_Arrow_Record;
+      Arrow_Type  : in Gtk_Arrow_Type;
+      Shadow_Type : in Gtk_Shadow_Type);
+   --  Set the direction and style of the Arrow.
 
-   --  The two following procedures are used to generate and create widgets
-   --  from a Node.
+   ----------------------------
+   -- Support for Gate/Dgate --
+   ----------------------------
 
-   procedure Generate (N      : in Node_Ptr;
-                       File   : in File_Type);
+   procedure Generate (N : in Node_Ptr; File : in File_Type);
+   --  Gate internal function
 
-   procedure Generate (Arrow  : in out Gtk.Object.Gtk_Object; N : in Node_Ptr);
+   procedure Generate (Arrow : in out Gtk.Object.Gtk_Object; N : in Node_Ptr);
+   --  Dgate internal function
+
+   -------------
+   -- Signals --
+   -------------
+
+   --  <signals>
+   --  The following new signals are defined for this widget:
+   --  </signals>
 
 private
    type Gtk_Arrow_Record is new Gtk.Misc.Gtk_Misc_Record with null record;
 
+   pragma Import (C, Get_Type, "gtk_arrow_get_type");
 end Gtk.Arrow;
