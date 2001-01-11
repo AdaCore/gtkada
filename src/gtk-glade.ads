cfe5b1a331a38b00c84cbe451a0b9faf47bd8427
Arnaud Charlet <charlet@adacore.com>
2001-01-11 11:38:01 +0000

    Change header since these are part of Gate, and therefore are not subject to  the special exception to the GPL (using plain GPL instead).

--- src/gtk-glade.ads
+++ src/gtk-glade.ads
@@ -1,38 +1,26 @@
 -----------------------------------------------------------------------
---          GtkAda - Ada95 binding for the Gimp Toolkit              --
+--                   Gate - GtkAda Components                        --
 --                                                                   --
---                     Copyright (C) 1999-2000                       --
+--                   Copyright (C) 1999-2001                         --
 --        Emmanuel Briot, Joel Brobecker and Arnaud Charlet          --
 --                                                                   --
--- This library is free software; you can redistribute it and/or     --
--- modify it under the terms of the GNU General Public               --
--- License as published by the Free Software Foundation; either      --
--- version 2 of the License, or (at your option) any later version.  --
+-- GATE is free software;  you can redistribute it and/or modify  it --
+-- under the terms of the GNU General Public License as published by --
+-- the Free Software Foundation; either version 2 of the License, or --
+-- (at your option) any later version.                               --
 --                                                                   --
--- This library is distributed in the hope that it will be useful,   --
--- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- This program is  distributed in the hope that it will be  useful, --
+-- but  WITHOUT ANY WARRANTY;  without even the  implied warranty of --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
--- General Public License for more details.                          --
---                                                                   --
--- You should have received a copy of the GNU General Public         --
--- License along with this library; if not, write to the             --
--- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
--- Boston, MA 02111-1307, USA.                                       --
---                                                                   --
--- As a special exception, if other files instantiate generics from  --
--- this unit, or you link this unit with other files to produce an   --
--- executable, this  unit  does not  by itself cause  the resulting  --
--- executable to be covered by the GNU General Public License. This  --
--- exception does not however invalidate any other reasons why the   --
--- executable file  might be covered by the  GNU Public License.     --
+-- General Public License for more details. You should have received --
+-- a copy of the GNU General Public License along with this library; --
+-- if not,  write to the  Free Software Foundation, Inc.,  59 Temple --
+-- Place - Suite 330, Boston, MA 02111-1307, USA.                    --
 -----------------------------------------------------------------------
 
---  <description>
 --  This package contains high level support for the Glade GUI builder.
---  It provides routines to generate Ada code and create widgets dynamically
---  from an XML definition file.
---  @pxref{Package_Glib.Glade} for the low level GUI builder support.
---  </description>
+--  It provides routines to generate Ada code from an XML definition file.
+--  See package Glib.Glade for the low level GUI builder support.
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Glib.Glade; use Glib.Glade; use Glib.Glade.Glib_XML;
