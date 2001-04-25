66e84e4e824913522c4ca3ec5a54d2ef66d3e88b
Joël Brobecker <brobecke@adacore.com>
2001-04-25 08:41:22 +0000

    (Root_Type): Removed, replaced by Glib.GObjects.GObject_Record. (Root_Type_Access): Replaced by Glib.GObjects.GObject. Moved Root_Type primitives and related subprograms inside Glib.GObjects. private entities related to Root_Type also moved to Glib.GObjects.

--- src/gtk.ads
+++ src/gtk.ads
@@ -30,26 +30,21 @@
 --  <description>
 --
 --  This package provides some basic Gtk+ functionalities such as getting the
---  version number. This is the top level package of the Gtk widget hierarchy.
---  For general GtkAda initializations, @pxref{Package_Gtk.Main}.
+--  version number.  For general GtkAda initializations,
+--  @pxref{Package_Gtk.Main}.
 --
 --  </description>
 --  <c_version>1.2.7</c_version>
 
 with Glib;                use Glib;
 with Gdk;                 use Gdk;
+
+pragma Warnings (Off);
+with Glib.GObjects;       use Glib.GObjects;
 with System;
+pragma Warnings (On);
 
 package Gtk is
-
-   type Root_Type is tagged private;
-   --  The base type of the hierarchy in GtkAda. It basically gives access
-   --  to an underlying C object. This is not a controlled type, for efficiency
-   --  reasons, and because gtk+ takes care of memory management on its own.
-
-   --  <doc_ignore>
-   type Root_Type_Access is access all Root_Type'Class;
-   --  </doc_ignore>
 
    type Gtk_Rc_Style is new Gdk.C_Proxy;
    --  Type used to handle resource styles.
@@ -80,54 +75,12 @@ package Gtk is
    --  It can contain a single child, and is also associated with a tab
    --  label used to select that page in the notebook.
 
-   function Type_Name (Type_Num : in Gtk_Type) return String;
-   --  Return the type name corresponding to a Gtk_Type.
-   --  This might be useful in debug messages.
-
-   function Type_From_Name (Name : in String) return Gtk_Type;
-   --  Convert a string to the matching type.
-   --  Name should be the C widget's name, such as GtkScrollbar or GtkButton,
-   --  rather than the Ada name.
-
-   function Is_Created (Object : in Root_Type'Class) return Boolean;
-   --  Return True if the associated C object has been created, False if no
-   --  C object is associated with Object.
-   --  This is not the same as testing whether an access type (for instance
-   --  any of the widgets) is "null", since this relates to the underlying
-   --  C object.
-
    ------------------------
    -- Interfacing with C --
    ------------------------
    --  The following functions are made public so that one can easily create
    --  new widgets outside the Gtk package hierarchy.
    --  Only experienced users should make use of these functions.
-
-   function Get_Object (Object : access Root_Type'Class) return System.Address;
-   --  Access the underlying C pointer.
-
-   procedure Set_Object
-     (Object : access Root_Type'Class;
-      Value  : in     System.Address);
-   --  Modify the underlying C pointer.
-
-   procedure Initialize_User_Data (Obj : access Root_Type'Class);
-   --  Sets a user data field for the C object associated with Obj.
-   --  This field will be used so that it is possible, knowing a
-   --  C object, to get the full ada object.
-
-   function Get_User_Data
-     (Obj  : in System.Address;
-      Stub : in Root_Type'Class) return Root_Type_Access;
-   --  Get the user data that was set by GtkAda.
-   --  If the Data is not set, return a new access type, that points to
-   --  a structure with the same tag as Stub.
-
-   function Unchecked_Cast
-     (Obj  : access Root_Type'Class;
-      Stub : Root_Type'Class) return Root_Type_Access;
-   --  Cast Obj in an object of tag Stub'Class.
-   --  Return the resulting object and free the memory pointed by Obj.
 
    function Count_Arguments
      (The_Type : Gtk_Type; Name : in String) return Guint;
@@ -138,6 +91,9 @@ package Gtk is
    --  extra parameters passed by gtk+ will simply be ignored), whereas having
    --  more arguments is impossible (they would never be set).
    --  Note that we provide this procedure here to avoid circularities.
+   --  <doc_ignore>
+   --  ??? This function should probably be moved to Glib.Gobjects
+   --  </doc_ignore>
 
    function Argument_Type
      (The_Type : Gtk_Type;
@@ -147,40 +103,14 @@ package Gtk is
    --  If Num is negative, return the type returned by the handlers for this
    --  signal.
    --  Note that we provide this procedure here to avoid circularities.
-
-private
-
-   type Root_Type is tagged record
-      Ptr : System.Address := System.Null_Address;
-   end record;
-
    --  <doc_ignore>
-
-   --  Note: the following functions and types should only be used
-   --  for internal usage, not in the user's applications.
-   --  If you use type inheritance for new widgets, you should not need
-   --  these functions.
-
-   GtkAda_String : constant String := "_GtkAda" & ASCII.NUL;
-   GtkAda_String_Quark : Glib.GQuark := Glib.Unknown_Quark;
-   --  The name for the user data that we set in the objects.
-   --  The Quark version is to speed up the string lookup (this is done
-   --  only once).
-
-   function Conversion_Function
-     (Obj : System.Address; Stub : Root_Type'Class) return Root_Type_Access;
-   --  This function has to convert a C object to an Ada object.
-   --  It will first try all the registered functions (in
-   --  Gtk.Type_Conversion_Hooks) and by default, will create a Stub'Class
-   --  object, no matter what the real C type is.
-   --  Stub is the expected type.
-
+   --  ??? This function should probably be moved to Glib.Gobjects
    --  </doc_ignore>
 
-   pragma Inline (Get_Object);
-   pragma Inline (Set_Object);
+private
 
    pragma Import (C, Major_Version, "ada_gtk_major_version");
    pragma Import (C, Minor_Version, "ada_gtk_minor_version");
    pragma Import (C, Micro_Version, "ada_gtk_micro_version");
+
 end Gtk;
