29276922c3d01615c602a4abf948d4a1d785d5f7
Arnaud Charlet <charlet@adacore.com>
2001-05-14 18:12:10 +0000

    (Set_Selection_Mode): Remove explicit "in" parameter. Remove use of 'Pos.

--- src/gtk-list.adb
+++ src/gtk-list.adb
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------
---          GtkAda - Ada95 binding for the Gimp Toolkit              --
+--               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
---                     Copyright (C) 1998-2000                       --
---        Emmanuel Briot, Joel Brobecker and Arnaud Charlet          --
+--   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
+--                Copyright (C) 2000-2001 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -258,15 +258,13 @@ package body Gtk.List is
 
    procedure Set_Selection_Mode
       (List : access Gtk_List_Record;
-       Mode : in Gtk_Selection_Mode)
+       Mode : Gtk_Selection_Mode)
    is
-      procedure Internal
-         (List : in System.Address;
-          Mode : in Gint);
+      procedure Internal (List : System.Address; Mode : Gtk_Selection_Mode);
       pragma Import (C, Internal, "gtk_list_set_selection_mode");
+
    begin
-      Internal (Get_Object (List),
-                Gtk_Selection_Mode'Pos (Mode));
+      Internal (Get_Object (List), Mode);
    end Set_Selection_Mode;
 
    --------------------
