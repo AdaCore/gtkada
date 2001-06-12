554d8536dc5dfc7c7d4770ba47106a828c03dae9
Joël Brobecker <brobecke@adacore.com>
2001-06-12 13:07:39 +0000

    Reorder all the enumerated types by alphabetical order.

--- src/gtk-enums.ads
+++ src/gtk-enums.ads
@@ -35,16 +35,13 @@ with System;
 
 package Gtk.Enums is
 
-   type Gtk_State_Type is
-     (State_Normal,
-      State_Active,
-      State_Prelight,
-      State_Selected,
-      State_Insensitive);
-   for Gtk_State_Type'Size use Gint'Size;
+   type Gtk_Arrow_Type is (Arrow_Up, Arrow_Down, Arrow_Left, Arrow_Right);
+   for Gtk_Arrow_Type'Size use Gint'Size;
 
-   type Gtk_Window_Type is (Window_Toplevel, Window_Dialog, Window_Popup);
-   for Gtk_Window_Type'Size use Gint'Size;
+   type Gtk_Attach_Options is new Glib.Guint32;
+   Expand : constant Gtk_Attach_Options := 1;
+   Shrink : constant Gtk_Attach_Options := 2;
+   Fill   : constant Gtk_Attach_Options := 4;
 
    type Gtk_Button_Action is new Guint;
    Button_Ignored : constant Gtk_Button_Action := 0;
@@ -60,35 +57,6 @@ package Gtk.Enums is
       Buttonbox_End);
    for Gtk_Button_Box_Style'Size use Gint'Size;
 
-   type Gtk_Direction_Type is
-     (Dir_Tab_Forward,
-      Dir_Tab_Backward,
-      Dir_Up,
-      Dir_Down,
-      Dir_Left,
-      Dir_Right);
-   for Gtk_Direction_Type'Size use Gint'Size;
-
-   type Gtk_Icon_Size is
-     (Icon_Size_Invalid,
-      Icon_Size_Menu,
-      Icon_Size_Small_Toolbar,
-      Icon_Size_Large_Toolbar,
-      Icon_Size_Button,
-      Icon_Size_Dialog);
-   for Gtk_Icon_Size'Size use Gint'Size;
-
-   type Gtk_Shadow_Type is
-     (Shadow_None,
-      Shadow_In,
-      Shadow_Out,
-      Shadow_Etched_In,
-      Shadow_Etched_Out);
-   for Gtk_Shadow_Type'Size use Gint'Size;
-
-   type Gtk_Arrow_Type is (Arrow_Up, Arrow_Down, Arrow_Left, Arrow_Right);
-   for Gtk_Arrow_Type'Size use Gint'Size;
-
    type Gtk_Cell_Type is
      (Cell_Empty,
       Cell_Text,
@@ -96,119 +64,6 @@ package Gtk.Enums is
       Cell_Pixtext,
       Cell_Widget);
    for Gtk_Cell_Type'Size use Gint'Size;
-
-   type Gtk_Pack_Type is (Pack_Start, Pack_End);
-   for Gtk_Pack_Type'Size use Gint'Size;
-
-   type Gtk_Policy_Type is (Policy_Always, Policy_Automatic, Policy_Never);
-   for Gtk_Policy_Type'Size use Gint'Size;
-
-   type Gtk_Update_Type is
-     (Update_Continuous,
-      Update_Discontinuous,
-      Update_Delayed);
-   for Gtk_Update_Type'Size use Gint'Size;
-
-   type Gtk_Relief_Style is (Relief_Normal, Relief_Half, Relief_None);
-   for Gtk_Relief_Style'Size use Gint'Size;
-
-   type Gtk_Attach_Options is new Glib.Guint32;
-   Expand : constant Gtk_Attach_Options := 1;
-   Shrink : constant Gtk_Attach_Options := 2;
-   Fill   : constant Gtk_Attach_Options := 4;
-
-   type Gtk_Window_Position is
-     (Win_Pos_None,
-      Win_Pos_Center,
-      Win_Pos_Mouse,
-      Win_Pos_Center_Always);
-   for Gtk_Window_Position'Size use Gint'Size;
-
-   type Gtk_Submenu_Direction is (Direction_Left, Direction_Right);
-   for Gtk_Submenu_Direction'Size use Gint'Size;
-
-   type Gtk_Submenu_Placement is (Top_Bottom, Left_Right);
-   for Gtk_Submenu_Placement'Size use Gint'Size;
-
-   type Gtk_Resize_Mode is
-     (Resize_Parent,     --  Pass request to the parent
-      Resize_Queue,      --  Queue resizes on this widget
-      Resize_Immediate); --  Perform the resizes now
-   for Gtk_Resize_Mode'Size use Gint'Size;
-
-   type Gtk_Metric_Type is (Pixels, Inches, Centimeters);
-   for Gtk_Metric_Type'Size use Gint'Size;
-
-   type Gtk_Scroll_Type is
-     (Scroll_None,
-      Scroll_Step_Backward,
-      Scroll_Step_Forward,
-      Scroll_Page_Backward,
-      Scroll_Page_Forward,
-      Scroll_Jump);
-   for Gtk_Scroll_Type'Size use Gint'Size;
-
-   type Gtk_Trough_Type is
-     (Trough_None,
-      Trough_Start,
-      Trough_End,
-      Trough_Jump);
-   for Gtk_Trough_Type'Size use Gint'Size;
-
-   type Gtk_Position_Type is
-     (Pos_Left,
-      Pos_Right,
-      Pos_Top,
-      Pos_Bottom);
-   for Gtk_Position_Type'Size use Gint'Size;
-
-   type Gtk_Preview_Type is (Preview_Color, Preview_Grayscale);
-   for Gtk_Preview_Type'Size use Gint'Size;
-
-   type Gtk_Justification is
-     (Justify_Left,
-      Justify_Right,
-      Justify_Center,
-      Justify_Fill);
-   for Gtk_Justification'Size use Gint'Size;
-
-   type Gtk_Selection_Mode is
-     (Selection_Single,
-      Selection_Browse,
-      Selection_Multiple,
-      Selection_Extended);
-   for Gtk_Selection_Mode'Size use Gint'Size;
-
-   type Gtk_Orientation is (Orientation_Horizontal, Orientation_Vertical);
-   for Gtk_Orientation'Size use Gint'Size;
-
-   type Gtk_Toolbar_Child_Type is
-     (Toolbar_Child_Space,
-      Toolbar_Child_Button,
-      Toolbar_Child_Togglebutton,
-      Toolbar_Child_Radiobutton,
-      Toolbar_Child_Widget);
-   for Gtk_Toolbar_Child_Type'Size use Gint'Size;
-
-   type Gtk_Toolbar_Style is (Toolbar_Icons, Toolbar_Text, Toolbar_Both);
-   for Gtk_Toolbar_Style'Size use Gint'Size;
-
-   type Gtk_Toolbar_Space_Style is (Toolbar_Space_Empty, Toolbar_Space_Line);
-   for Gtk_Toolbar_Space_Style'Size use Gint'Size;
-
-   type Gtk_Tree_View_Mode is (Tree_View_Line, Tree_View_Item);
-   for Gtk_Tree_View_Mode'Size use Gint'Size;
-
-   type Gtk_Visibility is
-     (Visibility_None, Visibility_Partial, Visibility_Full);
-   for Gtk_Visibility'Size use Gint'Size;
-
-   type Gtk_Progress_Bar_Orientation is
-     (Progress_Left_To_Right,
-      Progress_Right_To_Left,
-      Progress_Bottom_To_Top,
-      Progress_Top_To_Bottom);
-   for Gtk_Progress_Bar_Orientation'Size use Gint'Size;
 
    type Gtk_Ctree_Line_Style is
      (Ctree_Lines_None,
@@ -243,11 +98,106 @@ package Gtk.Enums is
    --  See Gtk.Ctree.Set_Expander_Style for more details.
    for Gtk_Ctree_Expander_Style'Size use Gint'Size;
 
-   type Gtk_Wrap_Mode is
-     (Wrapmode_None,
-      Wrapmode_Char,
-      Wrapmode_Word);
-   for Gtk_Wrap_Mode'Size use Gint'Size;
+   type Gtk_Direction_Type is
+     (Dir_Tab_Forward,
+      Dir_Tab_Backward,
+      Dir_Up,
+      Dir_Down,
+      Dir_Left,
+      Dir_Right);
+   for Gtk_Direction_Type'Size use Gint'Size;
+
+   type Gtk_Icon_Size is
+     (Icon_Size_Invalid,
+      Icon_Size_Menu,
+      Icon_Size_Small_Toolbar,
+      Icon_Size_Large_Toolbar,
+      Icon_Size_Button,
+      Icon_Size_Dialog);
+   for Gtk_Icon_Size'Size use Gint'Size;
+
+   type Gtk_Justification is
+     (Justify_Left,
+      Justify_Right,
+      Justify_Center,
+      Justify_Fill);
+   for Gtk_Justification'Size use Gint'Size;
+
+   type Gtk_Metric_Type is (Pixels, Inches, Centimeters);
+   for Gtk_Metric_Type'Size use Gint'Size;
+
+   type Gtk_Orientation is (Orientation_Horizontal, Orientation_Vertical);
+   for Gtk_Orientation'Size use Gint'Size;
+
+   type Gtk_Pack_Type is (Pack_Start, Pack_End);
+   for Gtk_Pack_Type'Size use Gint'Size;
+
+   type Gtk_Policy_Type is (Policy_Always, Policy_Automatic, Policy_Never);
+   for Gtk_Policy_Type'Size use Gint'Size;
+
+   type Gtk_Position_Type is
+     (Pos_Left,
+      Pos_Right,
+      Pos_Top,
+      Pos_Bottom);
+   for Gtk_Position_Type'Size use Gint'Size;
+
+   type Gtk_Preview_Type is (Preview_Color, Preview_Grayscale);
+   for Gtk_Preview_Type'Size use Gint'Size;
+
+   type Gtk_Progress_Bar_Orientation is
+     (Progress_Left_To_Right,
+      Progress_Right_To_Left,
+      Progress_Bottom_To_Top,
+      Progress_Top_To_Bottom);
+   for Gtk_Progress_Bar_Orientation'Size use Gint'Size;
+
+   type Gtk_Relief_Style is (Relief_Normal, Relief_Half, Relief_None);
+   for Gtk_Relief_Style'Size use Gint'Size;
+
+   type Gtk_Resize_Mode is
+     (Resize_Parent,     --  Pass request to the parent
+      Resize_Queue,      --  Queue resizes on this widget
+      Resize_Immediate); --  Perform the resizes now
+   for Gtk_Resize_Mode'Size use Gint'Size;
+
+   type Gtk_Scroll_Type is
+     (Scroll_None,
+      Scroll_Step_Backward,
+      Scroll_Step_Forward,
+      Scroll_Page_Backward,
+      Scroll_Page_Forward,
+      Scroll_Jump);
+   for Gtk_Scroll_Type'Size use Gint'Size;
+
+   type Gtk_Selection_Mode is
+     (Selection_Single,
+      Selection_Browse,
+      Selection_Multiple,
+      Selection_Extended);
+   for Gtk_Selection_Mode'Size use Gint'Size;
+
+   type Gtk_Shadow_Type is
+     (Shadow_None,
+      Shadow_In,
+      Shadow_Out,
+      Shadow_Etched_In,
+      Shadow_Etched_Out);
+   for Gtk_Shadow_Type'Size use Gint'Size;
+
+   type Gtk_State_Type is
+     (State_Normal,
+      State_Active,
+      State_Prelight,
+      State_Selected,
+      State_Insensitive);
+   for Gtk_State_Type'Size use Gint'Size;
+
+   type Gtk_Submenu_Direction is (Direction_Left, Direction_Right);
+   for Gtk_Submenu_Direction'Size use Gint'Size;
+
+   type Gtk_Submenu_Placement is (Top_Bottom, Left_Right);
+   for Gtk_Submenu_Placement'Size use Gint'Size;
 
    type Gtk_Text_Window_Type is
      (Text_Window_Private,
@@ -258,6 +208,56 @@ package Gtk.Enums is
       Text_Window_Top,
       Text_Window_Bottom);
    for Gtk_Text_Window_Type'Size use Gint'Size;
+
+   type Gtk_Toolbar_Child_Type is
+     (Toolbar_Child_Space,
+      Toolbar_Child_Button,
+      Toolbar_Child_Togglebutton,
+      Toolbar_Child_Radiobutton,
+      Toolbar_Child_Widget);
+   for Gtk_Toolbar_Child_Type'Size use Gint'Size;
+
+   type Gtk_Toolbar_Style is (Toolbar_Icons, Toolbar_Text, Toolbar_Both);
+   for Gtk_Toolbar_Style'Size use Gint'Size;
+
+   type Gtk_Toolbar_Space_Style is (Toolbar_Space_Empty, Toolbar_Space_Line);
+   for Gtk_Toolbar_Space_Style'Size use Gint'Size;
+
+   type Gtk_Tree_View_Mode is (Tree_View_Line, Tree_View_Item);
+   for Gtk_Tree_View_Mode'Size use Gint'Size;
+
+   type Gtk_Trough_Type is
+     (Trough_None,
+      Trough_Start,
+      Trough_End,
+      Trough_Jump);
+   for Gtk_Trough_Type'Size use Gint'Size;
+
+   type Gtk_Update_Type is
+     (Update_Continuous,
+      Update_Discontinuous,
+      Update_Delayed);
+   for Gtk_Update_Type'Size use Gint'Size;
+
+   type Gtk_Visibility is
+     (Visibility_None, Visibility_Partial, Visibility_Full);
+   for Gtk_Visibility'Size use Gint'Size;
+
+   type Gtk_Window_Position is
+     (Win_Pos_None,
+      Win_Pos_Center,
+      Win_Pos_Mouse,
+      Win_Pos_Center_Always);
+   for Gtk_Window_Position'Size use Gint'Size;
+
+   type Gtk_Window_Type is (Window_Toplevel, Window_Dialog, Window_Popup);
+   for Gtk_Window_Type'Size use Gint'Size;
+
+   type Gtk_Wrap_Mode is
+     (Wrapmode_None,
+      Wrapmode_Char,
+      Wrapmode_Word);
+   for Gtk_Wrap_Mode'Size use Gint'Size;
 
    --  Some Glib instantiations
 
