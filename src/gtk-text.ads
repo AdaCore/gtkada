2b2a3edb806ab2a8a96177ac061ca113f12c3b7e
Emmanuel Briot <briot@adacore.com>
2001-07-12 14:50:54 +0000

    Fix copyright notice (dates)

--- src/gtk-text.ads
+++ src/gtk-text.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --          GtkAda - Ada95 binding for the Gimp Toolkit              --
 --                                                                   --
---                     Copyright (C) 1998-1999                       --
+--                     Copyright (C) 1998-2001                       --
 --        Emmanuel Briot, Joel Brobecker and Arnaud Charlet          --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
