93882ac42d187b00c9cf1dfab4e5420ca9125d1a
Nicolas Setton <setton@adacore.com>
2001-11-13 16:57:09 +0000

    Initial revision.

---
+++ src/gtk-tree_store.adb
@@ -0,0 +1,317 @@
+-----------------------------------------------------------------------
+--              GtkAda - Ada95 binding for Gtk+/Gnome                --
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                         ACT-Europe                                --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+with Gtk.Tree_Model;
+with Gtk; use Gtk;
+with System;
+
+package body Gtk.Tree_Store is
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New
+     (Widget    : out Gtk_Tree_Store;
+      N_Columns : Gint;
+      Types     : GType_Array)
+   is
+   begin
+      Widget := new Gtk_Tree_Store_Record;
+      Initialize (Widget, N_Columns, Types);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize
+     (Widget    : access Gtk_Tree_Store_Record'Class;
+      N_Columns : Gint;
+      Types     : GType_Array)
+   is
+      function Internal
+        (N_Columns : Gint;
+         Types     : System.Address)
+         return System.Address;
+      pragma Import (C, Internal, "gtk_tree_store_newv");
+
+      Temp_Array : GType_Array (1 .. Types'Length + 1);
+   begin
+      Temp_Array (1 .. Types'Length) := Types;
+      Temp_Array (Temp_Array'Last) := GType_Invalid;
+
+      Set_Object (Widget, Internal (N_Columns,
+                                    Temp_Array (Temp_Array'First)'Address));
+      Initialize_User_Data (Widget);
+   end Initialize;
+
+   ---------------
+   -- Set_Value --
+   ---------------
+
+   procedure Set_Value
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Column     : Gint;
+      Value      : System.Address)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Column     : Gint;
+         Value      : System.Address);
+      pragma Import (C, Internal, "ada_gtk_tree_store_set_value");
+   begin
+      Internal (Get_Object (Tree_Store),
+                Iter'Address,
+                Column,
+                Value);
+   end Set_Value;
+
+   ---------------
+   -- Set_Value --
+   ---------------
+
+   procedure Set_Value
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Column     : Gint;
+      Value      : in out Glib.Values.GValue)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Column     : Gint;
+         Value      : System.Address);
+      pragma Import (C, Internal, "gtk_tree_store_set_value");
+   begin
+      Internal (Get_Object (Tree_Store),
+                Iter'Address,
+                Column,
+                Value'Address);
+   end Set_Value;
+
+   ------------
+   -- Remove --
+   ------------
+
+   procedure Remove
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address);
+      pragma Import (C, Internal, "gtk_tree_store_remove");
+   begin
+      Internal (Get_Object (Tree_Store),
+                Iter'Address);
+   end Remove;
+
+   ------------
+   -- Insert --
+   ------------
+
+   procedure Insert
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Position   : Gint)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Parent     : System.Address;
+         Position   : Gint);
+      pragma Import (C, Internal, "gtk_tree_store_insert");
+   begin
+      Internal (Get_Object (Tree_Store),
+                Iter'Address,
+                Parent'Address,
+                Position);
+   end Insert;
+
+   -------------------
+   -- Insert_Before --
+   -------------------
+
+   procedure Insert_Before
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Sibling    : Gtk.Tree_Model.Gtk_Tree_Iter)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Parent     : System.Address;
+         Sibling    : System.Address);
+      pragma Import (C, Internal, "gtk_tree_store_insert_before");
+   begin
+      Internal (Get_Object (Tree_Store),
+                Iter'Address,
+                Parent'Address,
+                Sibling'Address);
+   end Insert_Before;
+
+   ------------------
+   -- Insert_After --
+   ------------------
+
+   procedure Insert_After
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Sibling    : Gtk.Tree_Model.Gtk_Tree_Iter)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Parent     : System.Address;
+         Sibling    : System.Address);
+      pragma Import (C, Internal, "gtk_tree_store_insert_after");
+   begin
+      Internal (Get_Object (Tree_Store),
+                Iter'Address,
+                Parent'Address,
+                Sibling'Address);
+   end Insert_After;
+
+   -------------
+   -- Prepend --
+   -------------
+
+   procedure Prepend
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Parent     : System.Address);
+      pragma Import (C, Internal, "gtk_tree_store_prepend");
+   begin
+      Internal (Get_Object (Tree_Store),
+                Iter'Address,
+                Parent'Address);
+   end Prepend;
+
+   ------------
+   -- Append --
+   ------------
+
+   procedure Append
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter)
+   is
+      procedure Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Parent     : System.Address);
+      pragma Import (C, Internal, "gtk_tree_store_append");
+
+      Iter_Address   : System.Address;
+      Parent_Address : System.Address;
+
+      use Gtk.Tree_Model;
+   begin
+      if Iter = Null_Iter then
+         Iter_Address := System.Null_Address;
+      else
+         Iter_Address := Iter'Address;
+      end if;
+
+      if Parent = Null_Iter then
+         Parent_Address := System.Null_Address;
+      else
+         Parent_Address := Parent'Address;
+      end if;
+
+      Internal (Get_Object (Tree_Store),
+                Iter_Address,
+                Parent_Address);
+   end Append;
+
+   -----------------
+   -- Is_Ancestor --
+   -----------------
+
+   function Is_Ancestor
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Descendant : Gtk.Tree_Model.Gtk_Tree_Iter)
+      return Boolean
+   is
+      function Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address;
+         Descendant : System.Address)
+         return Gint;
+      pragma Import (C, Internal, "gtk_tree_store_is_ancestor");
+   begin
+      return Boolean'Val (Internal (Get_Object (Tree_Store),
+                                    Iter'Address,
+                                    Descendant'Address));
+   end Is_Ancestor;
+
+   ----------------
+   -- Iter_Depth --
+   ----------------
+
+   function Iter_Depth
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter)
+      return Gint
+   is
+      function Internal
+        (Tree_Store : System.Address;
+         Iter       : System.Address)
+         return Gint;
+      pragma Import (C, Internal, "gtk_tree_store_iter_depth");
+   begin
+      return Internal (Get_Object (Tree_Store),
+                       Iter'Address);
+   end Iter_Depth;
+
+   -----------
+   -- Clear --
+   -----------
+
+   procedure Clear (Tree_Store : access Gtk_Tree_Store_Record)
+   is
+      procedure Internal (Tree_Store : System.Address);
+      pragma Import (C, Internal, "gtk_tree_store_clear");
+   begin
+      Internal (Get_Object (Tree_Store));
+   end Clear;
+
+end Gtk.Tree_Store;
---
+++ src/gtk-tree_store.ads
@@ -0,0 +1,170 @@
+-----------------------------------------------------------------------
+--              GtkAda - Ada95 binding for Gtk+/Gnome                --
+--                                                                   --
+--                     Copyright (C) 2001                            --
+--                         ACT-Europe                                --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+with Glib.Values; use Glib.Values;
+with Gtk;
+with Gtk.Tree_Model;
+
+package Gtk.Tree_Store is
+
+   type Gtk_Tree_Store_Record is
+     new Gtk.Tree_Model.Gtk_Tree_Model_Record with private;
+   type Gtk_Tree_Store is access all Gtk_Tree_Store_Record'Class;
+
+   procedure Gtk_New
+     (Widget    : out Gtk_Tree_Store;
+      N_Columns : Gint;
+      Types     : GType_Array);
+   --  Creates a new tree store as with N_Columns columns each of
+   --  the types passed in.
+
+   procedure Initialize
+     (Widget    : access Gtk_Tree_Store_Record'Class;
+      N_Columns : Gint;
+      Types     : GType_Array);
+   --  Internal initialization function.
+   --  See the section "Creating your own widgets" in the documentation.
+
+   function Get_Type return Gtk.Gtk_Type;
+   --  Return the internal value associated with this widget.
+
+   procedure Set_Value
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Column     : Gint;
+      Value      : in out Glib.Values.GValue);
+
+   procedure Set_Value
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Column     : Gint;
+      Value      : System.Address);
+
+   procedure Remove
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter);
+   --  Removes Iter from Tree_Store.  After being removed, Iter is set to
+   --  the next valid row at that level, or invalidated if it previeously
+   --  pointed to the last one.
+
+   procedure Insert
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Position   : Gint);
+   --  Creates a new row at Position.  If parent is non-null, then the row
+   --  will be made a child of Parent. Otherwise, the row will be created at
+   --  the toplevel. If Position is larger than the number of rows at that
+   --  level, then the new row will be inserted to the end of the list.  Iter
+   --  will be changed to point to this new row.  The row will be empty before
+   --  this function is called.  To fill in values, you need to call Set_Value.
+
+   procedure Insert_Before
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Sibling    : Gtk.Tree_Model.Gtk_Tree_Iter);
+   --  Inserts a new row before Sibling.  If Sibling is Null_Iter, then the
+   --  row will be appended to the beginning of the Parent 's children.
+   --  If Parent and Sibling are Null_Iter, then the row will be appended to
+   --  the toplevel.  If both Sibling and Parent are set, then Parent must
+   --  be the parent of Sibling. When Sibling is set, Parent is optional.
+   --  Iter will be changed to point to this new row.  The row will be empty
+   --  after this function is called.  To fill in values, you need to call
+   --  Set_Value.
+
+   procedure Insert_After
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Sibling    : Gtk.Tree_Model.Gtk_Tree_Iter);
+   --  Inserts a new row after Sibling.  If Sibling is Null_Iter, then the
+   --  row will be prepended to the beginning of the Parent 's children.
+   --  If Parent and Sibling are Null_Iter, then the row will be prepended
+   --  to the toplevel.  If both Sibling and Parent are set, then Parent
+   --  must be the parent of Sibling When Sibling is set, Parent is optional.
+   --  Iter will be changed to point to this new row.  The row will be empty
+   --  after this function is called.  To fill in values, you need to call
+   --  Set_Value.
+
+   procedure Prepend
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter);
+   --  Prepends a new row to Tree_Store.  If Parent is non-NULL, then it
+   --  will prepend the new row before the first child of Parent, otherwise
+   --  it will prepend a row to the top level.  Iter will be changed to point
+   --  to this new row.  The row will be empty after this function is called.
+   --  To fill in values, you need to call Set_Value.
+
+   procedure Append
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Parent     : Gtk.Tree_Model.Gtk_Tree_Iter);
+   --  Appends a new row to Tree_Store.  If Parent is non-NULL, then it will
+   --  append the new row after the last child of Parent, otherwise it will
+   --  append a row to the top level.  Iter will be changed to point to this
+   --  new row.  The row will be empty after this function is called.  To
+   --  fill in values, you need to call Set_Value.
+
+   function Is_Ancestor
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter;
+      Descendant : Gtk.Tree_Model.Gtk_Tree_Iter)
+      return Boolean;
+   --  Returns True if Iter is an ancestor of Descendant.  That is, Iter is the
+   --  parent (or grandparent or great-grandparent) of Descendant.
+
+   function Iter_Depth
+     (Tree_Store : access Gtk_Tree_Store_Record;
+      Iter       : Gtk.Tree_Model.Gtk_Tree_Iter)
+     return Gint;
+   --  Returns the depth of Iter.  This will be 0 for anything on the root level,
+   --  1 for anything down a level, etc.
+
+   procedure Clear (Tree_Store : access Gtk_Tree_Store_Record);
+   --  Removes all rows from Tree_Store
+
+   -------------
+   -- Signals --
+   -------------
+
+   --  <signals>
+   --  The following new signals are defined for this widget:
+   --
+   --  </signals>
+
+private
+   type Gtk_Tree_Store_Record is
+     new Gtk.Tree_Model.Gtk_Tree_Model_Record with null record;
+
+   pragma Import (C, Get_Type, "gtk_tree_store_get_type");
+end Gtk.Tree_Store;
+
+--  ??? Missing : drag-and-drop stuff
