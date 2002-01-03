7c6cef978b1a28404314cbb7ee44b9249134ed33
Arnaud Charlet <charlet@adacore.com>
2002-01-03 09:09:45 +0000

    Update to Gtk+ 1.3.11 Replace explicit Size clause by more appropriate pragma Convention C.

--- src/gtk-enums.ads
+++ src/gtk-enums.ads
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2001 ACT-Europe                 --
+--                Copyright (C) 2000-2002 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -27,6 +27,8 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
+--  <c_version>1.3.11</c_version>
+
 with Glib;
 with Glib.Generic_Properties;  use Glib.Generic_Properties;
 pragma Elaborate_All (Glib.Generic_Properties);
@@ -47,6 +49,7 @@ package Gtk.Enums is
       Anchor_South_West,
       Anchor_West,
       Anchor_East);
+   pragma Convention (C, Gtk_Anchor_Type);
    --  Gtk_Anchor_Type indicates the exact location of the widget on its
    --  side. Note that not all anchors are relevant for each side.
    --
@@ -58,7 +61,6 @@ package Gtk.Enums is
    --  Thus, if a previous child was added on Side_North, then the new child
    --  will only appear on the second line in the container. The order the
    --  children are inserted into the container is important.
-   for Gtk_Anchor_Type'Size use Gint'Size;
 
    Anchor_N  : Gtk_Anchor_Type renames Anchor_North;
    Anchor_NW : Gtk_Anchor_Type renames Anchor_North_West;
@@ -70,18 +72,12 @@ package Gtk.Enums is
    Anchor_E  : Gtk_Anchor_Type renames Anchor_East;
 
    type Gtk_Arrow_Type is (Arrow_Up, Arrow_Down, Arrow_Left, Arrow_Right);
-   for Gtk_Arrow_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Arrow_Type);
 
    type Gtk_Attach_Options is new Glib.Guint32;
    Expand : constant Gtk_Attach_Options := 1;
    Shrink : constant Gtk_Attach_Options := 2;
    Fill   : constant Gtk_Attach_Options := 4;
-
-   type Gtk_Button_Action is new Guint;
-   Button_Ignored : constant Gtk_Button_Action := 0;
-   Button_Selects : constant Gtk_Button_Action := 1 ** 0;
-   Button_Drags   : constant Gtk_Button_Action := 1 ** 1;
-   Button_Expands : constant Gtk_Button_Action := 1 ** 2;
 
    type Gtk_Button_Box_Style is
      (Buttonbox_Default_Style,
@@ -89,63 +85,39 @@ package Gtk.Enums is
       Buttonbox_Edge,
       Buttonbox_Start,
       Buttonbox_End);
-   for Gtk_Button_Box_Style'Size use Gint'Size;
+   pragma Convention (C, Gtk_Button_Box_Style);
 
-   type Gtk_Cell_Type is
-     (Cell_Empty,
-      Cell_Text,
-      Cell_Pixmap,
-      Cell_Pixtext,
-      Cell_Widget);
-   for Gtk_Cell_Type'Size use Gint'Size;
+   type Gtk_Curve_Type is
+     (Curve_Type_Linear,
+      --  Linear interpolation
 
-   type Gtk_Ctree_Line_Style is
-     (Ctree_Lines_None,
-      --  No line will be drawn in the Ctree
+      Curve_Type_Spline,
+      --  Spline interpolation
 
-      Ctree_Lines_Solid,
-      --  Solid lines will be drawn
-
-      Ctree_Lines_Dotted,
-      --  Dotted lines will be drawn
-
-      Ctree_Lines_Tabbed
-      --  The tree won't be highlighted by lines but by tabs surrounding nodes
+      Curve_Type_Free
+      --  Free form curve
      );
-   for Gtk_Ctree_Line_Style'Size use Gint'Size;
-   --  See Gtk.Ctree.Set_Line_Style for more details.
-
-   type Gtk_Ctree_Expander_Style is
-     (Ctree_Expander_None,
-      --  No pixmap will be drawn, you will have to double-click on the node to
-      --  expand it.
-
-      Ctree_Expander_Square,
-      --  The pixmap will be a square
-
-      Ctree_Expander_Triangle,
-      --  The pixmap will be a triangle
-
-      Ctree_Expander_Circular
-      --  The pixmap will be a circle
-     );
-   --  See Gtk.Ctree.Set_Expander_Style for more details.
-   for Gtk_Ctree_Expander_Style'Size use Gint'Size;
+   pragma Convention (C, Gtk_Curve_Type);
 
    type Gtk_Delete_Type is
      (Delete_Chars,
       Delete_Word_Ends,
       --  Delete only the portion of the word to the left/right of the cursor
       --  if we are in the middle of a word.
+
       Delete_Words,
       Delete_Display_Lines,
       Delete_Display_Line_Ends,
       Delete_Paragraph_Ends,
-      --  Delete until end of line ???
+      --  Like C-k in Emacs: delete until end of line
+
       Delete_Paragraphs,
-      --  Delete current line ???
-      Delete_Whitespace);
-   for Gtk_Delete_Type'Size use Gint'Size;
+      --  C-k in pico: delete while line
+
+      Delete_Whitespace
+      --  M-\ in Emacs
+     );
+   pragma Convention (C, Gtk_Delete_Type);
 
    type Gtk_Direction_Type is
      (Dir_Tab_Forward,
@@ -154,7 +126,16 @@ package Gtk.Enums is
       Dir_Down,
       Dir_Left,
       Dir_Right);
-   for Gtk_Direction_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Direction_Type);
+   --  Focus movement types
+
+   type Gtk_Expander_Style is
+     (Expander_Collapsed,
+      Expander_Semi_Collapsed,
+      Expander_Semi_Expanded,
+      Expander_Expanded);
+   pragma Convention (C, Gtk_Expander_Style);
+   --  Expander styles
 
    type Gtk_Icon_Size is
      (Icon_Size_Invalid,
@@ -162,18 +143,61 @@ package Gtk.Enums is
       Icon_Size_Small_Toolbar,
       Icon_Size_Large_Toolbar,
       Icon_Size_Button,
+      Icon_Size_Dnd,
       Icon_Size_Dialog);
-   for Gtk_Icon_Size'Size use Gint'Size;
+   pragma Convention (C, Gtk_Icon_Size);
+   --  Built-in stock icon sizes
+
+   type Gtk_Side_Type is
+     (Side_Top,
+      Side_Bottom,
+      Side_Left,
+      Side_Right);
+   pragma Convention (C, Gtk_Side_Type);
+   --  Gtk_Side_Type indicates on which the widget should be inserted.
+   --  The children are displayed in the order they were inserted into the
+   --  container.
+   --  Each time a child is displayed, the available space for the remaining
+   --  child is restrained. For instance, every time you put a child on the
+   --  Side_Top or Side_Bottom, the available space is decreased so that no
+   --  other widget is inserted in the same line.
+   --
+   --  For instance, if you put two widgets on Side_Top, the second one will
+   --  appear below the first one. If you add two widgets on Side_Right, the
+   --  second one will be placed on the left of the first.
+
+   type Gtk_Text_Direction is
+     (Text_Dir_None,
+      Text_Dir_Ltr,
+      Text_Dir_Rtl);
+   pragma Convention (C, Gtk_Text_Direction);
 
    type Gtk_Justification is
      (Justify_Left,
       Justify_Right,
       Justify_Center,
       Justify_Fill);
-   for Gtk_Justification'Size use Gint'Size;
+   pragma Convention (C, Gtk_Justification);
+
+   type Gtk_Match_Type is
+     (Match_All,
+      Match_All_Tail,
+      Match_Head,
+      Match_Tail,
+      Match_Exact,
+      Match_Last);
+   pragma Convention (C, Gtk_Match_Type);
+
+   type Gtk_Menu_Direction_Type is
+     (Menu_Dir_Parent,
+      Menu_Dir_Child,
+      Menu_Dir_Next,
+      Menu_Dir_Prev);
+   pragma Convention (C, Gtk_Menu_Direction_Type);
+   --  Direction where to move the selection.
 
    type Gtk_Metric_Type is (Pixels, Inches, Centimeters);
-   for Gtk_Metric_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Metric_Type);
 
    type Gtk_Movement_Step is
      (Movement_Logical_Positions, --  move by forw/back graphemes
@@ -185,43 +209,63 @@ package Gtk.Enums is
       Movement_Paragraph_Ends,    --  move to either end of a paragraph
       Movement_Pages,             --  move by pages
       Movement_Buffer_Ends);      --  move to ends of the buffer
-   for Gtk_Movement_Step'Size use Gint'Size;
+   pragma Convention (C, Gtk_Movement_Step);
    --  Note that a paragraph is defined as a new-line ended line.
 
    type Gtk_Orientation is (Orientation_Horizontal, Orientation_Vertical);
-   for Gtk_Orientation'Size use Gint'Size;
+   pragma Convention (C, Gtk_Orientation);
+
+   type Gtk_Corner_Type is
+     (Corner_Top_Left,
+      Corner_Bottom_Left,
+      Corner_Top_Right,
+      Corner_Bottom_Right);
+   pragma Convention (C, Gtk_Corner_Type);
+   --  Type used by Set_Placement below to determine the location of the
+   --  child widget with respect to the scrollbars.
+   --  Corner_Top_Left means the child is in the top left, with the scrollbars
+   --  underneath and to the right.
 
    type Gtk_Pack_Type is (Pack_Start, Pack_End);
-   for Gtk_Pack_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Pack_Type);
+
+   type Gtk_Path_Priority_Type is mod 2 ** 32;
+
+   Path_Prio_Lowest      : constant Gtk_Path_Priority_Type := 0;
+   Path_Prio_Gtk         : constant Gtk_Path_Priority_Type := 4;
+   Path_Prio_Application : constant Gtk_Path_Priority_Type := 8;
+   Path_Prio_Theme       : constant Gtk_Path_Priority_Type := 10;
+   Path_Prio_RC          : constant Gtk_Path_Priority_Type := 12;
+   Path_Prio_Highest     : constant Gtk_Path_Priority_Type := 15;
+   --  Priorities for path lookups
+
+   Path_Prio_Mask        : constant Gtk_Path_Priority_Type := 16#0f#;
+
+   type Gtk_Path_Type is (Path_Widget, Path_Widget_Class, Path_Class);
+   pragma Convention (C, Gtk_Path_Type);
+   --  Widget path types
 
    type Gtk_Policy_Type is (Policy_Always, Policy_Automatic, Policy_Never);
-   for Gtk_Policy_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Policy_Type);
 
    type Gtk_Position_Type is
      (Pos_Left,
       Pos_Right,
       Pos_Top,
       Pos_Bottom);
-   for Gtk_Position_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Position_Type);
 
    type Gtk_Preview_Type is (Preview_Color, Preview_Grayscale);
-   for Gtk_Preview_Type'Size use Gint'Size;
-
-   type Gtk_Progress_Bar_Orientation is
-     (Progress_Left_To_Right,
-      Progress_Right_To_Left,
-      Progress_Bottom_To_Top,
-      Progress_Top_To_Bottom);
-   for Gtk_Progress_Bar_Orientation'Size use Gint'Size;
+   pragma Convention (C, Gtk_Preview_Type);
 
    type Gtk_Relief_Style is (Relief_Normal, Relief_Half, Relief_None);
-   for Gtk_Relief_Style'Size use Gint'Size;
+   pragma Convention (C, Gtk_Relief_Style);
 
    type Gtk_Resize_Mode is
      (Resize_Parent,     --  Pass request to the parent
       Resize_Queue,      --  Queue resizes on this widget
       Resize_Immediate); --  Perform the resizes now
-   for Gtk_Resize_Mode'Size use Gint'Size;
+   pragma Convention (C, Gtk_Resize_Mode);
 
    type Gtk_Scroll_Type is
      (Scroll_None,
@@ -238,16 +282,16 @@ package Gtk.Enums is
       Scroll_Step_Right,
       Scroll_Page_Left,
       Scroll_Page_Right,
-      Scroll_Page_Start,
-      Scroll_Page_End);
-   for Gtk_Scroll_Type'Size use Gint'Size;
+      Scroll_Start,
+      Scroll_End);
+   pragma Convention (C, Gtk_Scroll_Type);
 
    type Gtk_Selection_Mode is
      (Selection_None,
       Selection_Single,
       Selection_Browse,
       Selection_Multiple);
-   for Gtk_Selection_Mode'Size use Gint'Size;
+   pragma Convention (C, Gtk_Selection_Mode);
    --  The old Selection_Extended is now deprected, and replaced by
    --  Selection_Multiple.
 
@@ -257,29 +301,7 @@ package Gtk.Enums is
       Shadow_Out,
       Shadow_Etched_In,
       Shadow_Etched_Out);
-   for Gtk_Shadow_Type'Size use Gint'Size;
-
-   type Gtk_Side_Type is
-     (Side_Top,
-      Side_Bottom,
-      Side_Left,
-      Side_Right);
-   --  Gtk_Side_Type indicates on which the widget should be inserted.
-   --  The children are displayed in the order they were inserted into the
-   --  container.
-   --  Each time a child is displayed, the available space for the remaining
-   --  child is restrained. For instance, every time you put a child on the
-   --  Side_Top or Side_Bottom, the available space is decreased so that no
-   --  other widget is inserted in the same line.
-   --
-   --  For instance, if you put two widgets on Side_Top, the second one will
-   --  appear below the first one. If you add two widgets on Side_Right, the
-   --  second one will be placed on the left of the first.
-   for Gtk_Side_Type'Size use Gint'Size;
-
-   type Gtk_Sort_Type is
-     (Sort_Ascending,
-      Sort_Descending);
+   pragma Convention (C, Gtk_Shadow_Type);
 
    type Gtk_State_Type is
      (State_Normal,
@@ -287,19 +309,13 @@ package Gtk.Enums is
       State_Prelight,
       State_Selected,
       State_Insensitive);
-   for Gtk_State_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_State_Type);
 
    type Gtk_Submenu_Direction is (Direction_Left, Direction_Right);
-   for Gtk_Submenu_Direction'Size use Gint'Size;
+   pragma Convention (C, Gtk_Submenu_Direction);
 
    type Gtk_Submenu_Placement is (Top_Bottom, Left_Right);
-   for Gtk_Submenu_Placement'Size use Gint'Size;
-
-   type Gtk_Text_Direction is
-     (Text_Dir_None,
-      Text_Dir_Ltr,
-      Text_Dir_Rtl);
-   for Gtk_Text_Direction'Size use Gint'Size;
+   pragma Convention (C, Gtk_Submenu_Placement);
 
    type Gtk_Text_Window_Type is
      (Text_Window_Private,
@@ -309,7 +325,7 @@ package Gtk.Enums is
       Text_Window_Right,
       Text_Window_Top,
       Text_Window_Bottom);
-   for Gtk_Text_Window_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Text_Window_Type);
 
    type Gtk_Toolbar_Child_Type is
      (Toolbar_Child_Space,
@@ -317,26 +333,26 @@ package Gtk.Enums is
       Toolbar_Child_Togglebutton,
       Toolbar_Child_Radiobutton,
       Toolbar_Child_Widget);
-   for Gtk_Toolbar_Child_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Toolbar_Child_Type);
 
    type Gtk_Toolbar_Style is (Toolbar_Icons, Toolbar_Text, Toolbar_Both);
-   for Gtk_Toolbar_Style'Size use Gint'Size;
+   pragma Convention (C, Gtk_Toolbar_Style);
 
    type Gtk_Toolbar_Space_Style is (Toolbar_Space_Empty, Toolbar_Space_Line);
-   for Gtk_Toolbar_Space_Style'Size use Gint'Size;
+   pragma Convention (C, Gtk_Toolbar_Space_Style);
 
    type Gtk_Tree_View_Mode is (Tree_View_Line, Tree_View_Item);
-   for Gtk_Tree_View_Mode'Size use Gint'Size;
+   pragma Convention (C, Gtk_Tree_View_Mode);
 
    type Gtk_Update_Type is
      (Update_Continuous,
       Update_Discontinuous,
       Update_Delayed);
-   for Gtk_Update_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Update_Type);
 
    type Gtk_Visibility is
      (Visibility_None, Visibility_Partial, Visibility_Full);
-   for Gtk_Visibility'Size use Gint'Size;
+   pragma Convention (C, Gtk_Visibility);
 
    type Gtk_Window_Position is
      (Win_Pos_None,
@@ -344,16 +360,22 @@ package Gtk.Enums is
       Win_Pos_Mouse,
       Win_Pos_Center_Always,
       Win_Pos_Center_On_Parent);
-   for Gtk_Window_Position'Size use Gint'Size;
+   pragma Convention (C, Gtk_Window_Position);
 
    type Gtk_Window_Type is (Window_Toplevel, Window_Popup);
-   for Gtk_Window_Type'Size use Gint'Size;
+   pragma Convention (C, Gtk_Window_Type);
 
    type Gtk_Wrap_Mode is
-     (Wrapmode_None,
-      Wrapmode_Char,
-      Wrapmode_Word);
-   for Gtk_Wrap_Mode'Size use Gint'Size;
+     (Wrap_None,
+      Wrap_Char,
+      Wrap_Word);
+   pragma Convention (C, Gtk_Wrap_Mode);
+
+   type Gtk_Sort_Type is
+     (Sort_Ascending,
+      Sort_Descending);
+   pragma Convention (C, Gtk_Sort_Type);
+   --  How to sort
 
    --  Some Glib instantiations
 
@@ -383,37 +405,38 @@ package Gtk.Enums is
    package Guint_List is new
      Glib.Glist.Generic_List (Guint, Convert_UI, Convert_UA);
 
-
    ----------------
    -- Properties --
    ----------------
    --  The following packages and types are used to represent properties of
    --  the given type. They are used in the packages that use these properties
 
-   package Relief_Style_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Relief_Style);
-   package Resize_Mode_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Resize_Mode);
-   package Arrow_Type_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Arrow_Type);
-   package Shadow_Type_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Shadow_Type);
-   package Update_Type_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Update_Type);
-   package Position_Type_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Position_Type);
-   package Justification_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Justification);
-   package Orientation_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Orientation);
-   package Window_Type_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Window_Type);
-   package Window_Position_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Window_Position);
-   package Text_Direction_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Text_Direction);
-   package Wrap_Mode_Properties is new Generic_Internal_Discrete_Property
-     (Gtk_Text_Direction);
+   package Relief_Style_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Relief_Style);
+   package Resize_Mode_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Resize_Mode);
+   package Arrow_Type_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Arrow_Type);
+   package Shadow_Type_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Shadow_Type);
+   package Update_Type_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Update_Type);
+   package Position_Type_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Position_Type);
+   package Button_Box_Style_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Button_Box_Style);
+   package Justification_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Justification);
+   package Orientation_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Orientation);
+   package Window_Type_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Window_Type);
+   package Window_Position_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Window_Position);
+   package Text_Direction_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Text_Direction);
+   package Wrap_Mode_Properties is new
+     Generic_Internal_Discrete_Property (Gtk_Text_Direction);
 
    type Property_Gtk_Relief_Style  is new Relief_Style_Properties.Property;
    type Property_Gtk_Resize_Mode   is new Resize_Mode_Properties.Property;
@@ -421,6 +444,8 @@ package Gtk.Enums is
    type Property_Gtk_Shadow_Type   is new Shadow_Type_Properties.Property;
    type Property_Gtk_Update_Type   is new Update_Type_Properties.Property;
    type Property_Gtk_Position_Type is new Position_Type_Properties.Property;
+   type Property_Gtk_Button_Box_Style is new
+     Button_Box_Style_Properties.Property;
    type Property_Gtk_Justification is new Justification_Properties.Property;
    type Property_Gtk_Orientation   is new Orientation_Properties.Property;
    type Property_Gtk_Window_Type   is new Window_Type_Properties.Property;
