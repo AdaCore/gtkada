7aecc31674e31e8384c6e3c4e95d36b0693cef4e
Arnaud Charlet <charlet@adacore.com>
2002-01-03 09:19:42 +0000

    Add full binding for this package. Add 2002 to copyright notice.

--- src/gtk-rc.adb
+++ src/gtk-rc.adb
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2001 ACT-Europe                 --
+--                Copyright (C) 2000-2002 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -28,53 +28,195 @@
 -----------------------------------------------------------------------
 
 with System;
+with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Strings;
+with Gtk.Widget; use Gtk.Widget;
 
 package body Gtk.Rc is
 
-   ----------------------------
-   -- Add_Widget_Class_Style --
-   ----------------------------
+   -------------
+   -- Gtk_New --
+   -------------
 
-   procedure Add_Widget_Class_Style
-     (Style   : Gtk_Rc_Style;
-      Pattern : String)
-   is
-      procedure Internal
-        (Style : Gtk_Rc_Style; Pattern : String);
-      pragma Import (C, Internal, "gtk_rc_add_widget_class_style");
+   procedure Gtk_New (Rc_Style : out Gtk_Rc_Style) is
+   begin
+      Rc_Style := new Gtk_Rc_Style_Record;
+      Gtk.Rc.Initialize (Rc_Style);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize (Rc_Style : access Gtk_Rc_Style_Record'Class) is
+      function Internal return System.Address;
+      pragma Import (C, Internal, "gtk_rc_style_new");
 
    begin
-      Internal (Style, Pattern & ASCII.NUL);
-   end Add_Widget_Class_Style;
+      Set_Object (Rc_Style, Internal);
+      Initialize_User_Data (Rc_Style);
+   end Initialize;
 
-   ---------------------------
-   -- Add_Widget_Name_Style --
-   ---------------------------
+   ----------
+   -- Copy --
+   ----------
 
-   procedure Add_Widget_Name_Style
-     (Style   : Gtk_Rc_Style;
-      Pattern : String)
-   is
-      procedure Internal (Style : Gtk_Rc_Style; Pattern : String);
-      pragma Import (C, Internal, "gtk_rc_add_widget_name_style");
+   function Copy (Orig : access Gtk_Rc_Style_Record) return Gtk_Rc_Style is
+      function Internal (Orig : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_rc_style_copy");
+
+      Stub : Gtk_Rc_Style_Record;
+   begin
+      return Gtk_Rc_Style (Get_User_Data (Internal (Get_Object (Orig)), Stub));
+   end Copy;
+
+   ----------------------
+   -- Add_Default_File --
+   ----------------------
+
+   procedure Add_Default_File (Filename : String) is
+      procedure Internal (Filename : String);
+      pragma Import (C, Internal, "gtk_rc_add_default_file");
 
    begin
-      Internal (Style, Pattern & ASCII.NUL);
-   end Add_Widget_Name_Style;
+      Internal (Filename & ASCII.NUL);
+   end Add_Default_File;
+
+   -----------------------
+   -- Set_Default_Files --
+   -----------------------
+
+   procedure Set_Default_Files (Filenames : Chars_Ptr_Array) is
+      procedure Internal (Filenames : Chars_Ptr_Array);
+      pragma Import (C, Internal, "gtk_rc_set_default_files");
+
+   begin
+      Internal (Filenames + Null_Ptr);
+   end Set_Default_Files;
+
+   -----------------------
+   -- Get_Default_Files --
+   -----------------------
+
+   function Get_Default_Files return Chars_Ptr_Array is
+      type Flat_Chars_Ptr_Array is array (Positive) of Chars_Ptr;
+      type Flat_Access is access all Flat_Chars_Ptr_Array;
+
+      function Internal return Flat_Access;
+      pragma Import (C, Internal, "gtk_rc_get_default_files");
+
+      Addr : Flat_Access := Internal;
+      Len  : Positive;
+
+      use type Strings.chars_ptr;
+
+   begin
+      Len := 1;
+
+      loop
+         exit when Addr (Len) = Null_Ptr;
+         Len := Len + 1;
+      end loop;
+
+      declare
+         Result : Chars_Ptr_Array (1 .. size_t (Len) - 1);
+      begin
+         for J in Result'Range loop
+            Result (J) := Addr (Positive (J));
+         end loop;
+
+         return Result;
+      end;
+   end Get_Default_Files;
 
    ---------------
    -- Get_Style --
    ---------------
 
    function Get_Style
-     (Widget : access Gtk.Widget.Gtk_Widget_Record'Class) return Gtk_Rc_Style
+     (Widget : access Gtk.Widget.Gtk_Widget_Record'Class) return Gtk_Style
    is
-      function Internal (Widget : System.Address) return Gtk_Rc_Style;
+      function Internal (Widget : System.Address) return Gtk_Style;
       pragma Import (C, Internal, "gtk_rc_get_style");
 
    begin
       return Internal (Get_Object (Widget));
    end Get_Style;
+
+   ------------------------
+   -- Get_Style_By_Paths --
+   ------------------------
+
+   --  function Get_Style_By_Paths
+   --    (Settings    : access Gtk_Settings_Record'Class;
+   --     Widget_Path : String := "";
+   --     Class_Path  : String := "";
+   --     The_Type    : Glib.GType := Glib.GType_None) return Gtk_Style
+   --  is
+   --     function Internal
+   --       (Settings    : System.Address;
+   --        Widget_Path : System.Address;
+   --        Class_Path  : System.Address;
+   --        The_Type    : Glib.GType) return Gtk_Style;
+   --     pragma Import (C, Internal, "gtk_rc_get_style_by_paths");
+
+   --     W, C : System.Address := System.Null_Address;
+
+   --  begin
+   --     if Widget_Path /= "" then
+   --        W := Widget_Path'Address;
+   --     end if;
+
+   --     if Clast_Path /= "" then
+   --        C := Class_Path'Address;
+   --     end if;
+
+   --     return Internal (Get_Object (Settings, W, C, The_Type));
+   --  end Get_Style_By_Paths;
+
+   ------------------------------
+   -- Reparse_All_For_Settings --
+   ------------------------------
+
+   --  function Reparse_All_For_Settings
+   --    (Settings   : access Gtk_Settings_Record'Class;
+   --     Force_Load : Boolean) return Boolean
+   --  is
+   --     function Internal
+   --       (Settings   : System.Address;
+   --        Force_Load : Gboolean) return Gboolean;
+   --     pragma Import (C, Internal, "gtk_rc_reparse_all_for_settings");
+
+   --  begin
+   --     return To_Boolean (Internal
+   --       (Get_Object (Settings), To_Gboolean (Force_Load));
+   --  end Reparse_All_For_Settings;
+
+   -------------------------
+   -- Find_Pixmap_In_Path --
+   -------------------------
+
+   --  function Find_Pixmap_In_Path
+   --    (Settings    : access Gtk_Settings_Record'Class;
+   --     Pixmap_File : String) return String
+   --  is
+   --     function Internal
+   --       (Settings    : System.Address;
+   --        Scanner     : System.Address := System.Null_Address;
+   --        Pixmap_File : String) return Chars_Ptr;
+   --     pragma Import (C, Internal, "gtk_rc_find_pixmap_in_path");
+
+   --     S : Chars_Ptr := Internal
+   --       (Get_Object (Settings), Pixmap_File => Pixmap_File & ASCII.NUL);
+
+   --  begin
+   --     declare
+   --        Str : constant String := Strings.Value (S);
+   --     begin
+   --        g_free (S);
+   --        return Str;
+   --     end;
+   --  end Find_Pixmap_In_Path;
 
    -----------
    -- Parse --
@@ -99,5 +241,144 @@ package body Gtk.Rc is
    begin
       Internal (Rc_String & ASCII.NUL);
    end Parse_String;
+
+   -----------------
+   -- Reparse_All --
+   -----------------
+
+   function Reparse_All return Boolean is
+      function Internal return Gboolean;
+      pragma Import (C, Internal, "gtk_rc_reparse_all");
+
+   begin
+      return To_Boolean (Internal);
+   end Reparse_All;
+
+   -------------------------
+   -- Find_Module_In_Path --
+   -------------------------
+
+   function Find_Module_In_Path (Module_File : String) return String is
+      function Internal (Module_File : String) return Chars_Ptr;
+      pragma Import (C, Internal, "gtk_rc_find_module_in_path");
+
+      S : Chars_Ptr := Internal (Module_File & ASCII.NUL);
+
+   begin
+      declare
+         Str : constant String := Strings.Value (S);
+      begin
+         g_free (S);
+         return Str;
+      end;
+   end Find_Module_In_Path;
+
+   -------------------
+   -- Get_Theme_Dir --
+   -------------------
+
+   function Get_Theme_Dir return String is
+      function Internal return Chars_Ptr;
+      pragma Import (C, Internal, "gtk_rc_get_theme_dir");
+
+      S : Chars_Ptr := Internal;
+
+   begin
+      declare
+         Str : constant String := Strings.Value (S);
+      begin
+         g_free (S);
+         return Str;
+      end;
+   end Get_Theme_Dir;
+
+   --------------------
+   -- Get_Module_Dir --
+   --------------------
+
+   function Get_Module_Dir return String is
+      function Internal return Chars_Ptr;
+      pragma Import (C, Internal, "gtk_rc_get_module_dir");
+
+      S : Chars_Ptr := Internal;
+
+   begin
+      declare
+         Str : constant String := Strings.Value (S);
+      begin
+         g_free (S);
+         return Str;
+      end;
+   end Get_Module_Dir;
+
+   ------------------------
+   -- Get_Im_Module_Path --
+   ------------------------
+
+   function Get_Im_Module_Path return String is
+      function Internal return Chars_Ptr;
+      pragma Import (C, Internal, "gtk_rc_get_im_module_path");
+
+      S : Chars_Ptr := Internal;
+
+   begin
+      declare
+         Str : constant String := Strings.Value (S);
+      begin
+         g_free (S);
+         return Str;
+      end;
+   end Get_Im_Module_Path;
+
+   ------------------------
+   -- Get_Im_Module_File --
+   ------------------------
+
+   function Get_Im_Module_File return String is
+      function Internal return Chars_Ptr;
+      pragma Import (C, Internal, "gtk_rc_get_im_module_file");
+
+      S : Chars_Ptr := Internal;
+
+   begin
+      declare
+         Str : constant String := Strings.Value (S);
+      begin
+         g_free (S);
+         return Str;
+      end;
+   end Get_Im_Module_File;
+
+   ------------------------
+   -- Get_Modifier_Style --
+   ------------------------
+
+   function Get_Modifier_Style
+     (Widget : access Gtk_Widget_Record'Class) return Gtk_Rc_Style
+   is
+      function Internal (Widget : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_widget_get_modifier_style");
+
+      Stub : Gtk_Rc_Style_Record;
+
+   begin
+      return Gtk_Rc_Style
+        (Get_User_Data (Internal (Get_Object (Widget)), Stub));
+   end Get_Modifier_Style;
+
+   ------------------
+   -- Modify_Style --
+   ------------------
+
+   procedure Modify_Style
+     (Widget : access Gtk_Widget_Record'Class;
+      Style  : access Gtk_Rc_Style_Record'Class)
+   is
+      procedure Internal (Widget : System.Address; Style : System.Address);
+      pragma Import (C, Internal, "gtk_widget_modify_style");
+
+   begin
+      Internal (Get_Object (Widget), Get_Object (Style));
+   end Modify_Style;
 
 end Gtk.Rc;
--- src/gtk-rc.ads
+++ src/gtk-rc.ads
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2001 ACT-Europe                 --
+--                Copyright (C) 2000-2002 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -27,29 +27,131 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
+--  <c_version>1.3.11</c_version>
+
+with Glib.Object;
+with Gtk.Style; use Gtk.Style;
 with Gtk.Widget;
+with Gtkada.Types; use Gtkada.Types;
 
 package Gtk.Rc is
 
-   procedure Init;
+   type Gtk_Rc_Style_Record is new Glib.Object.GObject_Record with private;
+   type Gtk_Rc_Style is access all Gtk_Rc_Style_Record'Class;
+
+   procedure Gtk_New (Rc_Style : out Gtk_Rc_Style);
+
+   procedure Initialize (Rc_Style : access Gtk_Rc_Style_Record'Class);
+
+   function Get_Type return Glib.GType;
+   --  Return the internal value associated with Gtk_Rc_Style.
+
+   function Copy (Orig : access Gtk_Rc_Style_Record) return Gtk_Rc_Style;
+   --  Make a copy of the specified Gtk_Rc.Style.
+   --  This function will correctly copy an rc style that is a member of a
+   --  class derived from Gtk_Rc_Style.
+
+   procedure Add_Default_File (Filename : String);
+   --  Add a file to the list of files to be parsed at the end of Gtk.Main.Init
+
+   procedure Set_Default_Files (Filenames : Chars_Ptr_Array);
+   --  Set the list of files that GtkAda will read at the end of Gtk.Main.Init
+
+   function Get_Default_Files return Chars_Ptr_Array;
+   --  Retrieve the current list of RC files that will be parsed
+   --  at the end of Gtk.Main.Init
+
+   function Get_Style
+     (Widget : access Gtk.Widget.Gtk_Widget_Record'Class) return Gtk_Style;
+   --  Find all matching RC styles for a given widget, composites them
+   --  together, and then create a Gtk_Style representing the composite
+   --  appearance. (GtkAda actually keeps a cache of previously created styles,
+   --  so a new style may not be created)
+   --  Return the resulting style. No refcount is added to the returned style,
+   --  so if you want to save this style around, you should add a reference
+   --  yourself.
+
+   --  ??? function Get_Style_By_Paths
+   --    (Settings    : access Gtk_Settings_Record'Class;
+   --     Widget_Path : String := "";
+   --     Class_Path  : String := "";
+   --     The_Type    : Glib.GType := Glib.GType_None) return Gtk_Style;
+   --  Create up a Gtk_Style from styles defined in a RC file by providing
+   --  the raw components used in matching. This function may be useful
+   --  when creating pseudo-widgets that should be themed like widgets but
+   --  don't actually have corresponding GtkAda widgets. An example of this
+   --  would be items inside a GNOME canvas widget.
+   --
+   --  Widget_Path: the widget path to use when looking up the style
+   --  Class_Path: the class path to use when looking up the style
+   --  The_Type: a type that will be used along with parent types of this type
+   --            when matching against class styles.
+   --  Return a style created by matching with the supplied paths, or null if
+   --  nothing matching was specified and the default style should be used. The
+   --  returned value is owned by GtkAda as part of an internal cache, so you
+   --  must call Ref on the returned value if you want to keep a reference to
+   --  it.
+
+   --  ??? function Reparse_All_For_Settings
+   --    (Settings   : access Gtk_Settings_Record'Class;
+   --     Force_Load : Boolean) return Boolean;
+   --  If the modification time on any previously read file
+   --  for the given Gtk_Settings has changed, discard all style information
+   --  and then reread all previously read RC files.
+   --  Return True if the files were reread.
+
+   --  ??? function Find_Pixmap_In_Path
+   --    (Settings    : access Gtk_Settings_Record'Class;
+   --     Pixmap_File : String) return String;
+   --  Look up a file in pixmap path for the specified Gtk_Settings.
+   --  If the file is not found, it outputs a warning message and return null.
+   --  Pixmap_File: name of the pixmap file to locate.
 
    procedure Parse (Filename : String);
 
    procedure Parse_String (Rc_String : String);
 
-   function Get_Style
+   function Reparse_All return Boolean;
+   --  If the modification time on any previously read file for the
+   --  default Gtk_Settings has changed, discard all style information
+   --  and then reread all previously read RC files.
+   --  Return True if the files were reread.
+
+   function Find_Module_In_Path (Module_File : String) return String;
+
+   function Get_Theme_Dir return String;
+
+   function Get_Module_Dir return String;
+
+   function Get_Im_Module_Path return String;
+
+   function Get_Im_Module_File return String;
+
+   ------------------------------
+   -- Widget related functions --
+   ------------------------------
+
+   procedure Modify_Style
+     (Widget : access Gtk.Widget.Gtk_Widget_Record'Class;
+      Style  : access Gtk_Rc_Style_Record'Class);
+   --  Modify the default style of a widget.
+
+   function Get_Modifier_Style
      (Widget : access Gtk.Widget.Gtk_Widget_Record'Class) return Gtk_Rc_Style;
-
-   procedure Add_Widget_Name_Style
-     (Style   : Gtk_Rc_Style;
-      Pattern : String);
-
-   procedure Add_Widget_Class_Style
-     (Style   : Gtk_Rc_Style;
-      Pattern : String);
+   --  Return the current modifier style for the widget.
+   --  (As set by Modify_Style.) If no style has previously set, a new
+   --  Gtk_Rc_Style will be created with all values unset, and set as the
+   --  modifier style for the widget. If you make changes to this rc
+   --  style, you must call Modify_Style, passing in the
+   --  returned rc style, to make sure that your changes take effect.
+   --
+   --  Return value: the modifier style for the widget. This rc style is
+   --    owned by the widget. If you want to keep a pointer to value this
+   --    around, you must add a refcount using Ref.
 
 private
+   type Gtk_Rc_Style_Record is new Glib.Object.GObject_Record with null record;
 
-   pragma Import (C, Init, "gtk_rc_init");
+   pragma Import (C, Get_Type, "gtk_rc_style_get_type");
 
 end Gtk.Rc;
