cc4884b93a5251b2b5fab034c7ef086f0be27d9b
Emmanuel Briot <briot@adacore.com>
2002-01-04 11:53:21 +0000

    (Set_Items): Force the layout of the items, once they have been added to  the canvas

--- src/gtkada-canvas.adb
+++ src/gtkada-canvas.adb
@@ -938,7 +938,7 @@ package body Gtkada.Canvas is
       Canvas.Children := Items;
 
       if Canvas.Auto_Layout then
-         Layout (Canvas);
+         Layout (Canvas, Force => True);
       end if;
       Update_Adjustments (Canvas);
    end Set_Items;
