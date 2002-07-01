623ee7dd21cd5dbf85cd6ddbe3acd098810a35de
Emmanuel Briot <briot@adacore.com>
2002-07-01 07:51:34 +0000

    Complete binding of the public part of Pango.Layout. The functions not bound  seem to be only for people implementing new pango backends

---
+++ src/pango-layout.adb
@@ -0,0 +1,310 @@
+-----------------------------------------------------------------------
+--               GtkAda - Ada95 binding for Gtk+/Gnome               --
+--                                                                   --
+--                   Copyright (C) 2002 ACT-Europe                   --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+with Glib;                 use Glib;
+with Glib.Object;          use Glib.Object;
+with Gdk.Rectangle;        use Gdk.Rectangle;
+with Pango.Context;        use Pango.Context;
+with Pango.Font;           use Pango.Font;
+with System;               use System;
+with Gtkada.Types;         use Gtkada.Types;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+
+package body Pango.Layout is
+
+   --------------
+   -- Get_Size --
+   --------------
+
+   procedure Get_Size
+     (Layout : access Pango_Layout_Record;
+      Width  : out Gint;
+      Height : out Gint)
+   is
+      procedure Internal
+        (Layout : System.Address; Width, Height : out Gint);
+      pragma Import (C, Internal, "pango_layout_get_size");
+   begin
+      Internal (Get_Object (Layout), Width, Height);
+   end Get_Size;
+
+   --------------------
+   -- Get_Pixel_Size --
+   --------------------
+
+   procedure Get_Pixel_Size
+     (Layout : access Pango_Layout_Record;
+      Width  : out Gint;
+      Height : out Gint)
+   is
+      procedure Internal
+        (Layout : System.Address; Width, Height : out Gint);
+      pragma Import (C, Internal, "pango_layout_get_pixel_size");
+   begin
+      Internal (Get_Object (Layout), Width, Height);
+   end Get_Pixel_Size;
+
+   -----------------
+   -- Get_Extents --
+   -----------------
+
+   procedure Get_Extents
+     (Layout       : access Pango_Layout_Record;
+      Ink_Rect     : out Gdk.Rectangle.Gdk_Rectangle;
+      Logical_Rect : out Gdk.Rectangle.Gdk_Rectangle)
+   is
+      procedure Internal (Layout : System.Address;
+                          Ink_Rect, Logical_Rect : out Gdk_Rectangle);
+      pragma Import (C, Internal, "pango_layout_get_extents");
+   begin
+      Internal (Get_Object (Layout), Ink_Rect, Logical_Rect);
+   end Get_Extents;
+
+   -----------------------
+   -- Get_Pixel_Extents --
+   -----------------------
+
+   procedure Get_Pixel_Extents
+     (Layout       : access Pango_Layout_Record;
+      Ink_Rect     : out Gdk.Rectangle.Gdk_Rectangle;
+      Logical_Rect : out Gdk.Rectangle.Gdk_Rectangle)
+   is
+      procedure Internal (Layout : System.Address;
+                          Ink_Rect, Logical_Rect : out Gdk_Rectangle);
+      pragma Import (C, Internal, "pango_layout_get_pixel_extents");
+   begin
+      Internal (Get_Object (Layout), Ink_Rect, Logical_Rect);
+   end Get_Pixel_Extents;
+
+   --------------------------
+   -- Set_Font_Description --
+   --------------------------
+
+   procedure Set_Font_Description
+     (Layout : access Pango_Layout_Record;
+      Font   : Pango.Font.Pango_Font_Description)
+   is
+      procedure Internal
+        (Layout : System.Address; Font : Pango_Font_Description);
+      pragma Import (C, Internal, "pango_layout_set_font_description");
+   begin
+      Internal (Get_Object (Layout), Font);
+   end Set_Font_Description;
+
+   -------------
+   -- Gdk_New --
+   -------------
+
+   procedure Gdk_New
+     (Layout : out Pango_Layout;
+      Context : access Pango.Context.Pango_Context_Record'Class)
+   is
+      function Internal (Context : System.Address) return System.Address;
+      pragma Import (C, Internal, "pango_layout_new");
+   begin
+      Layout := new Pango_Layout_Record;
+      Set_Object (Layout, Internal (Get_Object (Context)));
+   end Gdk_New;
+
+   -----------------
+   -- Get_Context --
+   -----------------
+
+   function Get_Context (Layout : access Pango_Layout_Record)
+      return Pango.Context.Pango_Context
+   is
+      function Internal (Layout : System.Address) return System.Address;
+      pragma Import (C, Internal, "pango_layout_get_context");
+      Stub : Pango_Context_Record;
+   begin
+      return Pango_Context
+        (Get_User_Data (Internal (Get_Object (Layout)), Stub));
+   end Get_Context;
+
+   ---------------------
+   -- Context_Changed --
+   ---------------------
+
+   procedure Context_Changed (Layout : access Pango_Layout_Record) is
+      procedure Internal (Layout : System.Address);
+      pragma Import (C, Internal, "pango_layout_context_changed");
+   begin
+      Internal (Get_Object (Layout));
+   end Context_Changed;
+
+   --------------
+   -- Set_Text --
+   --------------
+
+   procedure Set_Text (Layout : access Pango_Layout_Record; Text : String) is
+      procedure Internal
+        (Layout : System.Address; Text : String; Length : Gint);
+      pragma Import (C, Internal, "pango_layout_set_text");
+   begin
+      Internal (Get_Object (Layout), Text, Text'Length);
+   end Set_Text;
+
+   --------------
+   -- Get_Text --
+   --------------
+
+   function Get_Text (Layout : access Pango_Layout_Record)
+      return Gtkada.Types.Chars_Ptr
+   is
+      function Internal (Layout : System.Address)
+         return Interfaces.C.Strings.chars_ptr;
+      pragma Import (C, Internal, "pango_layout_get_text");
+   begin
+      return Internal (Get_Object (Layout));
+   end Get_Text;
+
+   --------------
+   -- Get_Text --
+   --------------
+
+   function Get_Text (Layout : access Pango_Layout_Record) return String is
+   begin
+      return Value (Get_Text (Layout));
+   end Get_Text;
+
+   --------------------
+   -- Get_Line_Count --
+   --------------------
+
+   function Get_Line_Count (Layout : access Pango_Layout_Record)
+      return Glib.Gint
+   is
+      function Internal (Layout : System.Address) return Gint;
+      pragma Import (C, Internal, "pango_layout_get_line_count");
+   begin
+      return Internal (Get_Object (Layout));
+   end Get_Line_Count;
+
+   -----------------
+   -- Set_Justify --
+   -----------------
+
+   procedure Set_Justify
+     (Layout : access Pango_Layout_Record; Justify : Boolean)
+   is
+      procedure Internal (Layout : System.Address; Justify : Integer);
+      pragma Import (C, Internal, "pango_layout_set_justify");
+   begin
+      Internal (Get_Object (Layout), Boolean'Pos (Justify));
+   end Set_Justify;
+
+   -----------------
+   -- Get_Justify --
+   -----------------
+
+   function Get_Justify (Layout : access Pango_Layout_Record) return Boolean is
+      function Internal (Layout : System.Address) return Integer;
+      pragma Import (C, Internal, "pango_layout_get_justify");
+   begin
+      return Boolean'Val (Internal (Get_Object (Layout)));
+   end Get_Justify;
+
+   -------------------
+   -- Set_Alignment --
+   -------------------
+
+   procedure Set_Alignment
+     (Layout    : access Pango_Layout_Record'Class;
+      Alignment : Pango_Alignment)
+   is
+      procedure Internal (Layout : System.Address; Align : Pango_Alignment);
+      pragma Import (C, Internal, "pango_layout_set_alignment");
+   begin
+      Internal (Get_Object (Layout), Alignment);
+   end Set_Alignment;
+
+   -------------------
+   -- Get_Alignment --
+   -------------------
+
+   function Get_Alignment (Layout : access Pango_Layout_Record)
+      return Pango_Alignment
+   is
+      function Internal (Layout : System.Address) return Pango_Alignment;
+      pragma Import (C, Internal, "pango_layout_get_alignment");
+   begin
+      return Internal (Get_Object (Layout));
+   end Get_Alignment;
+
+   ---------------
+   -- Set_Width --
+   ---------------
+
+   procedure Set_Width
+     (Layout : access Pango_Layout_Record; Width : Glib.Gint)
+   is
+      procedure Internal (Layout : System.Address; Width : Gint);
+      pragma Import (C, Internal, "pango_layout_set_width");
+   begin
+      Internal (Get_Object (Layout), Width);
+   end Set_Width;
+
+   ---------------
+   -- Get_Width --
+   ---------------
+
+   function Get_Width (Layout : access Pango_Layout_Record) return Glib.Gint is
+      function Internal (Layout : System.Address) return Gint;
+      pragma Import (C, Internal, "pango_layout_get_width");
+   begin
+      return Internal (Get_Object (Layout));
+   end Get_Width;
+
+   --------------
+   -- Set_Wrap --
+   --------------
+
+   procedure Set_Wrap
+     (Layout : access Pango_Layout_Record; Mode : Pango_Wrap_Mode)
+   is
+      procedure Internal (Layout : System.Address; Mode : Pango_Wrap_Mode);
+      pragma Import (C, Internal, "pango_layout_set_wrap");
+   begin
+      Internal (Get_Object (Layout), Mode);
+   end Set_Wrap;
+
+   --------------
+   -- Get_Wrap --
+   --------------
+
+   function Get_Wrap (Layout : access Pango_Layout_Record)
+      return Pango_Wrap_Mode
+   is
+      function Internal (Layout : System.Address) return Pango_Wrap_Mode;
+      pragma Import (C, Internal, "pango_layout_get_wrap");
+   begin
+      return Internal (Get_Object (Layout));
+   end Get_Wrap;
+
+end Pango.Layout;
+
--- src/pango-layout.ads
+++ src/pango-layout.ads
@@ -26,21 +26,261 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
+--  <description>
+--
+--  This package provides a high-level object that is capable of arranging text
+--  in a visually correct manner. It supports international character sets,
+--  although all strings should be Utf8, supports left-to-right and
+--  right-to-left writing systems, is capable of handling multi-line texts, and
+--  properly aligns tab characters in the text.
+--
+--  This is the base type that is used in the standard gtk+ widget for all the
+--  widgets that display some text on the screen.
+--
+--  Since it works directly with Pango.Font.Pango_Font_Description fonts, it is
+--  also much better at handling resizing of text, wrapping,... than direct
+--  calls to Gdk.Drawable.Draw_Text.
+--
+--  The idea is that this widget is used to compute the layout of the
+--  characters (ie their screen position). It doesn't do any rendering,
+--  however, and should be used in conjonction with Gdk.Drawable.Draw_Layout to
+--  actually display something on the screen.
+--
+--  This widget is independent from any specific drawing systems, and might for
+--  instance be used to create postscript documents, for direct access to the
+--  win32 API,...
+--
+--  This widget represents one of the fundamental additions to gtk+ 2.0 over
+--  what previously existed in the gtk+ 1.x series. It obsoletes the package
+--  Gdk.Font, which should only be used for legacy applications.
+--
+--  </description>
+
 with Glib;
+with Pango.Context;
 with Pango.Font;
+with Glib.Object;
+with Gdk.Rectangle;
+with Gtkada.Types;
 
 package Pango.Layout is
 
-   type Pango_Layout is new Glib.C_Proxy;
+   type Pango_Layout_Record is new Glib.Object.GObject_Record with private;
+   type Pango_Layout is access all Pango_Layout_Record'Class;
+   --  A widget that allows you to easily display text, including handling of
+   --  internationalization, right-to-left writing,...
+   --  See the function Gtk.Widget.Create_Pango_Layout for more information on
+   --  how to create layouts
+   --  Use Glib.Object.Unref to destroy a Pango_Layout
+   --  See Gdk.Drawable.Draw_Layout for how to actually display the layout on
+   --  the screen.
+
+   type Pango_Alignment is
+     (Pango_Align_Left,
+      Pango_Align_Center,
+      Pango_Align_Right);
+   pragma Convention (C, Pango_Alignment);
+   --  The alignment for each line of the layout
+
+   type Pango_Wrap_Mode is
+     (Pango_Wrap_Word,
+      Pango_Wrap_Char);
+   pragma Convention (C, Pango_Wrap_Mode);
+   --  The wrap mode for a layout
+
+   -----------------------
+   -- Creating a layout --
+   -----------------------
+   --  A layout can be created in two ways: either from a widget
+   --  (Gtk.Widget.Create_Pango_Layout), from which it will inherit the font
+   --  and various other attributes, or directly from a Pango_Context.
+
+   procedure Gdk_New
+     (Layout : out Pango_Layout;
+      Context : access Pango.Context.Pango_Context_Record'Class);
+   --  Create a new layout, based on context.
+
+   --------------
+   -- Contexts --
+   --------------
+
+   function Get_Context (Layout : access Pango_Layout_Record)
+      return Pango.Context.Pango_Context;
+   --  Return the context of the layout. The returned value is the internal
+   --  context itself, so you must Glib.Object.Ref it if you need to keep a
+   --  reference. You shouldn't Unref it.
 
    procedure Set_Font_Description
-     (Layout : Pango_Layout;
+     (Layout : access Pango_Layout_Record;
       Font   : Pango.Font.Pango_Font_Description);
    --  Change the font used in the layout.
    --  If not font description is set for the layout, the font description from
    --  the layout's context is used.
 
+   procedure Context_Changed (Layout : access Pango_Layout_Record);
+   --  Forces recomputation of any state in Layout that might depend
+   --  on the layout's context. This function should be called if you make
+   --  changes to the context subsequent to creating the layout.
+
+   ----------------------
+   -- Getting the size --
+   ----------------------
+   --  Pango internally stores its sizes in pango units, which are a number of
+   --  pixels (device units, when not drawing on the screen) multiplied by
+   --  Pango_Scale. There are generally equivalent subprograms to get the sizes
+   --  directly in pixels.
+
+   procedure Get_Extents
+     (Layout       : access Pango_Layout_Record;
+      Ink_Rect     : out Gdk.Rectangle.Gdk_Rectangle;
+      Logical_Rect : out Gdk.Rectangle.Gdk_Rectangle);
+   --  Compute the logical and ink extents of Layout. Logical extents
+   --  are usually what you want for positioning things. The extents
+   --  are given in pango units; layout coordinates begin at the
+   --  top left corner of the layout.
+   --  Logical_Rect is the overall size of the layout, ie it includes Ink_Rect
+   --  (where the text is actually drawn) and a small border around it.
+
+   procedure Get_Size
+     (Layout : access Pango_Layout_Record;
+      Width  : out Glib.Gint;
+      Height : out Glib.Gint);
+   --  Return the logical size, in pango units, of the layout. This is a
+   --  convenience function around Get_Extents.
+
+   procedure Get_Pixel_Extents
+     (Layout       : access Pango_Layout_Record;
+      Ink_Rect     : out Gdk.Rectangle.Gdk_Rectangle;
+      Logical_Rect : out Gdk.Rectangle.Gdk_Rectangle);
+   --  Same as Get_Extents, but the returned values are in pixels (or device
+   --  units when not drawing on the screen).
+
+   procedure Get_Pixel_Size
+     (Layout : access Pango_Layout_Record;
+      Width  : out Glib.Gint;
+      Height : out Glib.Gint);
+   --  Same as Get_Size, but the returned values are in pixels.
+
+   ---------------------------
+   -- Manipulating the text --
+   ---------------------------
+   --  When initially created with Gtk.Widget.Create_Pango_Layout, the layout
+   --  contains some text. Of course, this text may also be changed later in
+   --  the life of the layout.
+
+   procedure Set_Text (Layout : access Pango_Layout_Record; Text : String);
+   --  Change the text that the layout displays
+   --  Text must be a valid UTF8 string. See Glib.Convert for useful conversion
+   --  functions.
+
+   function Get_Text (Layout : access Pango_Layout_Record) return String;
+   --  Return the text currently displayed in the layout.
+   --  It is more efficient to use the iterators on the layout than get text if
+   --  you do not need Ada-specific subprograms to act on the text.
+
+   function Get_Text (Layout : access Pango_Layout_Record)
+      return Gtkada.Types.Chars_Ptr;
+   --  Same a Get_Text, but return directly the C string, which is more
+   --  efficient. The returned value should not be freed or modified.
+
+   function Get_Line_Count (Layout : access Pango_Layout_Record)
+      return Glib.Gint;
+   --  Return the number of lines in Layout
+
+   ------------------------
+   -- Layouting the text --
+   ------------------------
+
+   procedure Set_Justify
+     (Layout : access Pango_Layout_Record; Justify : Boolean);
+   --  Set whether or not each complete line should be stretched to fill the
+   --  entire width of the layout. This stretching is typically done by adding
+   --  whitespace, but for some scripts (such as Arabic), the justification is
+   --  done by extending the characters.
+
+   function Get_Justify (Layout : access Pango_Layout_Record) return Boolean;
+   --  Return True if each complete line should be stretched to fill the entire
+   --  width of the layout.
+
+   procedure Set_Alignment
+     (Layout    : access Pango_Layout_Record'Class;
+      Alignment : Pango_Alignment);
+   --  Set the alignment for the layout (how partial lines are positioned
+   --  within the horizontal space available).
+
+   function Get_Alignment (Layout : access Pango_Layout_Record)
+     return Pango_Alignment;
+   --  Return the alignment for the layout.
+
+   procedure Set_Width
+     (Layout : access Pango_Layout_Record; Width : Glib.Gint);
+   --  Set the width to which the lines of Layout should be wrapped. No
+   --  wrapping will be performed if Width is -1.
+   --  Width is given in pango units.
+
+   function Get_Width (Layout : access Pango_Layout_Record) return Glib.Gint;
+   --  Return the wrapping width of Layout
+
+   procedure Set_Wrap
+     (Layout : access Pango_Layout_Record; Mode : Pango_Wrap_Mode);
+   --  Sets the wrap style; the wrap style only has an effect if a width is set
+   --  on the layout with pango_layout_set_width(). To turn off wrapping, set
+   --  the width to -1.
+
+   function Get_Wrap (Layout : access Pango_Layout_Record)
+      return Pango_Wrap_Mode;
+   --  Return the current wrap style
+
 private
-   pragma Import
-     (C, Set_Font_Description, "pango_layout_set_font_description");
+   type Pango_Layout_Record is new Glib.Object.GObject_Record with null record;
 end Pango.Layout;
+
+
+--  missing:
+--  pango_layout_get_context
+--  pango_layout_copy
+--  pango_layout_set_attributes
+--  pango_layout_get_attributes
+--  pango_layout_set_markup
+--  pango_layout_set_markup_with_accel
+--  pango_layout_set_indent
+--  pango_layout_get_indent
+--  pango_layout_set_spacing
+--  pango_layout_get_spacing
+--  pango_layout_set_tabs
+--  pango_layout_get_tabs
+--  pango_layout_set_single_paragraph_mode
+--  pango_layout_get_single_paragraph_mode
+--  pango_layout_get_log_attrs
+--  pango_layout_index_to_pos
+--  pango_layout_get_cursor_pos
+--  pango_layout_move_cursor_visually
+--  pango_layout_xy_to_index
+--  pango_layout_get_line
+--  pango_layout_get_lines
+--  pango_layout_line_ref
+--  pango_layout_line_unref
+--  pango_layout_line_x_to_index
+--  pango_layout_line_index_to_x
+--  pango_layout_line_get_x_ranges
+--  pango_layout_line_get_extents
+--  pango_layout_line_get_pixel_extents
+--  pango_layout_get_iter
+--  pango_layout_iter_free
+--  pango_layout_iter_get_index
+--  pango_layout_iter_get_run
+--  pango_layout_iter_get_line
+--  pango_layout_iter_at_last_line
+--  pango_layout_iter_next_char
+--  pango_layout_iter_next_cluster
+--  pango_layout_iter_next_run
+--  pango_layout_iter_next_line
+--  pango_layout_iter_get_char_extents
+--  pango_layout_iter_get_cluster_extents
+--  pango_layout_iter_get_run_extents
+--  pango_layout_iter_get_line_extents
+--  pango_layout_iter_get_line_yrange
+--  pango_layout_iter_get_layout_extents
+--  pango_layout_iter_get_baseline
+
+
