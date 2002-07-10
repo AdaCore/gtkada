251088376a6a24d94d6dd132cd6bb073ff54424f
Emmanuel Briot <briot@adacore.com>
2002-07-10 12:31:33 +0000

    (Add_Entry, Lookup_Entry, Change_Entry): New subprograms

--- src/gtk-accel_map.adb
+++ src/gtk-accel_map.adb
@@ -26,6 +26,9 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
+with Gdk.Types;       use Gdk.Types;
+with Gtk.Accel_Group; use Gtk.Accel_Group;
+
 package body Gtk.Accel_Map is
 
    ----------
@@ -49,5 +52,59 @@ package body Gtk.Accel_Map is
    begin
       Internal (File_Name & ASCII.NUL);
    end Load;
+
+   ---------------
+   -- Add_Entry --
+   ---------------
+
+   procedure Add_Entry
+     (Accel_Path : String;
+      Accel_Key  : Gdk.Types.Gdk_Key_Type;
+      Accel_Mods : Gdk.Types.Gdk_Modifier_Type)
+   is
+      procedure Internal (P : String; K : Gdk_Key_Type; M : Gdk_Modifier_Type);
+      pragma Import (C, Internal, "gtk_accel_map_add_entry");
+   begin
+      Internal (Accel_Path & ASCII.NUL, Accel_Key, Accel_Mods);
+   end Add_Entry;
+
+   ------------------
+   -- Lookup_Entry --
+   ------------------
+
+   procedure Lookup_Entry
+     (Accel_Path : String;
+      Key        : out Gtk.Accel_Group.Gtk_Accel_Key;
+      Found      : out Boolean)
+   is
+      type Gtk_Accel_Key_Access is access all Gtk_Accel_Key;
+      function Internal
+        (Path : String; Key : Gtk_Accel_Key_Access) return Integer;
+      pragma Import (C, Internal, "gtk_accel_map_lookup_entry");
+
+      K : aliased Gtk_Accel_Key;
+
+   begin
+      Found := Boolean'Val (Internal (Accel_Path & ASCII.NUL, K'Access));
+      Key := K;
+   end Lookup_Entry;
+
+   ------------------
+   -- Change_Entry --
+   ------------------
+
+   procedure Change_Entry
+     (Accel_Path : String;
+      Accel_Key  : Gdk.Types.Gdk_Key_Type;
+      Accel_Mods : Gdk.Types.Gdk_Modifier_Type;
+      Replace    : Boolean)
+   is
+      procedure Internal
+        (P : String; K : Gdk_Key_Type; M : Gdk_Modifier_Type; R : Integer);
+      pragma Import (C, Internal, "gtk_accel_map_change_entry");
+   begin
+      Internal (Accel_Path & ASCII.NUL, Accel_Key, Accel_Mods,
+                Boolean'Pos (Replace));
+   end Change_Entry;
 
 end Gtk.Accel_Map;
--- src/gtk-accel_map.ads
+++ src/gtk-accel_map.ads
@@ -26,6 +26,9 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
+with Gdk.Types;
+with Gtk.Accel_Group;
+
 package Gtk.Accel_Map is
 
    procedure Save (File_Name : String);
@@ -36,4 +39,54 @@ package Gtk.Accel_Map is
    procedure Load (File_Name : String);
    --  Load the key shortcuts from a file
 
+   procedure Add_Entry
+     (Accel_Path : String;
+      Accel_Key  : Gdk.Types.Gdk_Key_Type;
+      Accel_Mods : Gdk.Types.Gdk_Modifier_Type);
+   --  Register a new accelerator for a given menu item, within the global
+   --  accelerator map.
+   --  This function should only be called once per Accel_Path. To change it
+   --  programmatically duing runtime, use Change_Entry.
+   --  Accel_Path is of the form:
+   --     <app>/Category1/Category2/.../Action",
+   --  where "app" is a unique, application-specific identifier (for examples
+   --  of valid Accel_Path, check the file created by Save above).
+   --
+   --  For instance, the path in the testgtk application for the menu
+   --  File->Open would be
+   --     <testgtk>/file/open
+   --
+   --  It is better to use this function instead of Add_Accelerator, since when
+   --  the accelerators are changed interactively by the user, the new value
+   --  will be shown properly in the menu, which wouldn't happen if they had
+   --  been forced by Add_Accelerator.
+
+   procedure Lookup_Entry
+     (Accel_Path : String;
+      Key        : out Gtk.Accel_Group.Gtk_Accel_Key;
+      Found      : out Boolean);
+   --  Look up the accelerator for Accel_Path, and set Key appropriately. If no
+   --  accelerator was set, Found is set to False, and the value of Key is
+   --  meaningless.
+
+   procedure Change_Entry
+     (Accel_Path : String;
+      Accel_Key  : Gdk.Types.Gdk_Key_Type;
+      Accel_Mods : Gdk.Types.Gdk_Modifier_Type;
+      Replace    : Boolean);
+   --  Change the accelerator currently associated wtih Accel_Path.
+   --  A change may not always be possible due to conflicts with other
+   --  accelerators. Replace should be set to True if other accelerators may be
+   --  deleted to resolve such conflicts.
+
+
 end Gtk.Accel_Map;
+
+
+--  Missing:
+--    gtk_accel_map_foreach
+--    gtk_accel_map_load_fd
+--    gtk_accel_map_load_scanner
+--    gtk_accel_map_save_fd
+--    gtk_accel_map_add_filter
+--    gtk_accel_map_foreach_unfiltered
