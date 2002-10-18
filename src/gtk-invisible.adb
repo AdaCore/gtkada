db9bb0908a779636c34bceed04518f7984c0af5e
Arnaud Charlet <charlet@adacore.com>
2002-10-18 12:02:03 +0000

    Remove unused porcedure now detected by GNAT.

--- src/gtk-invisible.adb
+++ src/gtk-invisible.adb
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2001 ACT-Europe                 --
+--                Copyright (C) 2000-2002 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -36,9 +36,6 @@ package body Gtk.Invisible is
    -------------
 
    procedure Gtk_New (Widget : out Gtk_Invisible) is
-      function Internal return System.Address;
-      pragma Import (C, Internal, "gtk_invisible_new");
-
    begin
       Widget := new Gtk_Invisible_Record;
       Gtk.Invisible.Initialize (Widget);
