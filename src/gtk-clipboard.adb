3b87cc060901d984ea77a99d79f4c2489cc7bc53
Nicolas Setton <setton@adacore.com>
2002-12-11 13:55:19 +0000

    Initial revision for this file.

---
+++ src/gtk-clipboard.adb
@@ -0,0 +1,48 @@
+-----------------------------------------------------------------------
+--               GtkAda - Ada95 binding for Gtk+/Gnome               --
+--                                                                   --
+--                   Copyright (C) 2002 ACT-Europe                   --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+--  <c_version>2.0.0</c_version>
+
+with Gdk.Types;
+
+package body Gtk.Clipboard is
+   procedure Set_Text
+     (Clipboard : Gtk_Clipboard;
+      Text      : String)
+   is
+      procedure Internal
+        (Clipboard : Gtk_Clipboard;
+         Str       : String;
+         Len       : Integer);
+      pragma Import (C, Internal, "gtk_clipboard_set_text");
+
+   begin
+      Internal (Clipboard, Text & ASCII.NUL, Text'Length);
+   end Set_Text;
+
+end Gtk.Clipboard;
