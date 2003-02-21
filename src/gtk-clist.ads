a2c3347ccf1d43c5e47bd36c16a30d7a6647429d
Arnaud Charlet <charlet@adacore.com>
2003-02-21 15:55:22 +0000

    Clarify comment.

--- src/gtk-clist.ads
+++ src/gtk-clist.ads
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2002 ACT-Europe                 --
+--                Copyright (C) 2000-2003 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -398,8 +398,8 @@ package Gtk.Clist is
       Column  : in Gint;
       Visible : in Boolean);
    --  Modify the visibility of a column.
-   --  Note that GtkAda prevents the last visible column to be hidden. Nothing
-   --  will be done if you try to hide that last column.
+   --  Note that GtkAda prevents the last remaining visible column to be
+   --  hidden. Nothing will be done if you try to hide that last column.
    --  See the example below for an example how to hide all the columns but
    --  one.
 
