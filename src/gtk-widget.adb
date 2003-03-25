f78c50040d9d2d93be18ffa0a7871cc0f5aa1882
Arnaud Charlet <charlet@adacore.com>
2003-03-25 10:38:10 +0000

    Update copyright notice. Minor reformatting.

--- src/gtk-widget.adb
+++ src/gtk-widget.adb
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2002 ACT-Europe                 --
+--                Copyright (C) 2000-2003 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -1526,6 +1526,7 @@ package body Gtk.Widget is
         (Child : System.Address;
          Dir   : Gtk.Enums.Gtk_Direction_Type) return Integer;
       pragma Import (C, Internal, "gtk_widget_child_focus");
+
    begin
       return Boolean'Val (Internal (Get_Object (Child), Direction));
    end Child_Focus;
