d1e9e09815f05adc44e0f6248cd10dc6b36cb5b8
Arnaud Charlet <charlet@adacore.com>
2003-05-05 13:27:26 +0000

    Update copyright notice.

--- src/gtk-object.ads
+++ src/gtk-object.ads
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2002 ACT-Europe                 --
+--                Copyright (C) 2000-2003 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
