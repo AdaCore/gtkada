2136a7df8e301e79a85ee387ce3194b9b974e47b
Pascal Obry <obry@adacore.com>
2004-02-04 09:51:14 +0000

    Update copyright notice.

--- src/gtk-arguments.ads
+++ src/gtk-arguments.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
---   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
+--   Copyright (C) 1998-2004 E. Briot, J. Brobecker and A. Charlet   --
 --                Copyright (C) 2000-2003 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
