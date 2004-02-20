3f8c86f6c6d8daa5d33569cefdc579c30008a99a
Nicolas Setton <setton@adacore.com>
2004-02-20 16:37:41 +0000

    Add a line to compile Gtk.Icon_Factory.

--- src/make_gtk.adb
+++ src/make_gtk.adb
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2003 ACT-Europe                 --
+--                Copyright (C) 2000-2004 ACT-Europe                 --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -118,6 +118,7 @@ with Gtk.GRange;
 with Gtk.Handle_Box;
 with Gtk.Handlers;
 with Gtk.Hbutton_Box;
+with Gtk.Icon_Factory;
 with Gtk.Image;
 with Gtk.Image_Menu_Item;
 with Gtk.Input_Dialog;
