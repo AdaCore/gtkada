272c36349eadf76f4b2732b42a340202ae763ec5
Arnaud Charlet <charlet@adacore.com>
2005-09-29 07:41:27 +0000

    Update copyright notice.

--- src/gtk-extra-sheet.ads
+++ src/gtk-extra-sheet.ads
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --      Copyright (C) 2000 E. Briot, J. Brobecker and A. Charlet     --
---                Copyright (C) 2000-2003 ACT-Europe                 --
+--                Copyright (C) 2000-2005 AdaCore                    --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
