f08f356db6d129fcd84af68f60557e8bc982cb8a
Florian Villoing <villoing@adacore.com>
2005-10-17 07:54:20 +0000

    (Select_Range): Pass a Gtk_Sheet_Range to the GTK subprogram instead of an  access to it.
    Fix EA03-011.

--- src/gtk-extra-sheet.adb
+++ src/gtk-extra-sheet.adb
@@ -773,13 +773,12 @@ package body Gtk.Extra.Sheet is
       The_Range : Gtk_Sheet_Range)
    is
       procedure Internal
-        (Sheet : System.Address; The_Range : access Gtk_Sheet_Range);
+        (Sheet     : System.Address;
+         The_Range : Gtk_Sheet_Range);
       pragma Import (C, Internal, "gtk_sheet_select_range");
 
-      R : aliased Gtk_Sheet_Range := The_Range;
-
    begin
-      Internal (Get_Object (Sheet), R'Access);
+      Internal (Get_Object (Sheet), The_Range);
    end Select_Range;
 
    --------------------
