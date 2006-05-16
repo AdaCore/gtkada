ff0fcd22dc62dc58ef9a21416fe3620e25fc6995
Emmanuel Briot <briot@adacore.com>
2006-05-16 12:07:44 +0000

    Ported to gtk+ 2.8.17 (F515-002)

--- src/gtk-enums.ads
+++ src/gtk-enums.ads
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                Copyright (C) 2000-2003 ACT-Europe                 --
+--                Copyright (C) 2000-2006 AdaCore                    --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -27,7 +27,11 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
---  <c_version>1.3.11</c_version>
+--  <description>
+--  This package contains a number of types that are shared by several
+--  widgets in GtkAda.
+--  </description>
+--  <c_version>2.8.17</c_version>
 
 with Glib;
 with Glib.Generic_Properties;  use Glib.Generic_Properties;
@@ -49,7 +53,6 @@ package Gtk.Enums is
       Anchor_South_West,
       Anchor_West,
       Anchor_East);
-   pragma Convention (C, Gtk_Anchor_Type);
    --  Gtk_Anchor_Type indicates the exact location of the widget on its
    --  side. Note that not all anchors are relevant for each side.
    --
@@ -62,6 +65,8 @@ package Gtk.Enums is
    --  will only appear on the second line in the container. The order the
    --  children are inserted into the container is important.
 
+   pragma Convention (C, Gtk_Anchor_Type);
+
    Anchor_N  : Gtk_Anchor_Type renames Anchor_North;
    Anchor_NW : Gtk_Anchor_Type renames Anchor_North_West;
    Anchor_NE : Gtk_Anchor_Type renames Anchor_North_East;
@@ -71,10 +76,16 @@ package Gtk.Enums is
    Anchor_W  : Gtk_Anchor_Type renames Anchor_West;
    Anchor_E  : Gtk_Anchor_Type renames Anchor_East;
 
-   type Gtk_Arrow_Type is (Arrow_Up, Arrow_Down, Arrow_Left, Arrow_Right);
-   pragma Convention (C, Gtk_Arrow_Type);
+   type Gtk_Arrow_Type is
+     (Arrow_Up,
+      Arrow_Down,
+      Arrow_Left,
+      Arrow_Right);
+   --  The various types of arrows that can be represented by GtkAda
 
    type Gtk_Attach_Options is new Glib.Guint32;
+   --  The various options used for attaching widgets to tables
+
    Expand : constant Gtk_Attach_Options := 1;
    Shrink : constant Gtk_Attach_Options := 2;
    Fill   : constant Gtk_Attach_Options := 4;
@@ -85,39 +96,31 @@ package Gtk.Enums is
       Buttonbox_Edge,
       Buttonbox_Start,
       Buttonbox_End);
-   pragma Convention (C, Gtk_Button_Box_Style);
+   --  The style for button boxes (see gtk-button_box.ads)
 
    type Gtk_Curve_Type is
-     (Curve_Type_Linear,
-      --  Linear interpolation
-
-      Curve_Type_Spline,
-      --  Spline interpolation
-
-      Curve_Type_Free
-      --  Free form curve
-     );
-   pragma Convention (C, Gtk_Curve_Type);
+     (Curve_Type_Linear,       --  Linear interpolation
+      Curve_Type_Spline,       --  Spline interpolation
+      Curve_Type_Free);        --  Free form curve
+   --  The curve types that can be used in gtk-curve.ads
 
    type Gtk_Delete_Type is
      (Delete_Chars,
       Delete_Word_Ends,
-      --  Delete only the portion of the word to the left/right of the cursor
-      --  if we are in the middle of a word.
-
       Delete_Words,
       Delete_Display_Lines,
       Delete_Display_Line_Ends,
       Delete_Paragraph_Ends,
-      --  Like C-k in Emacs: delete until end of line
-
       Delete_Paragraphs,
-      --  C-k in pico: delete while line
-
-      Delete_Whitespace
-      --  M-\ in Emacs
-     );
-   pragma Convention (C, Gtk_Delete_Type);
+      Delete_Whitespace);
+   --  The deletion modes used in the text editor.
+   --  Delete_Word_Ends will delete only the portion of the word to the
+   --  left/right of the cursor if we are in the middle of a word.
+   --  Delete_Paragraph_Ends acts like c-k in Emacs: it deletes the text until,
+   --  but not including, the end of line.
+   --  Delete_Paragraphs acts like c-k in pico: it deletes the whole line.
+   --  Delete_Whitespace acts like M-\ in Emacs, and removes all white spaces
+   --  surrounding the cursor.
 
    type Gtk_Direction_Type is
      (Dir_Tab_Forward,
@@ -126,7 +129,6 @@ package Gtk.Enums is
       Dir_Down,
       Dir_Left,
       Dir_Right);
-   pragma Convention (C, Gtk_Direction_Type);
    --  Focus movement types
 
    type Gtk_Expander_Style is
@@ -134,8 +136,7 @@ package Gtk.Enums is
       Expander_Semi_Collapsed,
       Expander_Semi_Expanded,
       Expander_Expanded);
-   pragma Convention (C, Gtk_Expander_Style);
-   --  Expander styles
+   --  Expander styles, as seen in trees
 
    type Gtk_Icon_Size is
      (Icon_Size_Invalid,
@@ -145,59 +146,36 @@ package Gtk.Enums is
       Icon_Size_Button,
       Icon_Size_Dnd,
       Icon_Size_Dialog);
-   pragma Convention (C, Gtk_Icon_Size);
-   --  Built-in stock icon sizes
-
-   type Gtk_Side_Type is
-     (Side_Top,
-      Side_Bottom,
-      Side_Left,
-      Side_Right);
-   pragma Convention (C, Gtk_Side_Type);
-   --  Gtk_Side_Type indicates on which the widget should be inserted.
-   --  The children are displayed in the order they were inserted into the
-   --  container.
-   --  Each time a child is displayed, the available space for the remaining
-   --  child is restrained. For instance, every time you put a child on the
-   --  Side_Top or Side_Bottom, the available space is decreased so that no
-   --  other widget is inserted in the same line.
-   --
-   --  For instance, if you put two widgets on Side_Top, the second one will
-   --  appear below the first one. If you add two widgets on Side_Right, the
-   --  second one will be placed on the left of the first.
+   --  Built-in stock icon sizes. Depending on the context, icons should be
+   --  displayed larger or smaller (typically, icons in menus are much smaller
+   --  than icons in toolbars for instance).
 
    type Gtk_Text_Direction is
      (Text_Dir_None,
       Text_Dir_Ltr,
       Text_Dir_Rtl);
-   pragma Convention (C, Gtk_Text_Direction);
+   --  The directory in which text should be written (left to right or
+   --  right to left).
 
    type Gtk_Justification is
      (Justify_Left,
       Justify_Right,
       Justify_Center,
       Justify_Fill);
-   pragma Convention (C, Gtk_Justification);
-
-   type Gtk_Match_Type is
-     (Match_All,
-      Match_All_Tail,
-      Match_Head,
-      Match_Tail,
-      Match_Exact,
-      Match_Last);
-   pragma Convention (C, Gtk_Match_Type);
+   --  Within a paragraph, text can be justified in various ways: aligned on
+   --  the left, aligned on the right, centered, or justified (in which case
+   --  the width of the spaces might vary so that the text is aligned on both
+   --  sides).
 
    type Gtk_Menu_Direction_Type is
      (Menu_Dir_Parent,
       Menu_Dir_Child,
       Menu_Dir_Next,
       Menu_Dir_Prev);
-   pragma Convention (C, Gtk_Menu_Direction_Type);
    --  Direction where to move the selection.
 
    type Gtk_Metric_Type is (Pixels, Inches, Centimeters);
-   pragma Convention (C, Gtk_Metric_Type);
+   --  The unit to use when you display a ruler at the top of a drawing area.
 
    type Gtk_Movement_Step is
      (Movement_Logical_Positions, --  move by forw/back graphemes
@@ -208,28 +186,42 @@ package Gtk.Enums is
       Movement_Paragraphs,        --  move up/down paragraphs
       Movement_Paragraph_Ends,    --  move to either end of a paragraph
       Movement_Pages,             --  move by pages
-      Movement_Buffer_Ends);      --  move to ends of the buffer
-   pragma Convention (C, Gtk_Movement_Step);
+      Movement_Buffer_Ends,       --  move to ends of the buffer
+      Movement_Horizontal_Pages); --  move horizontally by pages
+   --  The various kind of movements that can be performed within an editor.
+   --  Gtk+ has builtin knowledge of all of the above.
    --  Note that a paragraph is defined as a new-line ended line.
 
+   type Gtk_Scroll_Step is
+     (Scroll_Steps,
+      Scroll_Pages,
+      Scroll_Ends,
+      Scroll_Horizontal_Steps,
+      Scroll_Horizontal_Pages,
+      Scroll_Horizontal_Ends);
+   --  The behavior of scrollbars for editors
+
    type Gtk_Orientation is (Orientation_Horizontal, Orientation_Vertical);
-   pragma Convention (C, Gtk_Orientation);
+   --  Orientation of widgets. Most widgets have no such notion, but for
+   --  instance toolbars can display different kind of information depending
+   --  on their current orientation
 
    type Gtk_Corner_Type is
      (Corner_Top_Left,
       Corner_Bottom_Left,
       Corner_Top_Right,
       Corner_Bottom_Right);
-   pragma Convention (C, Gtk_Corner_Type);
    --  Type used by Set_Placement below to determine the location of the
    --  child widget with respect to the scrollbars.
    --  Corner_Top_Left means the child is in the top left, with the scrollbars
    --  underneath and to the right.
 
    type Gtk_Pack_Type is (Pack_Start, Pack_End);
-   pragma Convention (C, Gtk_Pack_Type);
+   --  Whether items should be added at the start or at the end of the list of
+   --  children for a widget. This impacts the visual rendering of containers
 
    type Gtk_Path_Priority_Type is mod 2 ** 32;
+   --  Priorities for path lookups
 
    Path_Prio_Lowest      : constant Gtk_Path_Priority_Type := 0;
    Path_Prio_Gtk         : constant Gtk_Path_Priority_Type := 4;
@@ -237,35 +229,32 @@ package Gtk.Enums is
    Path_Prio_Theme       : constant Gtk_Path_Priority_Type := 10;
    Path_Prio_RC          : constant Gtk_Path_Priority_Type := 12;
    Path_Prio_Highest     : constant Gtk_Path_Priority_Type := 15;
-   --  Priorities for path lookups
-
    Path_Prio_Mask        : constant Gtk_Path_Priority_Type := 16#0f#;
 
    type Gtk_Path_Type is (Path_Widget, Path_Widget_Class, Path_Class);
-   pragma Convention (C, Gtk_Path_Type);
    --  Widget path types
 
    type Gtk_Policy_Type is (Policy_Always, Policy_Automatic, Policy_Never);
-   pragma Convention (C, Gtk_Policy_Type);
+   --  When should scrollbars be made visible in Gtk_Scrolled_Window
 
    type Gtk_Position_Type is
      (Pos_Left,
       Pos_Right,
       Pos_Top,
       Pos_Bottom);
-   pragma Convention (C, Gtk_Position_Type);
-
-   type Gtk_Preview_Type is (Preview_Color, Preview_Grayscale);
-   pragma Convention (C, Gtk_Preview_Type);
+   --  Use to define the position of children within a container
 
    type Gtk_Relief_Style is (Relief_Normal, Relief_Half, Relief_None);
-   pragma Convention (C, Gtk_Relief_Style);
+   --  Explains how the border of widgets should be displayed
 
    type Gtk_Resize_Mode is
      (Resize_Parent,     --  Pass request to the parent
       Resize_Queue,      --  Queue resizes on this widget
       Resize_Immediate); --  Perform the resizes now
-   pragma Convention (C, Gtk_Resize_Mode);
+   --  The resizing of widgets is generally done asynchronously, for efficiency
+   --  reasons. This can have some impact on the visual rendering of the widget
+   --  which might be an issue in some cases.
+   --  This type is only used when you are writting your own containers.
 
    type Gtk_Scroll_Type is
      (Scroll_None,
@@ -284,16 +273,16 @@ package Gtk.Enums is
       Scroll_Page_Right,
       Scroll_Start,
       Scroll_End);
-   pragma Convention (C, Gtk_Scroll_Type);
+   --  How clists should be scrolled
 
    type Gtk_Selection_Mode is
      (Selection_None,
       Selection_Single,
       Selection_Browse,
       Selection_Multiple);
-   pragma Convention (C, Gtk_Selection_Mode);
-   --  The old Selection_Extended is now deprected, and replaced by
-   --  Selection_Multiple.
+   --  Indicates what selection is allowed in a tree (no selection allowed, a
+   --  single line, a single line when the mouse is released, or multiple
+   --  lines).
 
    type Gtk_Shadow_Type is
      (Shadow_None,
@@ -301,7 +290,7 @@ package Gtk.Enums is
       Shadow_Out,
       Shadow_Etched_In,
       Shadow_Etched_Out);
-   pragma Convention (C, Gtk_Shadow_Type);
+   --  The type of shadows that can be drawn around widgets
 
    type Gtk_State_Type is
      (State_Normal,
@@ -309,13 +298,60 @@ package Gtk.Enums is
       State_Prelight,
       State_Selected,
       State_Insensitive);
-   pragma Convention (C, Gtk_State_Type);
+   --  Widgets can be in various states. This impacts their visual rendering,
+   --  but can also impact whether they react to events or not (they do not
+   --  when in State_Insensitive mode).
 
-   type Gtk_Submenu_Direction is (Direction_Left, Direction_Right);
-   pragma Convention (C, Gtk_Submenu_Direction);
+   type Gtk_Toolbar_Style is
+     (Toolbar_Icons,
+      Toolbar_Text,
+      Toolbar_Both,
+      Toolbar_Both_Horiz);
+   --  The style of toolbars. Toolbar_Both_Horiz indicates that both icon and
+   --  text should be displayed, arranged horizontally.
 
-   type Gtk_Submenu_Placement is (Top_Bottom, Left_Right);
-   pragma Convention (C, Gtk_Submenu_Placement);
+   type Gtk_Update_Type is
+     (Update_Continuous,
+      Update_Discontinuous,
+      Update_Delayed);
+   --  For some widgets, this indicates how often they should be updated
+
+   type Gtk_Visibility is
+     (Visibility_None,
+      Visibility_Partial,
+      Visibility_Full);
+   --  Generic visibility flags. This indicate how visible a window currently
+   --  is.
+
+   type Gtk_Window_Position is
+     (Win_Pos_None,
+      Win_Pos_Center,
+      Win_Pos_Mouse,
+      Win_Pos_Center_Always,
+      Win_Pos_Center_On_Parent);
+   --  The position at which a new window should be initially displayed on the
+   --  screen.
+
+   type Gtk_Window_Type is
+     (Window_Toplevel,
+      Window_Popup);
+   --  GtkAda supports multiple types of windows. They all act as top-level
+   --  containers, but the amount of decoration is different. A popup window
+   --  has no title bar for instance.
+
+   type Gtk_Wrap_Mode is
+     (Wrap_None,
+      Wrap_Char,
+      Wrap_Word,
+      Wrap_Word_Char);
+   --  Text wrapping algorithm. This indicates where a text widget is allowed
+   --  to break its contents to display multiple lines when a line doesn't fit
+   --  on the screen.
+
+   type Gtk_Sort_Type is
+     (Sort_Ascending,
+      Sort_Descending);
+   --  How to sort
 
    type Gtk_Text_Window_Type is
      (Text_Window_Private,
@@ -325,65 +361,11 @@ package Gtk.Enums is
       Text_Window_Right,
       Text_Window_Top,
       Text_Window_Bottom);
-   pragma Convention (C, Gtk_Text_Window_Type);
+   --  The various components of a Gtk.Text_View widget
 
-   type Gtk_Toolbar_Child_Type is
-     (Toolbar_Child_Space,
-      Toolbar_Child_Button,
-      Toolbar_Child_Togglebutton,
-      Toolbar_Child_Radiobutton,
-      Toolbar_Child_Widget);
-   pragma Convention (C, Gtk_Toolbar_Child_Type);
-
-   type Gtk_Toolbar_Style is
-     (Toolbar_Icons,
-      Toolbar_Text,
-      Toolbar_Both,
-      Toolbar_Both_Horiz);
-   pragma Convention (C, Gtk_Toolbar_Style);
-   --  The style of toolbars. Toolbar_Both_Horiz indicates that both icon and
-   --  text should be displayed, arranged horizontally.
-
-   type Gtk_Toolbar_Space_Style is (Toolbar_Space_Empty, Toolbar_Space_Line);
-   pragma Convention (C, Gtk_Toolbar_Space_Style);
-
-   type Gtk_Tree_View_Mode is (Tree_View_Line, Tree_View_Item);
-   pragma Convention (C, Gtk_Tree_View_Mode);
-
-   type Gtk_Update_Type is
-     (Update_Continuous,
-      Update_Discontinuous,
-      Update_Delayed);
-   pragma Convention (C, Gtk_Update_Type);
-
-   type Gtk_Visibility is
-     (Visibility_None, Visibility_Partial, Visibility_Full);
-   pragma Convention (C, Gtk_Visibility);
-
-   type Gtk_Window_Position is
-     (Win_Pos_None,
-      Win_Pos_Center,
-      Win_Pos_Mouse,
-      Win_Pos_Center_Always,
-      Win_Pos_Center_On_Parent);
-   pragma Convention (C, Gtk_Window_Position);
-
-   type Gtk_Window_Type is (Window_Toplevel, Window_Popup);
-   pragma Convention (C, Gtk_Window_Type);
-
-   type Gtk_Wrap_Mode is
-     (Wrap_None,
-      Wrap_Char,
-      Wrap_Word);
-   pragma Convention (C, Gtk_Wrap_Mode);
-
-   type Gtk_Sort_Type is
-     (Sort_Ascending,
-      Sort_Descending);
-   pragma Convention (C, Gtk_Sort_Type);
-   --  How to sort
-
-   --  Some Glib instantiations
+   -------------------------------
+   --  Some Glib instantiations --
+   -------------------------------
 
    function Convert (S : String) return System.Address;
    function Convert (S : System.Address) return String;
@@ -410,6 +392,39 @@ package Gtk.Enums is
    pragma Import (C, Convert_UA, "convert_ua");
    package Guint_List is new
      Glib.Glist.Generic_List (Guint, Convert_UI, Convert_UA);
+
+   pragma Convention (C, Gtk_Arrow_Type);
+   pragma Convention (C, Gtk_Button_Box_Style);
+   pragma Convention (C, Gtk_Curve_Type);
+   pragma Convention (C, Gtk_Delete_Type);
+   pragma Convention (C, Gtk_Direction_Type);
+   pragma Convention (C, Gtk_Path_Type);
+   pragma Convention (C, Gtk_Expander_Style);
+   pragma Convention (C, Gtk_Icon_Size);
+   pragma Convention (C, Gtk_Text_Direction);
+   pragma Convention (C, Gtk_Justification);
+   pragma Convention (C, Gtk_Menu_Direction_Type);
+   pragma Convention (C, Gtk_Metric_Type);
+   pragma Convention (C, Gtk_Movement_Step);
+   pragma Convention (C, Gtk_Orientation);
+   pragma Convention (C, Gtk_Corner_Type);
+   pragma Convention (C, Gtk_Pack_Type);
+   pragma Convention (C, Gtk_Policy_Type);
+   pragma Convention (C, Gtk_Position_Type);
+   pragma Convention (C, Gtk_Relief_Style);
+   pragma Convention (C, Gtk_Resize_Mode);
+   pragma Convention (C, Gtk_Scroll_Type);
+   pragma Convention (C, Gtk_Selection_Mode);
+   pragma Convention (C, Gtk_Shadow_Type);
+   pragma Convention (C, Gtk_State_Type);
+   pragma Convention (C, Gtk_Text_Window_Type);
+   pragma Convention (C, Gtk_Toolbar_Style);
+   pragma Convention (C, Gtk_Update_Type);
+   pragma Convention (C, Gtk_Visibility);
+   pragma Convention (C, Gtk_Window_Position);
+   pragma Convention (C, Gtk_Window_Type);
+   pragma Convention (C, Gtk_Wrap_Mode);
+   pragma Convention (C, Gtk_Sort_Type);
 
    ----------------
    -- Properties --
@@ -462,5 +477,46 @@ package Gtk.Enums is
      Window_Position_Properties.Property;
    type Property_Gtk_Text_Direction is new Text_Direction_Properties.Property;
    type Property_Gtk_Wrap_Mode     is new Wrap_Mode_Properties.Property;
+
+   -----------------
+   -- Obsolescent --
+   -----------------
+   --  All subprograms below are now obsolescent in gtk+. They might be removed
+   --  from future versions of gtk+ (and therefore GtkAda).
+   --  To find out whether your code uses any of these, we recommend compiling
+   --  with the -gnatwj switch
+   --  <doc_ignore>
+
+   type Gtk_Side_Type is (Side_Top, Side_Bottom, Side_Left, Side_Right);
+   --  pragma Obsolescent;
+   pragma Convention (C, Gtk_Side_Type);
+
+   type Gtk_Match_Type is
+     (Match_All, Match_All_Tail, Match_Head, Match_Tail,
+      Match_Exact, Match_Last);
+   --  pragma Obsolescent;
+   pragma Convention (C, Gtk_Match_Type);
+
+   type Gtk_Preview_Type is (Preview_Color, Preview_Grayscale);
+   --  pragma Obsolescent;
+   pragma Convention (C, Gtk_Preview_Type);
+
+   type Gtk_Submenu_Direction is (Direction_Left, Direction_Right);
+   --  pragma Obsolescent;
+
+   type Gtk_Submenu_Placement is (Top_Bottom, Left_Right);
+   --  pragma Obsolescent;
+
+   type Gtk_Toolbar_Space_Style is (Toolbar_Space_Empty, Toolbar_Space_Line);
+   --  pragma Obsolescent;
+
+   type Gtk_Tree_View_Mode is (Tree_View_Line, Tree_View_Item);
+   --  pragma Obsolescent;
+
+   pragma Convention (C, Gtk_Submenu_Direction);
+   pragma Convention (C, Gtk_Submenu_Placement);
+   pragma Convention (C, Gtk_Toolbar_Space_Style);
+   pragma Convention (C, Gtk_Tree_View_Mode);
+   --  </doc_ignore>
 
 end Gtk.Enums;
