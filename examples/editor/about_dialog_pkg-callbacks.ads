d2b8e1c8e8821c0d05cef226b6d1bd60bf67ac9f
Emmanuel Briot <briot@adacore.com>
2006-06-22 10:16:11 +0000

    Removed, since this demo depended on Gtk_Text, which is now almost  unsupported by gtk+ (F622-008)

--- examples/editor/about_dialog_pkg-callbacks.ads
+++
@@ -1,8 +0,0 @@
-with Gtk.Arguments;
-with Gtk.Widget; use Gtk.Widget;
-
-package About_Dialog_Pkg.Callbacks is
-   procedure On_About_Ok_Clicked
-     (Object : access Gtk_Button_Record'Class);
-
-end About_Dialog_Pkg.Callbacks;
