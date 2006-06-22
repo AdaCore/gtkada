95a36ccb27e5515a6b017312045e1feb39ef906b
Emmanuel Briot <briot@adacore.com>
2006-06-22 15:35:55 +0000

    Put back the interface in gtk.old_editable. This simulates the gtk+  interface GtkEditable for Gtk_Text, so that existing code depending on  the latter can still be compiled (F622-015)

--- src/gtk-old_editable.ads
+++ src/gtk-old_editable.ads
@@ -40,40 +40,121 @@
 with Gtk.Widget;
 
 package Gtk.Old_Editable is
+   pragma Obsolescent;
 
    type Gtk_Old_Editable_Record is new
      Gtk.Widget.Gtk_Widget_Record with private;
    type Gtk_Old_Editable is access all Gtk_Old_Editable_Record'Class;
 
-   -----------------
-   -- Obsolescent --
-   -----------------
-   --  All subprograms below are now obsolescent in gtk+. They might be removed
-   --  from future versions of gtk+ (and therefore GtkAda).
-   --  To find out whether your code uses any of these, we recommend compiling
-   --  with the -gnatwj switch
-   --  <doc_ignore>
+   function Get_Type return Gtk.Gtk_Type;
+   --  Return the internal value associated with a Gtk_Old_Editable.
 
    procedure Changed (Editable : access Gtk_Old_Editable_Record);
-   pragma Obsolescent; --  Changed
    --  Cause the "changed" signal to be emitted.
-
-   function Get_Type return Gtk.Gtk_Type;
-   pragma Obsolescent; --  Get_Type
-   --  Return the internal value associated with a Gtk_Old_Editable.
 
    procedure Claim_Selection
      (Editable : access Gtk_Old_Editable_Record;
       Claim    : in Boolean := True;
       Time     : in Guint32);
-   pragma Obsolescent;  --  Claim_Selection
    --  If Claim is set to True, claim the ownership of the primary X selection.
    --  Otherwise, release it. "Time" should be set to the
    --  time of the last-change time for the specified selection. It is
    --  discarded if it is earlier than the current last-change time, or
    --  later than the current X server time.
 
-   --  </doc_ignore>
+   procedure Copy_Clipboard
+     (Editable : access Gtk_Old_Editable_Record;
+      Time     : in Guint32);
+   --  Copy the characters in the current selection to the clipboard.
+
+   procedure Cut_Clipboard
+     (Editable : access Gtk_Old_Editable_Record;
+      Time     : Guint32);
+   --  Copy the characters in the current selection to the clipboard.
+   --  The selection is then deleted.
+
+   procedure Delete_Selection (Editable : access Gtk_Old_Editable_Record);
+   --  Disclaim and delete the current selection.
+
+   procedure Delete_Text
+     (Editable  : access Gtk_Old_Editable_Record;
+      Start_Pos : Gint := 0;
+      End_Pos   : Gint := -1);
+   --  Delete the characters from Start_Pos to End_Pos.
+   --  If End_Pos is negative, the characters are deleted from Start_Pos to the
+   --  end of the text.
+
+   function Get_Chars
+     (Editable  : access Gtk_Old_Editable_Record;
+      Start_Pos : Gint := 0;
+      End_Pos   : Gint := -1) return UTF8_String;
+   --  Get the text from Start_Pos to End_Pos.
+   --  If End_Pos is negative, the text from Start_Pos to the end is returned.
+
+   function Get_Clipboard_Text
+     (Widget : access Gtk_Old_Editable_Record) return UTF8_String;
+   --  Return the last text copied from the clipboard.
+
+   function Get_Editable
+     (Widget : access Gtk_Old_Editable_Record) return Boolean;
+   --  Return True if the widget is editable by the user.
+
+   procedure Set_Editable
+     (Widget   : access Gtk_Old_Editable_Record;
+      Editable : Boolean := True);
+   --  Set the editable status of the entry.
+   --  If Editable is False, the user can not modify the contents of the entry.
+   --  This does not affect the user of the insertion functions above.
+
+   function Get_Has_Selection
+     (Widget : access Gtk_Old_Editable_Record) return Boolean;
+   --  Return True if the selection is owned by the widget.
+
+   function Get_Selection_End_Pos
+     (Widget : access Gtk_Old_Editable_Record) return Guint;
+   --  Return the position of the end of the current selection.
+
+   function Get_Selection_Start_Pos
+     (Widget : access Gtk_Old_Editable_Record) return Guint;
+   --  Return the position of the beginning of the current selection.
+
+   procedure Insert_Text
+     (Editable : access Gtk_Old_Editable_Record;
+      New_Text : UTF8_String;
+      Position : in out Gint);
+   --  Insert the given string at the given position.
+   --  Position is set to the new cursor position.
+
+   procedure Paste_Clipboard
+     (Editable : access Gtk_Old_Editable_Record;
+      Time     : Guint32);
+   --  The contents of the clipboard is pasted into the given widget at
+   --  the current cursor position.
+
+   procedure Select_Region
+     (Editable : access Gtk_Old_Editable_Record;
+      Start    : Gint;
+      The_End  : Gint := -1);
+   --  Select the region of text from Start to The_End.
+   --  The characters that are selected are those characters at positions
+   --  from Start up to, but not including The_End. If The_End_Pos is
+   --  negative, then the characters selected will be those characters
+   --  from Start to the end of the text.
+
+   procedure Set_Position
+     (Editable : access Gtk_Old_Editable_Record;
+      Position : Gint);
+   --  Change the position of the cursor in the entry.
+   --  The cursor is displayed before the character with the given
+   --  index in the widget (the first character has index 0). The
+   --  value must be less than or equal to the number of characters in the
+   --  widget. A value of -1 indicates that the position
+   --  should be set after the last character in the entry.
+   --  Note that this position is in characters, not in bytes.
+
+   function Get_Position
+     (Editable : access Gtk_Old_Editable_Record) return Gint;
+   --  Return the position of the cursor.
 
    ---------------
    --  Signals  --
