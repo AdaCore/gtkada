583310c663930df5118ef19f6cb80660e51696c1
Arnaud Charlet <charlet@adacore.com>
2007-01-22 10:00:14 +0000

    Fix license.

--- examples/images/rect.adb
+++ examples/images/rect.adb
@@ -3,12 +3,12 @@
 --                                                                   --
 --                     Copyright (C) 2007 AdaCore                    --
 --                                                                   --
--- This library is free software; you can redistribute it and/or     --
+-- This program is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
 -- License as published by the Free Software Foundation; either      --
 -- version 2 of the License, or (at your option) any later version.  --
 --                                                                   --
--- This library is distributed in the hope that it will be useful,   --
+-- This program is distributed in the hope that it will be useful,   --
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
 -- General Public License for more details.                          --
@@ -18,12 +18,6 @@
 -- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
 -- Boston, MA 02111-1307, USA.                                       --
 --                                                                   --
--- As a special exception, if other files instantiate generics from  --
--- this unit, or you link this unit with other files to produce an   --
--- executable, this  unit  does not  by itself cause  the resulting  --
--- executable to be covered by the GNU General Public License. This  --
--- exception does not however invalidate any other reasons why the   --
--- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
 --  This example displays a toplevel window with a PNG image loaded from
