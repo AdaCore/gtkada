33964a79611af13fc91865c8ac8d529250e900d2
Arnaud Charlet <charlet@adacore.com>
2008-10-30 13:00:22 +0000

    (ada_gdk_move_pointer, ada_gdk_get_window_id): Provide dummy versions on  darwin, to be compatible with both X11 and Quartz. Part of H813-012

--- src/misc.c
+++ src/misc.c
@@ -4232,22 +4232,33 @@ ada_gdk_get_window_id (GdkWindow *w)
   return (void *)GDK_WINDOW_HWND (w);
 }
 
+#elif defined (__APPLE__)
+/*  Disable this function under Apple, since under Tiger the call to
+    XWarpPointer requires explicit linker flags pointing to X11 libs,
+    and when using Quartz, there is no X11 lib at all.
+*/
+void
+ada_gdk_move_pointer (gint x, gint y)
+{
+}
+
+void *
+ada_gdk_get_window_id (GdkWindow *w)
+{
+  return NULL;
+}
+
 #else
 #include <gdk/gdkx.h>
 
 void
 ada_gdk_move_pointer (gint x, gint y)
 {
-/*  Disable this function under Apple, since under Tiger the call to
-    XWarpPointer requires explicit linker flags pointing to X11 libs.
-*/
-#ifndef __APPLE__
   GdkDisplay *display = gdk_display_get_default ();
   Display *xdisplay = GDK_DISPLAY_XDISPLAY (display);
   Window xroot_window = GDK_WINDOW_XID (gdk_get_default_root_window ());
 
   XWarpPointer (xdisplay, None, xroot_window, 0, 0, 0, 0, x, y);
-#endif
 }
 
 void *
