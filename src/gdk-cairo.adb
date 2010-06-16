98997309f726e30e0c2ec71d1ed3dcc6234e426f
Nicolas Setton <setton@adacore.com>
2010-06-16 15:27:52 +0000

    Initial revision. For J614-014.

---
+++ src/gdk-cairo.adb
@@ -0,0 +1,48 @@
+-----------------------------------------------------------------------
+--               GtkAda - Ada95 binding for Gtk+/Gnome               --
+--                                                                   --
+--                    Copyright (C) 2010, AdaCore                    --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+with Glib.Object; use Glib.Object;
+with System; use System;
+
+package body Gdk.Cairo is
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create (Drawable : Gdk_Drawable) return Cairo_Context is
+      function Internal (S : System.Address) return Cairo_Context;
+      pragma Import (C, Internal, "gdk_cairo_create");
+
+      Cr : Cairo_Context;
+   begin
+      Cr := Internal (To_Address (Drawable));
+      return Cr;
+   end Create;
+
+end Gdk.Cairo;
