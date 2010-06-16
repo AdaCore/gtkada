fed238fa9791d6ae54742b1a17db06aca6d6afae
Arnaud Charlet <charlet@adacore.com>
2010-06-16 15:04:09 +0000

    Fix casing. Minor reformatting.

--- testgtk/opengl/lwobjects.adb
+++ testgtk/opengl/lwobjects.adb
@@ -1,24 +1,21 @@
 
 package body Lwobjects is
 
-   function Lw_Is_Lwobject (File : String) return Boolean
-   is
+   function Lw_Is_Lwobject (File : String) return Boolean is
       function Internal (File : String) return Integer;
       pragma Import (C, Internal, "lw_is_lwobject");
    begin
-      return Boolean'Val (Internal (File & ASCII.Nul));
+      return Boolean'Val (Internal (File & ASCII.NUL));
    end Lw_Is_Lwobject;
 
-   function Lw_Object_Read (File : String) return Lwobject
-   is
+   function Lw_Object_Read (File : String) return Lwobject is
       function Internal (File : String) return Lwobject;
       pragma Import (C, Internal, "lw_object_read");
    begin
-      return Internal (File & ASCII.Nul);
+      return Internal (File & ASCII.NUL);
    end Lw_Object_Read;
 
-   procedure Lw_Object_Free (Object : in out Lwobject)
-   is
+   procedure Lw_Object_Free (Object : in out Lwobject) is
       procedure Internal (Object : Lwobject);
       pragma Import (C, Internal, "lw_object_free");
    begin
