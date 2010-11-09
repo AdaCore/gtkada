0783d5615e4b72777dcdfdc4cdfc5c1339445807
Albert Lee <lee@adacore.com>
2010-11-09 22:26:36 +0000

    * gtkada-printing.ads: add signal handling functionality for begin-print, done,  end_print, paginate, request-page-setup, and status-changed. (Begin_Print_Handler, Install_Begin_Print_Handler): for begin-print signal (Done_Handler, Install_Done_Handler): for done signal (Draw_Page_Handler): fix comments (End_Print_Handler, Install_End_Print_Handler): for end-print signal (Paginate_Handler, Install_Paginate_Handler): for paginate signal (Request_Page_Setup_Handler, Install_Request_Page_Setup_Handler): for  request-page-setup signal (Status_Changed_Handler, Install_Status_Changed_Handler): for status-changed  signal (Gtkada_Print_Operation_Record): keep track of additional signal handlers
    * gtkada-printing.adb: add signal handling functionality for begin-print, done,
     end_print, paginate, request-page-setup, and status-changed.
    (Begin_Print_Handler, Begin_Print_Handler_Wrapper,
     Install_Begin_Print_Handler): for begin-print signal
    (Done_Handler, Done_Handler_Wrapper, Install_Done_Handler): for done signal
    (Draw_Page_Handler): fix comments
    (End_Print_Handler, End_Print_Handler_Wrapper, Install_End_Print_Handler):
     for end-print signal
    (Paginate_Handler, Paginate_Handler_Wrapper, Install_Paginate_Handler):
     for paginate signal
    (Request_Page_Setup_Handler, Request_Page_Setup_Handler_Wrapper,
     Install_Request_Page_Setup_Handler): for request-page-setup signal
    (Status_Changed_Handler, Status_Changed_Handler_Wrapper,
     Install_Status_Changed_Handler): for status-changed signal
    (Run): Install additional signal handler wrappers.
    
    Continuing work on JA07-018.

--- src/gtkada-printing.adb
+++ src/gtkada-printing.adb
@@ -26,13 +26,10 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
-with System;
-
 with Glib.Object; use Glib.Object;
 with Glib.Values; use Glib.Values;
 
-with Gtk.Arguments; use Gtk.Arguments;
-
+with Gtk.Arguments;  use Gtk.Arguments;
 with Gtk.Handlers;
 
 package body Gtkada.Printing is
@@ -40,16 +37,82 @@ package body Gtkada.Printing is
    package Object_Callback is new Gtk.Handlers.Callback
      (Gtkada_Print_Operation_Record);
 
+   package Object_Boolean_Return_Callback is new Gtk.Handlers.Return_Callback
+     (Gtkada_Print_Operation_Record, Gboolean);
+
    -----------------------
    -- Local subprograms --
    -----------------------
+
+   procedure Begin_Print_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues);
+   --  Wrapper around callback for "begin-print".
+
+   procedure Done_Handler_Wrapper
+     (Op     : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues);
+   --  Wrapper around callback for "done".
 
    procedure Draw_Page_Handler_Wrapper
      (Op   : access Gtkada_Print_Operation_Record'Class;
       Args : Glib.Values.GValues);
    --  Wrapper around callback for "draw-page".
 
-   --  ??? Other wrappers go here
+   procedure End_Print_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues);
+   --  Wrapper around callback for "end-print".
+
+   function Paginate_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues)
+     return Gboolean;
+   --  Wrapper around callback for "paginate".
+
+   procedure Request_Page_Setup_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues);
+   --  Wrapper around callback for "request-page-setup".
+
+   procedure Status_Changed_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues);
+   --  Wrapper around callback for "status-changed".
+
+   ---------------------------------
+   -- Begin_Print_Handler_Wrapper --
+   ---------------------------------
+
+   procedure Begin_Print_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues)
+   is
+      Context_Addr : constant System.Address := To_Address (Args, 1);
+      Context_Stub : Gtk_Print_Context_Record;
+      Context      : constant Gtk_Print_Context :=
+        Gtk_Print_Context (Get_User_Data (Context_Addr, Context_Stub));
+   begin
+      if Op.Begin_Print /= null then
+         Op.Begin_Print (Gtkada_Print_Operation (Op), Context);
+      end if;
+   end Begin_Print_Handler_Wrapper;
+
+   --------------------------
+   -- Done_Handler_Wrapper --
+   --------------------------
+
+   procedure Done_Handler_Wrapper
+     (Op     : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues)
+   is
+      Result : constant Gtk_Print_Operation_Result :=
+        Gtk_Print_Operation_Result'Val (To_Guint (Args, 1));
+   begin
+      if Op.Done /= null then
+         Op.Done (Gtkada_Print_Operation (Op), Result);
+      end if;
+   end Done_Handler_Wrapper;
 
    -------------------------------
    -- Draw_Page_Handler_Wrapper --
@@ -71,6 +134,24 @@ package body Gtkada.Printing is
       end if;
    end Draw_Page_Handler_Wrapper;
 
+   -------------------------------
+   -- End_Print_Handler_Wrapper --
+   -------------------------------
+
+   procedure End_Print_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues)
+   is
+      Context_Addr : constant System.Address := To_Address (Args, 1);
+      Context_Stub : Gtk_Print_Context_Record;
+      Context      : constant Gtk_Print_Context :=
+        Gtk_Print_Context (Get_User_Data (Context_Addr, Context_Stub));
+   begin
+      if Op.End_Print /= null then
+         Op.End_Print (Gtkada_Print_Operation (Op), Context);
+      end if;
+   end End_Print_Handler_Wrapper;
+
    -------------
    -- Gtk_New --
    -------------
@@ -91,6 +172,30 @@ package body Gtkada.Printing is
       Gtk.Print_Operation.Initialize (Widget);
    end Initialize;
 
+   ---------------------------------
+   -- Install_Begin_Print_Handler --
+   ---------------------------------
+
+   procedure Install_Begin_Print_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Begin_Print_Handler)
+   is
+   begin
+      Op.Begin_Print := Handler;
+   end Install_Begin_Print_Handler;
+
+   --------------------------
+   -- Install_Done_Handler --
+   --------------------------
+
+   procedure Install_Done_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Done_Handler)
+   is
+   begin
+      Op.Done := Handler;
+   end Install_Done_Handler;
+
    -------------------------------
    -- Install_Draw_Page_Handler --
    -------------------------------
@@ -102,6 +207,117 @@ package body Gtkada.Printing is
    begin
       Op.Draw_Page := Handler;
    end Install_Draw_Page_Handler;
+
+   -------------------------------
+   -- Install_End_Print_Handler --
+   -------------------------------
+
+   procedure Install_End_Print_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : End_Print_Handler)
+   is
+   begin
+      Op.End_Print := Handler;
+   end Install_End_Print_Handler;
+
+   ------------------------------
+   -- Install_Paginate_Handler --
+   ------------------------------
+
+   procedure Install_Paginate_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Paginate_Handler)
+   is
+   begin
+      Op.Paginate := Handler;
+   end Install_Paginate_Handler;
+
+   ----------------------------------------
+   -- Install_Request_Page_Setup_Handler --
+   ----------------------------------------
+
+   procedure Install_Request_Page_Setup_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Request_Page_Setup_Handler)
+   is
+   begin
+      Op.Request_Page_Setup := Handler;
+   end Install_Request_Page_Setup_Handler;
+
+   ------------------------------------
+   -- Install_Status_Changed_Handler --
+   ------------------------------------
+
+   procedure Install_Status_Changed_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Status_Changed_Handler)
+   is
+   begin
+      Op.Status_Changed := Handler;
+   end Install_Status_Changed_Handler;
+
+   ------------------------------
+   -- Paginate_Handler_Wrapper --
+   ------------------------------
+
+   function Paginate_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues)
+     return Gboolean
+   is
+      Context_Addr : constant System.Address := To_Address (Args, 1);
+      Context_Stub : Gtk_Print_Context_Record;
+      Context      : constant Gtk_Print_Context :=
+        Gtk_Print_Context (Get_User_Data (Context_Addr, Context_Stub));
+   begin
+      if Op.Paginate /= null then
+         return Boolean'Pos
+           (Op.Paginate (Gtkada_Print_Operation (Op), Context));
+      else
+         return Boolean'Pos (False);
+      end if;
+   end Paginate_Handler_Wrapper;
+
+   ----------------------------------------
+   -- Request_Page_Setup_Handler_Wrapper --
+   ----------------------------------------
+
+   procedure Request_Page_Setup_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues)
+   is
+      Context_Addr : constant System.Address := To_Address (Args, 1);
+      Context_Stub : Gtk_Print_Context_Record;
+      Context      : constant Gtk_Print_Context :=
+        Gtk_Print_Context (Get_User_Data (Context_Addr, Context_Stub));
+      Page_Num     : constant Gint := To_Gint (Args, 2);
+      Setup_Addr   : constant System.Address := To_Address (Args, 3);
+      Setup_Stub   : Gtk_Page_Setup_Record;
+      Setup        : constant Gtk_Page_Setup :=
+         Gtk_Page_Setup (Get_User_Data (Setup_Addr, Setup_Stub));
+      User_Data    : constant System.Address := To_Address (Args, 4);
+
+   begin
+      if Op.Request_Page_Setup /= null then
+         Op.Request_Page_Setup
+           (Gtkada_Print_Operation (Op), Context, Page_Num, Setup, User_Data);
+      end if;
+   end Request_Page_Setup_Handler_Wrapper;
+
+   ------------------------------------
+   -- Status_Changed_Handler_Wrapper --
+   ------------------------------------
+
+   procedure Status_Changed_Handler_Wrapper
+     (Op   : access Gtkada_Print_Operation_Record'Class;
+      Args : Glib.Values.GValues)
+   is
+      pragma Unreferenced (Args);
+   begin
+      if Op.Status_Changed /= null then
+         Op.Status_Changed (Gtkada_Print_Operation (Op));
+      end if;
+   end Status_Changed_Handler_Wrapper;
 
    ---------
    -- Run --
@@ -117,9 +333,40 @@ package body Gtkada.Printing is
    begin
       --  Connect all handlers
 
+      if Op.Begin_Print /= null then
+         Object_Callback.Connect
+           (Op, "begin-print", Begin_Print_Handler_Wrapper'Access);
+      end if;
+
+      if Op.Done /= null then
+         Object_Callback.Connect (Op, "done", Done_Handler_Wrapper'Access);
+      end if;
+
       if Op.Draw_Page /= null then
          Object_Callback.Connect
            (Op, "draw-page", Draw_Page_Handler_Wrapper'Access);
+      end if;
+
+      if Op.End_Print /= null then
+         Object_Callback.Connect
+           (Op, "end-print", End_Print_Handler_Wrapper'Access);
+      end if;
+
+      if Op.Paginate /= null then
+         Object_Boolean_Return_Callback.Connect
+           (Op, "paginate", Paginate_Handler_Wrapper'Access);
+      end if;
+
+      if Op.Request_Page_Setup /= null then
+         Object_Callback.Connect
+           (Op,
+            "request-page-setup",
+            Request_Page_Setup_Handler_Wrapper'Access);
+      end if;
+
+      if Op.Status_Changed /= null then
+         Object_Callback.Connect
+           (Op, "status-changed", Status_Changed_Handler_Wrapper'Access);
       end if;
 
       --  ??? Add other connections here
--- src/gtkada-printing.ads
+++ src/gtkada-printing.ads
@@ -37,9 +37,12 @@
 --  </description>
 --  <group>Miscellaneous</group>
 
+with System;
+
 with Glib; use Glib;
 with Glib.Error;
 
+with Gtk.Page_Setup;      use Gtk.Page_Setup;
 with Gtk.Print_Context;   use Gtk.Print_Context;
 with Gtk.Print_Operation; use Gtk.Print_Operation;
 with Gtk.Window;
@@ -54,6 +57,61 @@ package Gtkada.Printing is
    procedure Initialize (Widget : access Gtkada_Print_Operation_Record'Class);
    --  Initialize the print operation
 
+   function Connect_And_Run
+     (Op        : access Gtkada_Print_Operation_Record'Class;
+      Action    : Gtk_Print_Operation_Action;
+      Parent    : access Gtk.Window.Gtk_Window_Record'Class;
+      Error     : Glib.Error.GError := null)
+      return Gtk_Print_Operation_Result;
+   --  Runs the print operation, using the handlers installed in Op.
+   --  See Gtk.Print_Operations.Run.
+
+   -----------------
+   -- begin-print --
+   -----------------
+
+   type Begin_Print_Handler is access procedure
+     (Op      : Gtkada_Print_Operation;
+      Context : Gtk_Print_Context);
+   --  Called after the user has finished changing print settings in the
+   --  dialog, before the actual rendering starts.
+   --
+   --  A typical use is to use the parameters from the
+   --  Gtk_Print_Context and paginate the document accordingly, and then
+   --  set the number of pages with Gtk.Print_Operation.Set_N_Pages.
+
+   procedure Install_Begin_Print_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Begin_Print_Handler);
+   --  Install a Begin_Print_Handler.
+
+   ----------
+   -- done --
+   ----------
+
+   type Done_Handler is access procedure
+     (Op     : Gtkada_Print_Operation;
+      Result : Gtk_Print_Operation_Result);
+   --  Called when the print operation run has finished doing everything
+   --  required for printing.
+   --
+   --  Result gives you information about what happened during the run.
+   --  If Result is Result_Error then you can call Get_Error for more
+   --  information.
+   --
+   --  If you enabled print status tracking then
+   --  Gtk.Print_Operation.Is_Finished may still return False after
+   --  done was emitted.
+
+   procedure Install_Done_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Done_Handler);
+   --  Install a Done_Handler.
+
+   ---------------
+   -- draw-page --
+   ---------------
+
    type Draw_Page_Handler is access procedure
      (Op          : Gtkada_Print_Operation;
       Context     : Gtk_Print_Context;
@@ -61,28 +119,107 @@ package Gtkada.Printing is
    --  Called for every page that is printed. This handler must render the
    --  page Page_Number onto the cairo context obtained from Context using
    --  Gtk.Print_Context.Get_Cairo_Context.
-   --  ??? Complete doc from gtkprintoperation.c
+   --
+   --  Use Gtk.Print_Operation.Set_Use_Full_Page and
+   --  Gtk.Print_Operation.Set_Unit before starting the print operation to set
+   --  up the transformation of the cairo context according to your needs.
 
    procedure Install_Draw_Page_Handler
      (Op      : access Gtkada_Print_Operation_Record'Class;
       Handler : Draw_Page_Handler);
    --  Install a Draw_Page_Handler.
 
-   function Connect_And_Run
-     (Op        : access Gtkada_Print_Operation_Record'Class;
-      Action    : Gtk_Print_Operation_Action;
-      Parent    : access Gtk.Window.Gtk_Window_Record'Class;
-      Error     : Glib.Error.GError := null)
-      return Gtk_Print_Operation_Result;
-   --  Runs the print operation.
-   --  See Gtk.Print_Operations.Run.
+   ---------------
+   -- end-print --
+   ---------------
+
+   type End_Print_Handler is access procedure
+     (Op      : Gtkada_Print_Operation;
+      Context : Gtk_Print_Context);
+   --  Called after all pages have been rendered.
+   --
+   --  This handler can clean up any resources that have been allocated
+   --  in the ::begin-print handler.
+
+   procedure Install_End_Print_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : End_Print_Handler);
+   --  Install an End_Print_Handler.
+
+   --------------
+   -- paginate --
+   --------------
+
+   type Paginate_Handler is access function
+     (Op      : Gtkada_Print_Operation;
+      Context : Gtk_Print_Context)
+     return Boolean;
+   --  Called after the ::begin-print signal, but before the actual rendering
+   --  starts. It keeps getting emitted until a connected signal handler
+   --  returns True.
+   --
+   --  The ::paginate signal is intended to be used for paginating a document
+   --  in small chunks, to avoid blocking the user interface for a long
+   --  time. The signal handler should update the number of pages using
+   --  Gtk.Print_Operation.Set_N_Pages, and return True if the document
+   --  has been completely paginated.
+   --
+   --  If you don't need to do pagination in chunks, you can simply do
+   --  it all in the ::begin-print handler, and set the number of pages
+   --  from there.
+
+   procedure Install_Paginate_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Paginate_Handler);
+   --  Install a Paginate_Handler.
+
+   ------------------------
+   -- request-page-setup --
+   ------------------------
+
+   type Request_Page_Setup_Handler is access procedure
+     (Op          : Gtkada_Print_Operation;
+      Context     : Gtk_Print_Context;
+      Page_Number : Gint;
+      Setup       : Gtk_Page_Setup;
+      User_Data   : System.Address);
+   --  Called once for every page that is printed, to give the application
+   --  a chance to modify the page setup. Any changes done to setup will be
+   --  in force only for printing this page.
+
+   procedure Install_Request_Page_Setup_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Request_Page_Setup_Handler);
+   --  Install a Request_Page_Setup_Handler.
+
+   --------------------
+   -- status-changed --
+   --------------------
+
+   type Status_Changed_Handler is access procedure
+     (Op : Gtkada_Print_Operation);
+   --  Called between the various phases of the print operation.
+   --  See Gtk_Print_Status for the phases that are being discriminated.
+   --  Use Gtk.Print_Operation.Get_Status to find out the current
+   --  status.
+
+   procedure Install_Status_Changed_Handler
+     (Op      : access Gtkada_Print_Operation_Record'Class;
+      Handler : Status_Changed_Handler);
+   --  Install a Status_Changed_Handler.
 
 private
 
    type Gtkada_Print_Operation_Record is
      new Gtk_Print_Operation_Record
    with record
-      Draw_Page : Draw_Page_Handler := null;
+      Begin_Print        : Begin_Print_Handler        := null;
+      Done               : Done_Handler               := null;
+      Draw_Page          : Draw_Page_Handler          := null;
+      End_Print          : End_Print_Handler          := null;
+      Paginate           : Paginate_Handler           := null;
+      Request_Page_Setup : Request_Page_Setup_Handler := null;
+      Status_Changed     : Status_Changed_Handler     := null;
    end record;
 
 end Gtkada.Printing;
