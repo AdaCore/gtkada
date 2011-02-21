9d921cd438c8daf6a1d84cf47503a554364fa90e
Arnaud Charlet <charlet@adacore.com>
2011-02-21 12:12:20 +0000

    Remove use of Ada 2005, break build of GtkAda with older GNAT compilers.

--- testgtk/testcairo_printing.ads
+++ testgtk/testcairo_printing.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --          GtkAda - Ada95 binding for the Gimp Toolkit              --
 --                                                                   --
---                    Copyright (C) 2010, AdaCore                    --
+--                 Copyright (C) 2010-2011, AdaCore                  --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -26,8 +26,6 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
-pragma Ada_2005;
-
 with Glib;  use Glib;
 
 with Gtkada.Printing;        use Gtkada.Printing;
@@ -47,8 +45,8 @@ package Testcairo_Printing is
    type Testcairo_Print_Operation is access all
      Testcairo_Print_Operation_Record'Class;
 
-   overriding procedure Draw_Page
-     (Op          : not null access Testcairo_Print_Operation_Record;
+   procedure Draw_Page
+     (Op          : access Testcairo_Print_Operation_Record;
       Context     : Gtk_Print_Context;
       Page_Number : Gint);
    --  Handler responsible for printing pages
