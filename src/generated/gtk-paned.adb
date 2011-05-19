ad5e080c31a3c17ad04bd396430bb2bc1a19fd58
Emmanuel Briot <briot@adacore.com>
2011-05-19 09:58:11 +0000

    Check in automatically-generated version of this package, since the  other version still depends on the ada_paned_* functions, now gone. The intent, anyway, is to replace the manual versions by the automatic  versions eventually in svn. K419-006

---
+++ src/generated/gtk-paned.adb
@@ -0,0 +1,229 @@
+-----------------------------------------------------------------------
+--               GtkAda - Ada95 binding for Gtk+/Gnome               --
+--                                                                   --
+--   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
+--                Copyright (C) 2000-2011, AdaCore                   --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+pragma Style_Checks (Off);
+pragma Warnings (Off, "*is already use-visible*");
+with Glib.Type_Conversion_Hooks; use Glib.Type_Conversion_Hooks;
+
+package body Gtk.Paned is
+
+   package Type_Conversion is new Glib.Type_Conversion_Hooks.Hook_Registrator
+     (Get_Type'Access, Gtk_Paned_Record);
+   pragma Unreferenced (Type_Conversion);
+
+   --------------------
+   -- Gtk_New_Hpaned --
+   --------------------
+
+   procedure Gtk_New_Hpaned (Self : out Gtk_Hpaned) is
+   begin
+      Self := new Gtk_Hpaned_Record;
+      Gtk.Paned.Initialize_Hpaned (Self);
+   end Gtk_New_Hpaned;
+
+   --------------------
+   -- Gtk_New_Vpaned --
+   --------------------
+
+   procedure Gtk_New_Vpaned (Self : out Gtk_Vpaned) is
+   begin
+      Self := new Gtk_Vpaned_Record;
+      Gtk.Paned.Initialize_Vpaned (Self);
+   end Gtk_New_Vpaned;
+
+   -----------------------
+   -- Initialize_Hpaned --
+   -----------------------
+
+   procedure Initialize_Hpaned (Self : access Gtk_Hpaned_Record'Class) is
+      function Internal return System.Address;
+      pragma Import (C, Internal, "gtk_hpaned_new");
+   begin
+      Set_Object (Self, Internal);
+   end Initialize_Hpaned;
+
+   -----------------------
+   -- Initialize_Vpaned --
+   -----------------------
+
+   procedure Initialize_Vpaned (Self : access Gtk_Vpaned_Record'Class) is
+      function Internal return System.Address;
+      pragma Import (C, Internal, "gtk_vpaned_new");
+   begin
+      Set_Object (Self, Internal);
+   end Initialize_Vpaned;
+
+   ----------
+   -- Add1 --
+   ----------
+
+   procedure Add1
+      (Self  : access Gtk_Paned_Record;
+       Child : access Gtk_Widget_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Child : System.Address);
+      pragma Import (C, Internal, "gtk_paned_add1");
+   begin
+      Internal (Get_Object (Self), Get_Object_Or_Null (GObject (Child)));
+   end Add1;
+
+   ----------
+   -- Add2 --
+   ----------
+
+   procedure Add2
+      (Self  : access Gtk_Paned_Record;
+       Child : access Gtk_Widget_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Child : System.Address);
+      pragma Import (C, Internal, "gtk_paned_add2");
+   begin
+      Internal (Get_Object (Self), Get_Object_Or_Null (GObject (Child)));
+   end Add2;
+
+   ----------------------
+   -- Compute_Position --
+   ----------------------
+
+   procedure Compute_Position
+      (Self       : access Gtk_Paned_Record;
+       Allocation : Gint;
+       Child1_Req : Gint;
+       Child2_Req : Gint)
+   is
+      procedure Internal
+         (Self       : System.Address;
+          Allocation : Gint;
+          Child1_Req : Gint;
+          Child2_Req : Gint);
+      pragma Import (C, Internal, "gtk_paned_compute_position");
+   begin
+      Internal (Get_Object (Self), Allocation, Child1_Req, Child2_Req);
+   end Compute_Position;
+
+   ----------------
+   -- Get_Child1 --
+   ----------------
+
+   function Get_Child1 (Self : access Gtk_Paned_Record) return Gtk_Widget is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_paned_get_child1");
+      Stub : Gtk_Widget_Record;
+   begin
+      return Gtk_Widget (Get_User_Data (Internal (Get_Object (Self)), Stub));
+   end Get_Child1;
+
+   ----------------
+   -- Get_Child2 --
+   ----------------
+
+   function Get_Child2 (Self : access Gtk_Paned_Record) return Gtk_Widget is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_paned_get_child2");
+      Stub : Gtk_Widget_Record;
+   begin
+      return Gtk_Widget (Get_User_Data (Internal (Get_Object (Self)), Stub));
+   end Get_Child2;
+
+   -----------------------
+   -- Get_Handle_Window --
+   -----------------------
+
+   function Get_Handle_Window
+      (Self : access Gtk_Paned_Record) return Gdk.Window.Gdk_Window
+   is
+      function Internal (Self : System.Address) return Gdk.Window.Gdk_Window;
+      pragma Import (C, Internal, "gtk_paned_get_handle_window");
+   begin
+      return Internal (Get_Object (Self));
+   end Get_Handle_Window;
+
+   ------------------
+   -- Get_Position --
+   ------------------
+
+   function Get_Position (Self : access Gtk_Paned_Record) return Gint is
+      function Internal (Self : System.Address) return Gint;
+      pragma Import (C, Internal, "gtk_paned_get_position");
+   begin
+      return Internal (Get_Object (Self));
+   end Get_Position;
+
+   -----------
+   -- Pack1 --
+   -----------
+
+   procedure Pack1
+      (Self   : access Gtk_Paned_Record;
+       Child  : access Gtk_Widget_Record'Class;
+       Resize : Boolean := False;
+       Shrink : Boolean := True)
+   is
+      procedure Internal
+         (Self   : System.Address;
+          Child  : System.Address;
+          Resize : Integer;
+          Shrink : Integer);
+      pragma Import (C, Internal, "gtk_paned_pack1");
+   begin
+      Internal (Get_Object (Self), Get_Object_Or_Null (GObject (Child)), Boolean'Pos (Resize), Boolean'Pos (Shrink));
+   end Pack1;
+
+   -----------
+   -- Pack2 --
+   -----------
+
+   procedure Pack2
+      (Self   : access Gtk_Paned_Record;
+       Child  : access Gtk_Widget_Record'Class;
+       Resize : Boolean := False;
+       Shrink : Boolean := False)
+   is
+      procedure Internal
+         (Self   : System.Address;
+          Child  : System.Address;
+          Resize : Integer;
+          Shrink : Integer);
+      pragma Import (C, Internal, "gtk_paned_pack2");
+   begin
+      Internal (Get_Object (Self), Get_Object_Or_Null (GObject (Child)), Boolean'Pos (Resize), Boolean'Pos (Shrink));
+   end Pack2;
+
+   ------------------
+   -- Set_Position --
+   ------------------
+
+   procedure Set_Position (Self : access Gtk_Paned_Record; Position : Gint) is
+      procedure Internal (Self : System.Address; Position : Gint);
+      pragma Import (C, Internal, "gtk_paned_set_position");
+   begin
+      Internal (Get_Object (Self), Position);
+   end Set_Position;
+
+end Gtk.Paned;
---
+++ src/generated/gtk-paned.ads
@@ -0,0 +1,271 @@
+-----------------------------------------------------------------------
+--               GtkAda - Ada95 binding for Gtk+/Gnome               --
+--                                                                   --
+--   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
+--                Copyright (C) 2000-2011, AdaCore                   --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+--  <description>
+--  A Gtk_Paned is a container that organizes its two children either
+--  horizontally or vertically. The initial size allocated to the children
+--  depends on the size they request. However, the user has the possibility to
+--  interactively move a separation bar between the two to enlarge one of the
+--  children, while at the same time shrinking the second one. The bar can be
+--  moved by clicking with the mouse on a small cursor displayed in the bar, and
+--  then dragging the mouse.
+-- 
+--  No additional decoration is provided around the children.
+-- 
+--  Each child has two parameters, Resize and Shrink.
+-- 
+--  If Shrink is True, then the widget can be made smaller than its requisition
+--  size by the user. Set this to False if you want to set a minimum size.
+-- 
+--  if Resize is True, this means that the child accepts to be resized, and
+--  will not require any size. Thus, the size allocated to it will be the total
+--  size allocated to the container minus the size requested by the other child.
+--  If Resize is False, the child should ask for a specific size, which it will
+--  get. The other child will be resized accordingly. If both Child have the
+--  same value for Resize (either True or False), then the size allocated to
+--  each is a ratio between the size requested by both.
+-- 
+--  When you use Set_Position with a parameter other than -1, or the user moves
+--  the handle to resize the widgets, the behavior of Resize is canceled.
+-- 
+--  </description>
+--  <screenshot>gtk-paned</screenshot>
+--  <group>Layout container</group>
+--  <testgtk>create_paned.adb</testgtk>
+
+pragma Style_Checks (Off);
+pragma Warnings (Off, "*is already use-visible*");
+with Gdk.Window;      use Gdk.Window;
+with Glib;            use Glib;
+with Glib.Properties; use Glib.Properties;
+with Gtk.Container;   use Gtk.Container;
+with Gtk.Widget;      use Gtk.Widget;
+
+package Gtk.Paned is
+
+   type Gtk_Paned_Record is new Gtk_Container_Record with null record;
+   type Gtk_Paned is access all Gtk_Paned_Record'Class;
+
+   subtype Gtk_Vpaned_Record is Gtk_Paned_Record;
+   subtype Gtk_Vpaned is Gtk_Paned;
+
+   subtype Gtk_Hpaned_Record is Gtk_Paned_Record;
+   subtype Gtk_Hpaned is Gtk_Paned;
+
+   ------------------
+   -- Constructors --
+   ------------------
+
+   function Get_Type return Glib.GType;
+   pragma Import (C, Get_Type, "gtk_paned_get_type");
+
+   procedure Gtk_New_Vpaned (Self : out Gtk_Vpaned);
+   procedure Initialize_Vpaned (Self : access Gtk_Vpaned_Record'Class);
+   --  The children will be displayed one on top of the other
+
+   function Get_Type_Vpaned return Glib.GType;
+   pragma Import (C, Get_Type_Vpaned, "gtk_vpaned_get_type");
+
+   procedure Gtk_New_Hpaned (Self : out Gtk_Hpaned);
+   procedure Initialize_Hpaned (Self : access Gtk_Hpaned_Record'Class);
+   --  The children will be displayed next to each other
+
+   function Get_Type_Hpaned return Glib.GType;
+   pragma Import (C, Get_Type_Hpaned, "gtk_hpaned_get_type");
+
+   -------------
+   -- Methods --
+   -------------
+
+   procedure Add1
+      (Self  : access Gtk_Paned_Record;
+       Child : access Gtk_Widget_Record'Class);
+   --  Add the first child of the container. The child will be displayed either
+   --  in the top or in the left pane, depending on the orientation of the
+   --  container. This is equivalent to using the Pack1 procedure with its
+   --  default parameters.
+
+   procedure Add2
+      (Self  : access Gtk_Paned_Record;
+       Child : access Gtk_Widget_Record'Class);
+   --  Add the second child of the container. It will be displayed in the
+   --  bottom or right pane, depending on the container's orientation. This is
+   --  equivalent to using Pack2 with its default parameters.
+
+   procedure Compute_Position
+      (Self       : access Gtk_Paned_Record;
+       Allocation : Gint;
+       Child1_Req : Gint;
+       Child2_Req : Gint);
+
+   function Get_Child1 (Self : access Gtk_Paned_Record) return Gtk_Widget;
+   --  Obtains the first child of the paned widget.
+   --  Since: gtk+ 2.4
+
+   function Get_Child2 (Self : access Gtk_Paned_Record) return Gtk_Widget;
+   --  Obtains the second child of the paned widget.
+   --  Since: gtk+ 2.4
+
+   function Get_Handle_Window
+      (Self : access Gtk_Paned_Record) return Gdk.Window.Gdk_Window;
+   --  Returns the Gdk.Window.Gdk_Window of the handle. This function is useful
+   --  when handling button or motion events because it enables the callback to
+   --  distinguish between the window of the paned, a child and the handle.
+   --  Since: gtk+ 2.20
+
+   function Get_Position (Self : access Gtk_Paned_Record) return Gint;
+   procedure Set_Position (Self : access Gtk_Paned_Record; Position : Gint);
+   --  Sets the position of the divider between the two panes.
+   --  "position": pixel position of divider, a negative value means that the
+   --  position is unset.
+
+   procedure Pack1
+      (Self   : access Gtk_Paned_Record;
+       Child  : access Gtk_Widget_Record'Class;
+       Resize : Boolean := False;
+       Shrink : Boolean := True);
+   --  Add a child to the top or left pane. You can not change dynamically the
+   --  attributes Resize and Shrink. Instead, you have to remove the child from
+   --  the container, and put it back with the new value of the attributes. You
+   --  should also first call Gtk.Object.Ref on the child so as to be sure it is
+   --  not destroyed when you remove it, and Gtk.Object.Unref it at the end. See
+   --  the example in testgtk/ in the GtkAda distribution.
+
+   procedure Pack2
+      (Self   : access Gtk_Paned_Record;
+       Child  : access Gtk_Widget_Record'Class;
+       Resize : Boolean := False;
+       Shrink : Boolean := False);
+
+   ----------------
+   -- Properties --
+   ----------------
+   --  The following properties are defined for this widget. See
+   --  Glib.Properties for more information on properties)
+   -- 
+   --  Name: Max_Position_Property
+   --  Type: Int
+   --  Flags: read-write
+   --  The largest possible value for the position property. This property is
+   --  derived from the size and shrinkability of the widget's children.
+   -- 
+   --  Name: Min_Position_Property
+   --  Type: Int
+   --  Flags: read-write
+   --  The smallest possible value for the position property. This property is
+   --  derived from the size and shrinkability of the widget's children.
+   -- 
+   --  Name: Position_Property
+   --  Type: Int
+   --  Flags: read-write
+   -- 
+   --  Name: Position_Set_Property
+   --  Type: Boolean
+   --  Flags: read-write
+   --  The following properties are defined for this widget. See
+   --  Glib.Properties for more information on properties)
+   --  The following properties are defined for this widget. See
+   --  Glib.Properties for more information on properties)
+
+   Max_Position_Property : constant Glib.Properties.Property_Int;
+   Min_Position_Property : constant Glib.Properties.Property_Int;
+   Position_Property : constant Glib.Properties.Property_Int;
+   Position_Set_Property : constant Glib.Properties.Property_Boolean;
+
+   -------------
+   -- Signals --
+   -------------
+   --  The following new signals are defined for this widget:
+   -- 
+   --  "accept-position"
+   --  function Handler (Self : access Gtk_Paned_Record'Class) return --
+   --  gboolean;
+   --  The ::accept-position signal is a <link
+   --  linkend="keybinding-signals">keybinding signal</link> which gets emitted
+   --  to accept the current position of the handle when moving it using key
+   --  bindings. The default binding for this signal is Return or Space.
+   -- 
+   --  "cancel-position"
+   --  function Handler (Self : access Gtk_Paned_Record'Class) return --
+   --  gboolean;
+   --  The ::cancel-position signal is a <link
+   --  linkend="keybinding-signals">keybinding signal</link> which gets emitted
+   --  to cancel moving the position of the handle using key bindings. The
+   --  position of the handle will be reset to the value prior to moving it. The
+   --  default binding for this signal is Escape.
+   -- 
+   --  "cycle-child-focus"
+   --  function Handler (Self : access Gtk_Paned_Record'Class) return --
+   --  gboolean;
+   --  The ::cycle-child-focus signal is a <link
+   --  linkend="keybinding-signals">keybinding signal</link> which gets emitted
+   --  to cycle the focus between the children of the paned. The default binding
+   --  is f6.
+   -- 
+   --  "cycle-handle-focus"
+   --  function Handler (Self : access Gtk_Paned_Record'Class) return --
+   --  gboolean;
+   --  The ::cycle-handle-focus signal is a <link
+   --  linkend="keybinding-signals">keybinding signal</link> which gets emitted
+   --  to cycle whether the paned should grab focus to allow the user to change
+   --  position of the handle by using key bindings. The default binding for
+   --  this signal is f8.
+   -- 
+   --  "move-handle"
+   --  function Handler (Self : access Gtk_Paned_Record'Class) return --
+   --  gboolean;
+   --  The ::move-handle signal is a <link
+   --  linkend="keybinding-signals">keybinding signal</link> which gets emitted
+   --  to move the handle when the user is using key bindings to move it.
+   -- 
+   --  "toggle-handle-focus"
+   --  function Handler (Self : access Gtk_Paned_Record'Class) return --
+   --  gboolean;
+   --  The ::toggle-handle-focus is a <link
+   --  linkend="keybinding-signals">keybinding signal</link> which gets emitted
+   --  to accept the current position of the handle and then move focus to the
+   --  next widget in the focus chain. The default binding is Tab.
+
+   Signal_Accept_Position : constant Glib.Signal_Name := "accept-position";
+   Signal_Cancel_Position : constant Glib.Signal_Name := "cancel-position";
+   Signal_Cycle_Child_Focus : constant Glib.Signal_Name := "cycle-child-focus";
+   Signal_Cycle_Handle_Focus : constant Glib.Signal_Name := "cycle-handle-focus";
+   Signal_Move_Handle : constant Glib.Signal_Name := "move-handle";
+   Signal_Toggle_Handle_Focus : constant Glib.Signal_Name := "toggle-handle-focus";
+
+private
+   Max_Position_Property : constant Glib.Properties.Property_Int:=
+     Glib.Properties.Build ("Max_Position_Property");
+   Min_Position_Property : constant Glib.Properties.Property_Int:=
+     Glib.Properties.Build ("Min_Position_Property");
+   Position_Property : constant Glib.Properties.Property_Int:=
+     Glib.Properties.Build ("Position_Property");
+   Position_Set_Property : constant Glib.Properties.Property_Boolean:=
+     Glib.Properties.Build ("Position_Set_Property");
+end Gtk.Paned;
