aac036768897dd722ae47c2f3c24dc7afd412e1d
Arnaud Charlet <charlet@adacore.com>
2011-05-31 12:41:20 +0000

    Fix code (new GNAT warning).

--- src/gtk-color_selection.adb
+++ src/gtk-color_selection.adb
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                 Copyright (C) 2000-2008, AdaCore                  --
+--                 Copyright (C) 2000-2011, AdaCore                  --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -63,7 +63,6 @@ package body Gtk.Color_Selection is
       pragma Import (C, Internal, "gtk_color_selection_get_color");
 
    begin
-      Color (Opacity) := 0.0;
       Internal (Get_Object (Colorsel), Color);
    end Get_Color;
 
