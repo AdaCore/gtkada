be019514b74c892b3739998044e05a44f9899d80
Nicolas Setton <setton@adacore.com>
2011-06-07 12:18:06 +0000

    We now generate gtk-event_box.ad?.

--- src/generated/gtk-event_box.adb
+++ src/generated/gtk-event_box.adb
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet   --
---                 Copyright (C) 2000-2008, AdaCore                  --
+--                Copyright (C) 2000-2011, AdaCore                   --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -27,15 +27,15 @@
 -- executable file  might be covered by the  GNU Public License.     --
 -----------------------------------------------------------------------
 
-with System;
-
-with Glib.Type_Conversion_Hooks;
+pragma Style_Checks (Off);
+pragma Warnings (Off, "*is already use-visible*");
+with Glib.Type_Conversion_Hooks; use Glib.Type_Conversion_Hooks;
 
 package body Gtk.Event_Box is
 
    package Type_Conversion is new Glib.Type_Conversion_Hooks.Hook_Registrator
      (Get_Type'Access, Gtk_Event_Box_Record);
-   pragma Warnings (Off, Type_Conversion);
+   pragma Unreferenced (Type_Conversion);
 
    -------------
    -- Gtk_New --
@@ -54,51 +54,9 @@ package body Gtk.Event_Box is
    procedure Initialize (Event_Box : access Gtk_Event_Box_Record'Class) is
       function Internal return System.Address;
       pragma Import (C, Internal, "gtk_event_box_new");
-
    begin
       Set_Object (Event_Box, Internal);
    end Initialize;
-
-   ------------------------
-   -- Set_Visible_Window --
-   ------------------------
-
-   procedure Set_Visible_Window
-      (Event_Box : access Gtk_Event_Box_Record;
-       Visible_Window : Boolean)
-   is
-      procedure Internal (Event_Box : System.Address; Visible : Integer);
-      pragma Import (C, Internal, "gtk_event_box_set_visible_window");
-   begin
-      Internal (Get_Object (Event_Box), Boolean'Pos (Visible_Window));
-   end Set_Visible_Window;
-
-   ------------------------
-   -- Get_Visible_Window --
-   ------------------------
-
-   function Get_Visible_Window
-      (Event_Box : access Gtk_Event_Box_Record) return Boolean
-   is
-      function Internal (Box : System.Address) return Integer;
-      pragma Import (C, Internal, "gtk_event_box_get_visible_window");
-   begin
-      return Internal (Get_Object (Event_Box)) /= 0;
-   end Get_Visible_Window;
-
-   ---------------------
-   -- Set_Above_Child --
-   ---------------------
-
-   procedure Set_Above_Child
-      (Event_Box : access Gtk_Event_Box_Record;
-       Above_Child : Boolean)
-   is
-      procedure Internal (Box : System.Address; Above : Integer);
-      pragma Import (C, Internal, "gtk_event_box_set_above_child");
-   begin
-      Internal (Get_Object (Event_Box), Boolean'Pos (Above_Child));
-   end Set_Above_Child;
 
    ---------------------
    -- Get_Above_Child --
@@ -107,10 +65,53 @@ package body Gtk.Event_Box is
    function Get_Above_Child
       (Event_Box : access Gtk_Event_Box_Record) return Boolean
    is
-      function Internal (Box : System.Address) return Integer;
+      function Internal (Event_Box : System.Address) return Integer;
       pragma Import (C, Internal, "gtk_event_box_get_above_child");
    begin
-      return Internal (Get_Object (Event_Box)) /= 0;
+      return Boolean'Val (Internal (Get_Object (Event_Box)));
    end Get_Above_Child;
+
+   ------------------------
+   -- Get_Visible_Window --
+   ------------------------
+
+   function Get_Visible_Window
+      (Event_Box : access Gtk_Event_Box_Record) return Boolean
+   is
+      function Internal (Event_Box : System.Address) return Integer;
+      pragma Import (C, Internal, "gtk_event_box_get_visible_window");
+   begin
+      return Boolean'Val (Internal (Get_Object (Event_Box)));
+   end Get_Visible_Window;
+
+   ---------------------
+   -- Set_Above_Child --
+   ---------------------
+
+   procedure Set_Above_Child
+      (Event_Box   : access Gtk_Event_Box_Record;
+       Above_Child : Boolean)
+   is
+      procedure Internal (Event_Box : System.Address; Above_Child : Integer);
+      pragma Import (C, Internal, "gtk_event_box_set_above_child");
+   begin
+      Internal (Get_Object (Event_Box), Boolean'Pos (Above_Child));
+   end Set_Above_Child;
+
+   ------------------------
+   -- Set_Visible_Window --
+   ------------------------
+
+   procedure Set_Visible_Window
+      (Event_Box      : access Gtk_Event_Box_Record;
+       Visible_Window : Boolean)
+   is
+      procedure Internal
+         (Event_Box      : System.Address;
+          Visible_Window : Integer);
+      pragma Import (C, Internal, "gtk_event_box_set_visible_window");
+   begin
+      Internal (Get_Object (Event_Box), Boolean'Pos (Visible_Window));
+   end Set_Visible_Window;
 
 end Gtk.Event_Box;
