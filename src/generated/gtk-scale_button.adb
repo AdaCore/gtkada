50c2c5f21469782f66c5cd854c864c9f23de01a6
Albert Lee <lee@adacore.com>
2012-01-18 16:01:16 +0000

    Remove old src/gtk-scale_button.ad[sb] files. Regenerate.

---
+++ src/generated/gtk-scale_button.adb
@@ -0,0 +1,317 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 2000-2012, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma Ada_05;
+pragma Style_Checks (Off);
+pragma Warnings (Off, "*is already use-visible*");
+with Glib.Type_Conversion_Hooks; use Glib.Type_Conversion_Hooks;
+with Gtkada.Bindings;            use Gtkada.Bindings;
+with Gtkada.Types;               use Gtkada.Types;
+with Interfaces.C.Strings;       use Interfaces.C.Strings;
+
+package body Gtk.Scale_Button is
+
+   package Type_Conversion is new Glib.Type_Conversion_Hooks.Hook_Registrator
+     (Get_Type'Access, Gtk_Scale_Button_Record);
+   pragma Unreferenced (Type_Conversion);
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New
+      (Button : out Gtk_Scale_Button;
+       Size   : Gtk.Enums.Gtk_Icon_Size;
+       Min    : Gdouble;
+       Max    : Gdouble;
+       Step   : Gdouble;
+       Icons  : GNAT.Strings.String_List)
+   is
+   begin
+      Button := new Gtk_Scale_Button_Record;
+      Gtk.Scale_Button.Initialize (Button, Size, Min, Max, Step, Icons);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize
+      (Button : access Gtk_Scale_Button_Record'Class;
+       Size   : Gtk.Enums.Gtk_Icon_Size;
+       Min    : Gdouble;
+       Max    : Gdouble;
+       Step   : Gdouble;
+       Icons  : GNAT.Strings.String_List)
+   is
+      function Internal
+         (Size  : Integer;
+          Min   : Gdouble;
+          Max   : Gdouble;
+          Step  : Gdouble;
+          Icons : Interfaces.C.Strings.chars_ptr_array)
+          return System.Address;
+      pragma Import (C, Internal, "gtk_scale_button_new");
+      Tmp_Icons  : Interfaces.C.Strings.chars_ptr_array := From_String_List (Icons);
+      Tmp_Return : System.Address;
+   begin
+      Tmp_Return := Internal (Gtk.Enums.Gtk_Icon_Size'Pos (Size), Min, Max, Step, Tmp_Icons);
+      GtkAda.Types.Free (Tmp_Icons);
+      Set_Object (Button, Tmp_Return);
+   end Initialize;
+
+   --------------------
+   -- Get_Adjustment --
+   --------------------
+
+   function Get_Adjustment
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Adjustment.Gtk_Adjustment
+   is
+      function Internal (Button : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_scale_button_get_adjustment");
+      Stub_Gtk_Adjustment : Gtk.Adjustment.Gtk_Adjustment_Record;
+   begin
+      return Gtk.Adjustment.Gtk_Adjustment (Get_User_Data (Internal (Get_Object (Button)), Stub_Gtk_Adjustment));
+   end Get_Adjustment;
+
+   ----------------------
+   -- Get_Minus_Button --
+   ----------------------
+
+   function Get_Minus_Button
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Widget.Gtk_Widget
+   is
+      function Internal (Button : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_scale_button_get_minus_button");
+      Stub_Gtk_Widget : Gtk.Widget.Gtk_Widget_Record;
+   begin
+      return Gtk.Widget.Gtk_Widget (Get_User_Data (Internal (Get_Object (Button)), Stub_Gtk_Widget));
+   end Get_Minus_Button;
+
+   ---------------------
+   -- Get_Plus_Button --
+   ---------------------
+
+   function Get_Plus_Button
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Widget.Gtk_Widget
+   is
+      function Internal (Button : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_scale_button_get_plus_button");
+      Stub_Gtk_Widget : Gtk.Widget.Gtk_Widget_Record;
+   begin
+      return Gtk.Widget.Gtk_Widget (Get_User_Data (Internal (Get_Object (Button)), Stub_Gtk_Widget));
+   end Get_Plus_Button;
+
+   ---------------
+   -- Get_Popup --
+   ---------------
+
+   function Get_Popup
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Widget.Gtk_Widget
+   is
+      function Internal (Button : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_scale_button_get_popup");
+      Stub_Gtk_Widget : Gtk.Widget.Gtk_Widget_Record;
+   begin
+      return Gtk.Widget.Gtk_Widget (Get_User_Data (Internal (Get_Object (Button)), Stub_Gtk_Widget));
+   end Get_Popup;
+
+   ---------------
+   -- Get_Value --
+   ---------------
+
+   function Get_Value
+      (Button : not null access Gtk_Scale_Button_Record) return Gdouble
+   is
+      function Internal (Button : System.Address) return Gdouble;
+      pragma Import (C, Internal, "gtk_scale_button_get_value");
+   begin
+      return Internal (Get_Object (Button));
+   end Get_Value;
+
+   --------------------
+   -- Set_Adjustment --
+   --------------------
+
+   procedure Set_Adjustment
+      (Button     : not null access Gtk_Scale_Button_Record;
+       Adjustment : not null access Gtk.Adjustment.Gtk_Adjustment_Record'Class)
+      
+   is
+      procedure Internal
+         (Button     : System.Address;
+          Adjustment : System.Address);
+      pragma Import (C, Internal, "gtk_scale_button_set_adjustment");
+   begin
+      Internal (Get_Object (Button), Get_Object (Adjustment));
+   end Set_Adjustment;
+
+   ---------------
+   -- Set_Icons --
+   ---------------
+
+   procedure Set_Icons
+      (Button : not null access Gtk_Scale_Button_Record;
+       Icons  : GNAT.Strings.String_List)
+   is
+      procedure Internal
+         (Button : System.Address;
+          Icons  : Interfaces.C.Strings.chars_ptr_array);
+      pragma Import (C, Internal, "gtk_scale_button_set_icons");
+      Tmp_Icons : Interfaces.C.Strings.chars_ptr_array := From_String_List (Icons);
+   begin
+      Internal (Get_Object (Button), Tmp_Icons);
+      GtkAda.Types.Free (Tmp_Icons);
+   end Set_Icons;
+
+   ---------------
+   -- Set_Value --
+   ---------------
+
+   procedure Set_Value
+      (Button : not null access Gtk_Scale_Button_Record;
+       Value  : Gdouble)
+   is
+      procedure Internal (Button : System.Address; Value : Gdouble);
+      pragma Import (C, Internal, "gtk_scale_button_set_value");
+   begin
+      Internal (Get_Object (Button), Value);
+   end Set_Value;
+
+   ---------------------------
+   -- Do_Set_Related_Action --
+   ---------------------------
+
+   procedure Do_Set_Related_Action
+      (Self   : not null access Gtk_Scale_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_do_set_related_action");
+   begin
+      Internal (Get_Object (Self), Get_Object (Action));
+   end Do_Set_Related_Action;
+
+   ---------------------
+   -- Get_Orientation --
+   ---------------------
+
+   function Get_Orientation
+      (Self : not null access Gtk_Scale_Button_Record)
+       return Gtk.Enums.Gtk_Orientation
+   is
+      function Internal (Self : System.Address) return Integer;
+      pragma Import (C, Internal, "gtk_orientable_get_orientation");
+   begin
+      return Gtk.Enums.Gtk_Orientation'Val (Internal (Get_Object (Self)));
+   end Get_Orientation;
+
+   ------------------------
+   -- Get_Related_Action --
+   ------------------------
+
+   function Get_Related_Action
+      (Self : not null access Gtk_Scale_Button_Record)
+       return Gtk.Action.Gtk_Action
+   is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_activatable_get_related_action");
+      Stub_Gtk_Action : Gtk.Action.Gtk_Action_Record;
+   begin
+      return Gtk.Action.Gtk_Action (Get_User_Data (Internal (Get_Object (Self)), Stub_Gtk_Action));
+   end Get_Related_Action;
+
+   -------------------------------
+   -- Get_Use_Action_Appearance --
+   -------------------------------
+
+   function Get_Use_Action_Appearance
+      (Self : not null access Gtk_Scale_Button_Record) return Boolean
+   is
+      function Internal (Self : System.Address) return Integer;
+      pragma Import (C, Internal, "gtk_activatable_get_use_action_appearance");
+   begin
+      return Boolean'Val (Internal (Get_Object (Self)));
+   end Get_Use_Action_Appearance;
+
+   ---------------------
+   -- Set_Orientation --
+   ---------------------
+
+   procedure Set_Orientation
+      (Self        : not null access Gtk_Scale_Button_Record;
+       Orientation : Gtk.Enums.Gtk_Orientation)
+   is
+      procedure Internal (Self : System.Address; Orientation : Integer);
+      pragma Import (C, Internal, "gtk_orientable_set_orientation");
+   begin
+      Internal (Get_Object (Self), Gtk.Enums.Gtk_Orientation'Pos (Orientation));
+   end Set_Orientation;
+
+   ------------------------
+   -- Set_Related_Action --
+   ------------------------
+
+   procedure Set_Related_Action
+      (Self   : not null access Gtk_Scale_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_set_related_action");
+   begin
+      Internal (Get_Object (Self), Get_Object (Action));
+   end Set_Related_Action;
+
+   -------------------------------
+   -- Set_Use_Action_Appearance --
+   -------------------------------
+
+   procedure Set_Use_Action_Appearance
+      (Self           : not null access Gtk_Scale_Button_Record;
+       Use_Appearance : Boolean)
+   is
+      procedure Internal (Self : System.Address; Use_Appearance : Integer);
+      pragma Import (C, Internal, "gtk_activatable_set_use_action_appearance");
+   begin
+      Internal (Get_Object (Self), Boolean'Pos (Use_Appearance));
+   end Set_Use_Action_Appearance;
+
+   ----------------------------
+   -- Sync_Action_Properties --
+   ----------------------------
+
+   procedure Sync_Action_Properties
+      (Self   : not null access Gtk_Scale_Button_Record;
+       Action : access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_sync_action_properties");
+   begin
+      Internal (Get_Object (Self), Get_Object_Or_Null (GObject (Action)));
+   end Sync_Action_Properties;
+
+end Gtk.Scale_Button;
---
+++ src/generated/gtk-scale_button.ads
@@ -0,0 +1,277 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 2000-2012, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma Ada_05;
+--  <description>
+--  Gtk.Scale_Button.Gtk_Scale_Button provides a button which pops up a scale
+--  widget. This kind of widget is commonly used for volume controls in
+--  multimedia applications, and GTK+ provides a
+--  Gtk.Volume_Button.Gtk_Volume_Button subclass that is tailored for this use
+--  case.
+--
+--  </description>
+
+pragma Warnings (Off, "*is already use-visible*");
+with GNAT.Strings;    use GNAT.Strings;
+with Glib;            use Glib;
+with Glib.Properties; use Glib.Properties;
+with Glib.Types;      use Glib.Types;
+with Gtk.Action;      use Gtk.Action;
+with Gtk.Activatable; use Gtk.Activatable;
+with Gtk.Adjustment;  use Gtk.Adjustment;
+with Gtk.Buildable;   use Gtk.Buildable;
+with Gtk.Button;      use Gtk.Button;
+with Gtk.Enums;       use Gtk.Enums;
+with Gtk.Orientable;  use Gtk.Orientable;
+with Gtk.Widget;      use Gtk.Widget;
+
+package Gtk.Scale_Button is
+
+   type Gtk_Scale_Button_Record is new Gtk_Button_Record with null record;
+   type Gtk_Scale_Button is access all Gtk_Scale_Button_Record'Class;
+
+   ------------------
+   -- Constructors --
+   ------------------
+
+   procedure Gtk_New
+      (Button : out Gtk_Scale_Button;
+       Size   : Gtk.Enums.Gtk_Icon_Size;
+       Min    : Gdouble;
+       Max    : Gdouble;
+       Step   : Gdouble;
+       Icons  : GNAT.Strings.String_List);
+   procedure Initialize
+      (Button : access Gtk_Scale_Button_Record'Class;
+       Size   : Gtk.Enums.Gtk_Icon_Size;
+       Min    : Gdouble;
+       Max    : Gdouble;
+       Step   : Gdouble;
+       Icons  : GNAT.Strings.String_List);
+   --  Creates a Gtk.Scale_Button.Gtk_Scale_Button, with a range between Min
+   --  and Max, with a stepping of Step.
+   --  Since: gtk+ 2.12
+   --  "size": a stock icon size
+   --  "min": the minimum value of the scale (usually 0)
+   --  "max": the maximum value of the scale (usually 100)
+   --  "step": the stepping of value when a scroll-wheel event, or up/down
+   --  arrow event occurs (usually 2)
+   --  "icons": a null-terminated array of icon names, or null if you want to
+   --  set the list later with Gtk.Scale_Button.Set_Icons
+
+   function Get_Type return Glib.GType;
+   pragma Import (C, Get_Type, "gtk_scale_button_get_type");
+
+   -------------
+   -- Methods --
+   -------------
+
+   function Get_Adjustment
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Adjustment.Gtk_Adjustment;
+   procedure Set_Adjustment
+      (Button     : not null access Gtk_Scale_Button_Record;
+       Adjustment : not null access Gtk.Adjustment.Gtk_Adjustment_Record'Class)
+      ;
+   --  Sets the Gtk.Adjustment.Gtk_Adjustment to be used as a model for the
+   --  Gtk.Scale_Button.Gtk_Scale_Button's scale. See Gtk.GRange.Set_Adjustment
+   --  for details.
+   --  Since: gtk+ 2.12
+   --  "adjustment": a Gtk.Adjustment.Gtk_Adjustment
+
+   function Get_Minus_Button
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Widget.Gtk_Widget;
+   --  Retrieves the minus button of the Gtk.Scale_Button.Gtk_Scale_Button.
+   --  Since: gtk+ 2.14
+
+   function Get_Plus_Button
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Widget.Gtk_Widget;
+   --  Retrieves the plus button of the Gtk.Scale_Button.Gtk_Scale_Button.
+   --  Since: gtk+ 2.14
+
+   function Get_Popup
+      (Button : not null access Gtk_Scale_Button_Record)
+       return Gtk.Widget.Gtk_Widget;
+   --  Retrieves the popup of the Gtk.Scale_Button.Gtk_Scale_Button.
+   --  Since: gtk+ 2.14
+
+   function Get_Value
+      (Button : not null access Gtk_Scale_Button_Record) return Gdouble;
+   procedure Set_Value
+      (Button : not null access Gtk_Scale_Button_Record;
+       Value  : Gdouble);
+   --  Sets the current value of the scale; if the value is outside the
+   --  minimum or maximum range values, it will be clamped to fit inside them.
+   --  The scale button emits the
+   --  Gtk.Scale_Button.Gtk_Scale_Button::value-changed signal if the value
+   --  changes.
+   --  Since: gtk+ 2.12
+   --  "value": new value of the scale button
+
+   procedure Set_Icons
+      (Button : not null access Gtk_Scale_Button_Record;
+       Icons  : GNAT.Strings.String_List);
+   --  Sets the icons to be used by the scale button. For details, see the
+   --  Gtk.Scale_Button.Gtk_Scale_Button:icons property.
+   --  Since: gtk+ 2.12
+   --  "icons": a null-terminated array of icon names
+
+   ---------------------------------------------
+   -- Inherited subprograms (from interfaces) --
+   ---------------------------------------------
+   --  Methods inherited from the Buildable interface are not duplicated here
+   --  since they are meant to be used by tools, mostly. If you need to call
+   --  them, use an explicit cast through the "-" operator below.
+
+   procedure Do_Set_Related_Action
+      (Self   : not null access Gtk_Scale_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class);
+
+   function Get_Related_Action
+      (Self : not null access Gtk_Scale_Button_Record)
+       return Gtk.Action.Gtk_Action;
+   procedure Set_Related_Action
+      (Self   : not null access Gtk_Scale_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class);
+
+   function Get_Use_Action_Appearance
+      (Self : not null access Gtk_Scale_Button_Record) return Boolean;
+   procedure Set_Use_Action_Appearance
+      (Self           : not null access Gtk_Scale_Button_Record;
+       Use_Appearance : Boolean);
+
+   procedure Sync_Action_Properties
+      (Self   : not null access Gtk_Scale_Button_Record;
+       Action : access Gtk.Action.Gtk_Action_Record'Class);
+
+   function Get_Orientation
+      (Self : not null access Gtk_Scale_Button_Record)
+       return Gtk.Enums.Gtk_Orientation;
+   procedure Set_Orientation
+      (Self        : not null access Gtk_Scale_Button_Record;
+       Orientation : Gtk.Enums.Gtk_Orientation);
+
+   ----------------
+   -- Interfaces --
+   ----------------
+   --  This class implements several interfaces. See Glib.Types
+   --
+   --  - "Activatable"
+   --
+   --  - "Buildable"
+   --
+   --  - "Orientable"
+
+   package Implements_Activatable is new Glib.Types.Implements
+     (Gtk.Activatable.Gtk_Activatable, Gtk_Scale_Button_Record, Gtk_Scale_Button);
+   function "+"
+     (Widget : access Gtk_Scale_Button_Record'Class)
+   return Gtk.Activatable.Gtk_Activatable
+   renames Implements_Activatable.To_Interface;
+   function "-"
+     (Interf : Gtk.Activatable.Gtk_Activatable)
+   return Gtk_Scale_Button
+   renames Implements_Activatable.To_Object;
+
+   package Implements_Buildable is new Glib.Types.Implements
+     (Gtk.Buildable.Gtk_Buildable, Gtk_Scale_Button_Record, Gtk_Scale_Button);
+   function "+"
+     (Widget : access Gtk_Scale_Button_Record'Class)
+   return Gtk.Buildable.Gtk_Buildable
+   renames Implements_Buildable.To_Interface;
+   function "-"
+     (Interf : Gtk.Buildable.Gtk_Buildable)
+   return Gtk_Scale_Button
+   renames Implements_Buildable.To_Object;
+
+   package Implements_Orientable is new Glib.Types.Implements
+     (Gtk.Orientable.Gtk_Orientable, Gtk_Scale_Button_Record, Gtk_Scale_Button);
+   function "+"
+     (Widget : access Gtk_Scale_Button_Record'Class)
+   return Gtk.Orientable.Gtk_Orientable
+   renames Implements_Orientable.To_Interface;
+   function "-"
+     (Interf : Gtk.Orientable.Gtk_Orientable)
+   return Gtk_Scale_Button
+   renames Implements_Orientable.To_Object;
+
+   ----------------
+   -- Properties --
+   ----------------
+   --  The following properties are defined for this widget. See
+   --  Glib.Properties for more information on properties)
+   --
+   --  Name: Adjustment_Property
+   --  Type: Gtk.Adjustment.Gtk_Adjustment
+   --  Flags: read-write
+   --
+   --  Name: Size_Property
+   --  Type: Gtk.Enums.Gtk_Icon_Size
+   --  Flags: read-write
+   --
+   --  Name: Value_Property
+   --  Type: Gdouble
+   --  Flags: read-write
+
+   Adjustment_Property : constant Glib.Properties.Property_Object;
+   Size_Property : constant Gtk.Enums.Property_Gtk_Icon_Size;
+   Value_Property : constant Glib.Properties.Property_Double;
+
+   -------------
+   -- Signals --
+   -------------
+   --  The following new signals are defined for this widget:
+   --
+   --  "popdown"
+   --     procedure Handler (Self : access Gtk_Scale_Button_Record'Class);
+   --  The ::popdown signal is a <link linkend="keybinding-signals">keybinding
+   --  signal</link> which gets emitted to popdown the scale widget.
+   --  The default binding for this signal is Escape.
+   --
+   --  "popup"
+   --     procedure Handler (Self : access Gtk_Scale_Button_Record'Class);
+   --  The ::popup signal is a <link linkend="keybinding-signals">keybinding
+   --  signal</link> which gets emitted to popup the scale widget.
+   --  The default bindings for this signal are Space, Enter and Return.
+   --
+   --  "value-changed"
+   --     procedure Handler
+   --       (Self  : access Gtk_Scale_Button_Record'Class;
+   --        Value : Gdouble);
+   --    --  "value": the new value
+   --  The ::value-changed signal is emitted when the value field has changed.
+
+   Signal_Popdown : constant Glib.Signal_Name := "popdown";
+   Signal_Popup : constant Glib.Signal_Name := "popup";
+   Signal_Value_Changed : constant Glib.Signal_Name := "value-changed";
+
+private
+   Adjustment_Property : constant Glib.Properties.Property_Object :=
+     Glib.Properties.Build ("adjustment");
+   Size_Property : constant Gtk.Enums.Property_Gtk_Icon_Size :=
+     Gtk.Enums.Build ("size");
+   Value_Property : constant Glib.Properties.Property_Double :=
+     Glib.Properties.Build ("value");
+end Gtk.Scale_Button;
