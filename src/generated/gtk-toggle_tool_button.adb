21d7868d742aa29fc9d08b4b297b83f51a915862
Emmanuel Briot <briot@adacore.com>
2012-03-07 16:42:28 +0000

    Automatic binding for Gtk.Toggle_Tool_Button

---
+++ src/generated/gtk-toggle_tool_button.adb
@@ -0,0 +1,203 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 2000-2012, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma Ada_05;
+pragma Style_Checks (Off);
+pragma Warnings (Off, "*is already use-visible*");
+with Glib.Type_Conversion_Hooks; use Glib.Type_Conversion_Hooks;
+with Interfaces.C.Strings;       use Interfaces.C.Strings;
+
+package body Gtk.Toggle_Tool_Button is
+
+   package Type_Conversion_Gtk_Toggle_Tool_Button is new Glib.Type_Conversion_Hooks.Hook_Registrator
+     (Get_Type'Access, Gtk_Toggle_Tool_Button_Record);
+   pragma Unreferenced (Type_Conversion_Gtk_Toggle_Tool_Button);
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Button : out Gtk_Toggle_Tool_Button) is
+   begin
+      Button := new Gtk_Toggle_Tool_Button_Record;
+      Gtk.Toggle_Tool_Button.Initialize (Button);
+   end Gtk_New;
+
+   ------------------------
+   -- Gtk_New_From_Stock --
+   ------------------------
+
+   procedure Gtk_New_From_Stock
+      (Button   : out Gtk_Toggle_Tool_Button;
+       Stock_Id : UTF8_String)
+   is
+   begin
+      Button := new Gtk_Toggle_Tool_Button_Record;
+      Gtk.Toggle_Tool_Button.Initialize_From_Stock (Button, Stock_Id);
+   end Gtk_New_From_Stock;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize
+      (Button : not null access Gtk_Toggle_Tool_Button_Record'Class)
+   is
+      function Internal return System.Address;
+      pragma Import (C, Internal, "gtk_toggle_tool_button_new");
+   begin
+      Set_Object (Button, Internal);
+   end Initialize;
+
+   ---------------------------
+   -- Initialize_From_Stock --
+   ---------------------------
+
+   procedure Initialize_From_Stock
+      (Button   : not null access Gtk_Toggle_Tool_Button_Record'Class;
+       Stock_Id : UTF8_String)
+   is
+      function Internal
+         (Stock_Id : Interfaces.C.Strings.chars_ptr) return System.Address;
+      pragma Import (C, Internal, "gtk_toggle_tool_button_new_from_stock");
+      Tmp_Stock_Id : Interfaces.C.Strings.chars_ptr := New_String (Stock_Id);
+      Tmp_Return   : System.Address;
+   begin
+      Tmp_Return := Internal (Tmp_Stock_Id);
+      Free (Tmp_Stock_Id);
+      Set_Object (Button, Tmp_Return);
+   end Initialize_From_Stock;
+
+   ----------------
+   -- Get_Active --
+   ----------------
+
+   function Get_Active
+      (Button : not null access Gtk_Toggle_Tool_Button_Record)
+       return Boolean
+   is
+      function Internal (Button : System.Address) return Integer;
+      pragma Import (C, Internal, "gtk_toggle_tool_button_get_active");
+   begin
+      return Boolean'Val (Internal (Get_Object (Button)));
+   end Get_Active;
+
+   ----------------
+   -- Set_Active --
+   ----------------
+
+   procedure Set_Active
+      (Button    : not null access Gtk_Toggle_Tool_Button_Record;
+       Is_Active : Boolean)
+   is
+      procedure Internal (Button : System.Address; Is_Active : Integer);
+      pragma Import (C, Internal, "gtk_toggle_tool_button_set_active");
+   begin
+      Internal (Get_Object (Button), Boolean'Pos (Is_Active));
+   end Set_Active;
+
+   ---------------------------
+   -- Do_Set_Related_Action --
+   ---------------------------
+
+   procedure Do_Set_Related_Action
+      (Self   : not null access Gtk_Toggle_Tool_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_do_set_related_action");
+   begin
+      Internal (Get_Object (Self), Get_Object (Action));
+   end Do_Set_Related_Action;
+
+   ------------------------
+   -- Get_Related_Action --
+   ------------------------
+
+   function Get_Related_Action
+      (Self : not null access Gtk_Toggle_Tool_Button_Record)
+       return Gtk.Action.Gtk_Action
+   is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_activatable_get_related_action");
+      Stub_Gtk_Action : Gtk.Action.Gtk_Action_Record;
+   begin
+      return Gtk.Action.Gtk_Action (Get_User_Data (Internal (Get_Object (Self)), Stub_Gtk_Action));
+   end Get_Related_Action;
+
+   -------------------------------
+   -- Get_Use_Action_Appearance --
+   -------------------------------
+
+   function Get_Use_Action_Appearance
+      (Self : not null access Gtk_Toggle_Tool_Button_Record) return Boolean
+   is
+      function Internal (Self : System.Address) return Integer;
+      pragma Import (C, Internal, "gtk_activatable_get_use_action_appearance");
+   begin
+      return Boolean'Val (Internal (Get_Object (Self)));
+   end Get_Use_Action_Appearance;
+
+   ------------------------
+   -- Set_Related_Action --
+   ------------------------
+
+   procedure Set_Related_Action
+      (Self   : not null access Gtk_Toggle_Tool_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_set_related_action");
+   begin
+      Internal (Get_Object (Self), Get_Object (Action));
+   end Set_Related_Action;
+
+   -------------------------------
+   -- Set_Use_Action_Appearance --
+   -------------------------------
+
+   procedure Set_Use_Action_Appearance
+      (Self           : not null access Gtk_Toggle_Tool_Button_Record;
+       Use_Appearance : Boolean)
+   is
+      procedure Internal (Self : System.Address; Use_Appearance : Integer);
+      pragma Import (C, Internal, "gtk_activatable_set_use_action_appearance");
+   begin
+      Internal (Get_Object (Self), Boolean'Pos (Use_Appearance));
+   end Set_Use_Action_Appearance;
+
+   ----------------------------
+   -- Sync_Action_Properties --
+   ----------------------------
+
+   procedure Sync_Action_Properties
+      (Self   : not null access Gtk_Toggle_Tool_Button_Record;
+       Action : access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_sync_action_properties");
+   begin
+      Internal (Get_Object (Self), Get_Object_Or_Null (GObject (Action)));
+   end Sync_Action_Properties;
+
+end Gtk.Toggle_Tool_Button;
---
+++ src/generated/gtk-toggle_tool_button.ads
@@ -0,0 +1,179 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 2000-2012, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma Ada_05;
+--  <description>
+--  A Gtk.Toggle_Tool_Button.Gtk_Toggle_Tool_Button is a
+--  Gtk.Tool_Item.Gtk_Tool_Item that contains a toggle button.
+--
+--  Use Gtk.Toggle_Tool_Button.Gtk_New to create a new
+--  Gtk.Toggle_Tool_Button.Gtk_Toggle_Tool_Button. Use
+--  Gtk.Toggle_Tool_Button.Gtk_New_From_Stock to create a new
+--  Gtk.Toggle_Tool_Button.Gtk_Toggle_Tool_Button containing a stock item.
+--
+--  </description>
+
+pragma Warnings (Off, "*is already use-visible*");
+with Glib;            use Glib;
+with Glib.Properties; use Glib.Properties;
+with Glib.Types;      use Glib.Types;
+with Gtk.Action;      use Gtk.Action;
+with Gtk.Activatable; use Gtk.Activatable;
+with Gtk.Buildable;   use Gtk.Buildable;
+with Gtk.Tool_Button; use Gtk.Tool_Button;
+
+package Gtk.Toggle_Tool_Button is
+
+   type Gtk_Toggle_Tool_Button_Record is new Gtk_Tool_Button_Record with null record;
+   type Gtk_Toggle_Tool_Button is access all Gtk_Toggle_Tool_Button_Record'Class;
+
+   ------------------
+   -- Constructors --
+   ------------------
+
+   procedure Gtk_New (Button : out Gtk_Toggle_Tool_Button);
+   procedure Initialize
+      (Button : not null access Gtk_Toggle_Tool_Button_Record'Class);
+   --  Returns a new Gtk.Toggle_Tool_Button.Gtk_Toggle_Tool_Button
+   --  Since: gtk+ 2.4
+
+   procedure Gtk_New_From_Stock
+      (Button   : out Gtk_Toggle_Tool_Button;
+       Stock_Id : UTF8_String);
+   procedure Initialize_From_Stock
+      (Button   : not null access Gtk_Toggle_Tool_Button_Record'Class;
+       Stock_Id : UTF8_String);
+   --  Creates a new Gtk.Toggle_Tool_Button.Gtk_Toggle_Tool_Button containing
+   --  the image and text from a stock item. Some stock ids have preprocessor
+   --  macros like GTK_STOCK_OK and GTK_STOCK_APPLY.
+   --  It is an error if Stock_Id is not a name of a stock item.
+   --  Since: gtk+ 2.4
+   --  "stock_id": the name of the stock item
+
+   function Get_Type return Glib.GType;
+   pragma Import (C, Get_Type, "gtk_toggle_tool_button_get_type");
+
+   -------------
+   -- Methods --
+   -------------
+
+   function Get_Active
+      (Button : not null access Gtk_Toggle_Tool_Button_Record)
+       return Boolean;
+   procedure Set_Active
+      (Button    : not null access Gtk_Toggle_Tool_Button_Record;
+       Is_Active : Boolean);
+   --  Sets the status of the toggle tool button. Set to True if you want the
+   --  GtkToggleButton to be 'pressed in', and False to raise it. This action
+   --  causes the toggled signal to be emitted.
+   --  Since: gtk+ 2.4
+   --  "is_active": whether Button should be active
+
+   ---------------------------------------------
+   -- Inherited subprograms (from interfaces) --
+   ---------------------------------------------
+   --  Methods inherited from the Buildable interface are not duplicated here
+   --  since they are meant to be used by tools, mostly. If you need to call
+   --  them, use an explicit cast through the "-" operator below.
+
+   procedure Do_Set_Related_Action
+      (Self   : not null access Gtk_Toggle_Tool_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class);
+
+   function Get_Related_Action
+      (Self : not null access Gtk_Toggle_Tool_Button_Record)
+       return Gtk.Action.Gtk_Action;
+   procedure Set_Related_Action
+      (Self   : not null access Gtk_Toggle_Tool_Button_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class);
+
+   function Get_Use_Action_Appearance
+      (Self : not null access Gtk_Toggle_Tool_Button_Record) return Boolean;
+   procedure Set_Use_Action_Appearance
+      (Self           : not null access Gtk_Toggle_Tool_Button_Record;
+       Use_Appearance : Boolean);
+
+   procedure Sync_Action_Properties
+      (Self   : not null access Gtk_Toggle_Tool_Button_Record;
+       Action : access Gtk.Action.Gtk_Action_Record'Class);
+
+   ----------------
+   -- Interfaces --
+   ----------------
+   --  This class implements several interfaces. See Glib.Types
+   --
+   --  - "Activatable"
+   --
+   --  - "Buildable"
+
+   package Implements_Activatable is new Glib.Types.Implements
+     (Gtk.Activatable.Gtk_Activatable, Gtk_Toggle_Tool_Button_Record, Gtk_Toggle_Tool_Button);
+   function "+"
+     (Widget : access Gtk_Toggle_Tool_Button_Record'Class)
+   return Gtk.Activatable.Gtk_Activatable
+   renames Implements_Activatable.To_Interface;
+   function "-"
+     (Interf : Gtk.Activatable.Gtk_Activatable)
+   return Gtk_Toggle_Tool_Button
+   renames Implements_Activatable.To_Object;
+
+   package Implements_Buildable is new Glib.Types.Implements
+     (Gtk.Buildable.Gtk_Buildable, Gtk_Toggle_Tool_Button_Record, Gtk_Toggle_Tool_Button);
+   function "+"
+     (Widget : access Gtk_Toggle_Tool_Button_Record'Class)
+   return Gtk.Buildable.Gtk_Buildable
+   renames Implements_Buildable.To_Interface;
+   function "-"
+     (Interf : Gtk.Buildable.Gtk_Buildable)
+   return Gtk_Toggle_Tool_Button
+   renames Implements_Buildable.To_Object;
+
+   ----------------
+   -- Properties --
+   ----------------
+   --  The following properties are defined for this widget. See
+   --  Glib.Properties for more information on properties)
+   --
+   --  Name: Active_Property
+   --  Type: Boolean
+   --  Flags: read-write
+   --  If the toggle tool button should be pressed in.
+
+   Active_Property : constant Glib.Properties.Property_Boolean;
+
+   -------------
+   -- Signals --
+   -------------
+   --  The following new signals are defined for this widget:
+   --
+   --  "toggled"
+   --     procedure Handler
+   --       (Self : access Gtk_Toggle_Tool_Button_Record'Class);
+   --  Emitted whenever the toggle tool button changes state.
+
+   Signal_Toggled : constant Glib.Signal_Name := "toggled";
+
+private
+   Active_Property : constant Glib.Properties.Property_Boolean :=
+     Glib.Properties.Build ("active");
+end Gtk.Toggle_Tool_Button;
