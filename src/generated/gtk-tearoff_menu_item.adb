b4655b9a4aefa6b912f76170ba3bee94b875c808
Emmanuel Briot <briot@adacore.com>
2012-03-07 16:42:08 +0000

    Automatic binding for Gtk_Tearoff_Menu_Item

---
+++ src/generated/gtk-tearoff_menu_item.adb
@@ -0,0 +1,142 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 2000-2012, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma Ada_05;
+pragma Style_Checks (Off);
+pragma Warnings (Off, "*is already use-visible*");
+with Glib.Type_Conversion_Hooks; use Glib.Type_Conversion_Hooks;
+
+package body Gtk.Tearoff_Menu_Item is
+
+   package Type_Conversion_Gtk_Tearoff_Menu_Item is new Glib.Type_Conversion_Hooks.Hook_Registrator
+     (Get_Type'Access, Gtk_Tearoff_Menu_Item_Record);
+   pragma Unreferenced (Type_Conversion_Gtk_Tearoff_Menu_Item);
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Menu_Item : out Gtk_Tearoff_Menu_Item) is
+   begin
+      Menu_Item := new Gtk_Tearoff_Menu_Item_Record;
+      Gtk.Tearoff_Menu_Item.Initialize (Menu_Item);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize
+      (Menu_Item : not null access Gtk_Tearoff_Menu_Item_Record'Class)
+   is
+      function Internal return System.Address;
+      pragma Import (C, Internal, "gtk_tearoff_menu_item_new");
+   begin
+      Set_Object (Menu_Item, Internal);
+   end Initialize;
+
+   ---------------------------
+   -- Do_Set_Related_Action --
+   ---------------------------
+
+   procedure Do_Set_Related_Action
+      (Self   : not null access Gtk_Tearoff_Menu_Item_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_do_set_related_action");
+   begin
+      Internal (Get_Object (Self), Get_Object (Action));
+   end Do_Set_Related_Action;
+
+   ------------------------
+   -- Get_Related_Action --
+   ------------------------
+
+   function Get_Related_Action
+      (Self : not null access Gtk_Tearoff_Menu_Item_Record)
+       return Gtk.Action.Gtk_Action
+   is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gtk_activatable_get_related_action");
+      Stub_Gtk_Action : Gtk.Action.Gtk_Action_Record;
+   begin
+      return Gtk.Action.Gtk_Action (Get_User_Data (Internal (Get_Object (Self)), Stub_Gtk_Action));
+   end Get_Related_Action;
+
+   -------------------------------
+   -- Get_Use_Action_Appearance --
+   -------------------------------
+
+   function Get_Use_Action_Appearance
+      (Self : not null access Gtk_Tearoff_Menu_Item_Record) return Boolean
+   is
+      function Internal (Self : System.Address) return Integer;
+      pragma Import (C, Internal, "gtk_activatable_get_use_action_appearance");
+   begin
+      return Boolean'Val (Internal (Get_Object (Self)));
+   end Get_Use_Action_Appearance;
+
+   ------------------------
+   -- Set_Related_Action --
+   ------------------------
+
+   procedure Set_Related_Action
+      (Self   : not null access Gtk_Tearoff_Menu_Item_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_set_related_action");
+   begin
+      Internal (Get_Object (Self), Get_Object (Action));
+   end Set_Related_Action;
+
+   -------------------------------
+   -- Set_Use_Action_Appearance --
+   -------------------------------
+
+   procedure Set_Use_Action_Appearance
+      (Self           : not null access Gtk_Tearoff_Menu_Item_Record;
+       Use_Appearance : Boolean)
+   is
+      procedure Internal (Self : System.Address; Use_Appearance : Integer);
+      pragma Import (C, Internal, "gtk_activatable_set_use_action_appearance");
+   begin
+      Internal (Get_Object (Self), Boolean'Pos (Use_Appearance));
+   end Set_Use_Action_Appearance;
+
+   ----------------------------
+   -- Sync_Action_Properties --
+   ----------------------------
+
+   procedure Sync_Action_Properties
+      (Self   : not null access Gtk_Tearoff_Menu_Item_Record;
+       Action : access Gtk.Action.Gtk_Action_Record'Class)
+   is
+      procedure Internal (Self : System.Address; Action : System.Address);
+      pragma Import (C, Internal, "gtk_activatable_sync_action_properties");
+   begin
+      Internal (Get_Object (Self), Get_Object_Or_Null (GObject (Action)));
+   end Sync_Action_Properties;
+
+end Gtk.Tearoff_Menu_Item;
---
+++ src/generated/gtk-tearoff_menu_item.ads
@@ -0,0 +1,126 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 2000-2012, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma Ada_05;
+--  <description>
+--  A Gtk.Tearoff_Menu_Item.Gtk_Tearoff_Menu_Item is a special
+--  Gtk.Menu_Item.Gtk_Menu_Item which is used to tear off and reattach its
+--  menu.
+--
+--  When its menu is shown normally, the
+--  Gtk.Tearoff_Menu_Item.Gtk_Tearoff_Menu_Item is drawn as a dotted line
+--  indicating that the menu can be torn off. Activating it causes its menu to
+--  be torn off and displayed in its own window as a tearoff menu.
+--
+--  When its menu is shown as a tearoff menu, the
+--  Gtk.Tearoff_Menu_Item.Gtk_Tearoff_Menu_Item is drawn as a dotted line which
+--  has a left pointing arrow graphic indicating that the tearoff menu can be
+--  reattached. Activating it will erase the tearoff menu window.
+--
+--  </description>
+
+pragma Warnings (Off, "*is already use-visible*");
+with Glib;            use Glib;
+with Glib.Types;      use Glib.Types;
+with Gtk.Action;      use Gtk.Action;
+with Gtk.Activatable; use Gtk.Activatable;
+with Gtk.Buildable;   use Gtk.Buildable;
+with Gtk.Menu_Item;   use Gtk.Menu_Item;
+
+package Gtk.Tearoff_Menu_Item is
+
+   type Gtk_Tearoff_Menu_Item_Record is new Gtk_Menu_Item_Record with null record;
+   type Gtk_Tearoff_Menu_Item is access all Gtk_Tearoff_Menu_Item_Record'Class;
+
+   ------------------
+   -- Constructors --
+   ------------------
+
+   procedure Gtk_New (Menu_Item : out Gtk_Tearoff_Menu_Item);
+   procedure Initialize
+      (Menu_Item : not null access Gtk_Tearoff_Menu_Item_Record'Class);
+   --  Creates a new Gtk.Tearoff_Menu_Item.Gtk_Tearoff_Menu_Item.
+
+   function Get_Type return Glib.GType;
+   pragma Import (C, Get_Type, "gtk_tearoff_menu_item_get_type");
+
+   ---------------------------------------------
+   -- Inherited subprograms (from interfaces) --
+   ---------------------------------------------
+   --  Methods inherited from the Buildable interface are not duplicated here
+   --  since they are meant to be used by tools, mostly. If you need to call
+   --  them, use an explicit cast through the "-" operator below.
+
+   procedure Do_Set_Related_Action
+      (Self   : not null access Gtk_Tearoff_Menu_Item_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class);
+
+   function Get_Related_Action
+      (Self : not null access Gtk_Tearoff_Menu_Item_Record)
+       return Gtk.Action.Gtk_Action;
+   procedure Set_Related_Action
+      (Self   : not null access Gtk_Tearoff_Menu_Item_Record;
+       Action : not null access Gtk.Action.Gtk_Action_Record'Class);
+
+   function Get_Use_Action_Appearance
+      (Self : not null access Gtk_Tearoff_Menu_Item_Record) return Boolean;
+   procedure Set_Use_Action_Appearance
+      (Self           : not null access Gtk_Tearoff_Menu_Item_Record;
+       Use_Appearance : Boolean);
+
+   procedure Sync_Action_Properties
+      (Self   : not null access Gtk_Tearoff_Menu_Item_Record;
+       Action : access Gtk.Action.Gtk_Action_Record'Class);
+
+   ----------------
+   -- Interfaces --
+   ----------------
+   --  This class implements several interfaces. See Glib.Types
+   --
+   --  - "Activatable"
+   --
+   --  - "Buildable"
+
+   package Implements_Activatable is new Glib.Types.Implements
+     (Gtk.Activatable.Gtk_Activatable, Gtk_Tearoff_Menu_Item_Record, Gtk_Tearoff_Menu_Item);
+   function "+"
+     (Widget : access Gtk_Tearoff_Menu_Item_Record'Class)
+   return Gtk.Activatable.Gtk_Activatable
+   renames Implements_Activatable.To_Interface;
+   function "-"
+     (Interf : Gtk.Activatable.Gtk_Activatable)
+   return Gtk_Tearoff_Menu_Item
+   renames Implements_Activatable.To_Object;
+
+   package Implements_Buildable is new Glib.Types.Implements
+     (Gtk.Buildable.Gtk_Buildable, Gtk_Tearoff_Menu_Item_Record, Gtk_Tearoff_Menu_Item);
+   function "+"
+     (Widget : access Gtk_Tearoff_Menu_Item_Record'Class)
+   return Gtk.Buildable.Gtk_Buildable
+   renames Implements_Buildable.To_Interface;
+   function "-"
+     (Interf : Gtk.Buildable.Gtk_Buildable)
+   return Gtk_Tearoff_Menu_Item
+   renames Implements_Buildable.To_Object;
+
+end Gtk.Tearoff_Menu_Item;
