056a80df7ce2906b99a03e400c9d3bc74886dc1a
Nicolas Setton <setton@adacore.com>
2012-04-11 22:17:20 +0000

    (Button_Pressed): Do not clear the selection when an item is under the cursor,  so as to allow dragging of multiple boxes. Fixes L411-050.

--- src/gtkada-canvas.adb
+++ src/gtkada-canvas.adb
@@ -2,7 +2,7 @@
 --               GtkAda - Ada95 binding for Gtk+/Gnome               --
 --                                                                   --
 --   Copyright (C) 1998-2000, E. Briot, J. Brobecker and A. Charlet  --
---                Copyright (C) 2000-2011, AdaCore                   --
+--                Copyright (C) 2000-2012, AdaCore                   --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -2823,9 +2823,6 @@ package body Gtkada.Canvas is
                Add_To_Selection (Canvas, Canvas.Item_Press);
             end if;
          else
-
-            Clear_Selection (Canvas);
-
             Set_X
               (Event,
                Canvas.World_X_At_Click - Gdouble (Canvas.Item_Press.Coord.X));
