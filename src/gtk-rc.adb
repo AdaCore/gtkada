d9e01cb8c226c0d4f73066906a406357133d33d8
Emmanuel Briot <briot@adacore.com>
2012-12-05 08:13:00 +0000

    (Get_Theme_Dir): put back this function, since it is needed for GPS  and still has an implementation in gtk+ in fact.

---
+++ src/gtk-rc.adb
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------------
+--                  GtkAda - Ada95 binding for Gtk+/Gnome                   --
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 1998-2012, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Gtkada.Types; use Gtkada.Types;
+with Interfaces.C.Strings;
+with Interfaces.C;  use Interfaces.C;
+
+package body Gtk.Rc is
+
+   -------------------
+   -- Get_Theme_Dir --
+   -------------------
+
+   function Get_Theme_Dir return String is
+      function Internal return Chars_Ptr;
+      pragma Import (C, Internal, "gtk_rc_get_theme_dir");
+
+      S   : constant Chars_Ptr := Internal;
+      Str : constant String := Strings.Value (S);
+
+   begin
+      g_free (S);
+      return Str;
+   end Get_Theme_Dir;
+
+end Gtk.Rc;
