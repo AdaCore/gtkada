3cac38e7e79faac15bfe88a9170f748e4d1ab545
Emmanuel Briot <briot@adacore.com>
2014-01-06 10:21:16 +0000

    Update copyright notice to 2014 for automatically generated files N103-019

--- src/generated/gtk-radio_menu_item.ads
+++ src/generated/gtk-radio_menu_item.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
---                     Copyright (C) 2000-2013, AdaCore                     --
+--                     Copyright (C) 2000-2014, AdaCore                     --
 --                                                                          --
 -- This library is free software;  you can redistribute it and/or modify it --
 -- under terms of the  GNU General Public License  as published by the Free --
