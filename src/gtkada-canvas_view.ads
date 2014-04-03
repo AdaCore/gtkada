6e8cc2d557c85fa01e11533fa1fe9731b2fa9501
Emmanuel Briot <briot@adacore.com>
2014-04-03 12:21:48 +0000

    a Text_Item no longer draws a rectangle, just the text
    Drawing the rectangle means that the default style (with a black stroke)
    is almost never suitable for the text item, and users thus need a custom
    style. When users want to draw the text with special background rectangle,
    they should just put it in a Rect_Item.

--- src/gtkada-canvas_view.ads
+++ src/gtkada-canvas_view.ads
@@ -845,7 +845,8 @@ package Gtkada.Canvas_View is
 
    type Text_Item_Record is new Container_Item_Record with private;
    type Text_Item is access all Text_Item_Record'Class;
-   --  A predefined object that displays itself as text.
+   --  A predefined object that displays itself as text. All style
+   --  properties for the stroke or fill are ignored.
 
    type Text_Arrow_Direction is
      (No_Text_Arrow,
