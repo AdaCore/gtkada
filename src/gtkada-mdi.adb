a15ac9dadbe319321a81af49fed5c023b81fcf9d
Emmanuel Briot <briot@adacore.com>
2014-09-03 11:33:33 +0200

    Prevent reordering tabs when a notebook has only two pages
    Otherwise, when the reorder ends up being a drag to another notebook,
    the display of tabs in the initial notebook is broken with gtk+ 3.8
    
    N408-003
    
    Change-Id: I2eaf2b616e14cea4db239ed805b1a62660b2b168

--- src/gtkada-mdi.adb
+++ src/gtkada-mdi.adb
@@ -4416,7 +4416,12 @@ package body Gtkada.MDI is
 
          Note.Set_Tab_Label (Child, Event);
          Note.Set_Tab_Detachable (Child, False);
-         Note.Set_Tab_Reorderable (Child, Note.Get_N_Pages > 1);
+
+         --  Disable reordering when we have two pages or less. Otherwise, we
+         --  hit a bug in gtk 3.8 where the display of the tabs is broken when
+         --  the reorder operation ends up being a drag of the view to another
+         --  notebook (N408-003)
+         Note.Set_Tab_Reorderable (Child, Note.Get_N_Pages > 2);
 
          Show_All (Event);
 
