3a59c65116e42bf5897c2104878e2951324ede96
Anthony Leonardo Gracio <leonardo@adacore.com>
2017-12-06 18:21:13 +0100

    QC07-015: Add a new Gtkada_Multiline_Entry widget
    This widget has the same style as the Gtk_Entry one by default and
    provides a basic API to get/set its contents.
    
    Change-Id: I378bcc65ff23b7d01be573517f3da9e319748d2c

---
+++ src/gtkada-multiline_entry.adb
@@ -0,0 +1,134 @@
+------------------------------------------------------------------------------
+--                  GtkAda - Ada95 binding for Gtk+/Gnome                   --
+--                                                                          --
+--                     Copyright (C) 2017, AdaCore                          --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Glib.Error;
+
+with Gtk.Css_Provider;    use Gtk.Css_Provider;
+with Gtk.Enums;           use Gtk.Enums;
+with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
+with Gtk.Style_Context;   use Gtk.Style_Context;
+with Gtk.Style_Provider;
+with Gtk.Text_Buffer;     use Gtk.Text_Buffer;
+with Gtk.Text_Iter;       use Gtk.Text_Iter;
+
+package body Gtkada.Multiline_Entry is
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New
+     (Mult_Entry : out Gtkada_Multiline_Entry)
+   is
+   begin
+      Mult_Entry := new Gtkada_Multiline_Entry_Record;
+      Gtkada.Multiline_Entry.Initialize (Mult_Entry);
+   end Gtk_New;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize
+     (Mult_Entry : not null access Gtkada_Multiline_Entry_Record'Class)
+   is
+      Scrolled : Gtk_Scrolled_Window;
+      Provider : Gtk_Css_Provider;
+      Error    : aliased Glib.Error.GError;
+      Success  : Boolean;
+      Css      : constant String := ".entry.multiline {"
+                   & ASCII.LF
+                   & "padding: 8px;}"
+                   & ASCII.LF
+                   & ASCII.LF
+                   & ".entry.multiline GtkTextView" & ASCII.LF
+                   & "{background-color: transparent;}" & ASCII.LF;
+      pragma Unreferenced (Success);
+   begin
+      Gtk.Frame.Initialize (Mult_Entry);
+
+      Gtk_New (Scrolled);
+      Scrolled.Set_Policy (Policy_Never, Policy_Automatic);
+      Mult_Entry.Add (Scrolled);
+
+      Gtk_New (Mult_Entry.Text_View);
+      Scrolled.Add (Mult_Entry.Text_View);
+
+      --  Add the '.entry' class to the multiline entry widget so that it
+      --  inherits the default style of entries.
+
+      Get_Style_Context (Mult_Entry).Add_Class ("entry");
+
+      --  Add a '.multiline' class so that users can distinguish normal entries
+      --  from the multiline ones.
+
+      Get_Style_Context (Mult_Entry).Add_Class ("multiline");
+
+      --  Customize the CSS for multiline entriies so that the Gtk_Text_View's
+      --  background is transparent.
+
+      Gtk_New (Provider);
+
+      Success := Load_From_Data
+        (Provider,
+         Data  => Css,
+         Error => Error'Access);
+
+      Gtk.Style_Context.Add_Provider_For_Screen
+        (Get_Style_Context (Mult_Entry).Get_Screen, +Provider,
+         Priority => Gtk.Style_Provider.Priority_Settings);
+   end Initialize;
+
+   --------------
+   -- Get_Text --
+   --------------
+
+   function Get_Text
+     (Mult_Entry : not null access Gtkada_Multiline_Entry_Record)
+      return UTF8_String
+   is
+      Buffer   : constant Gtk_Text_Buffer := Mult_Entry.Text_View.Get_Buffer;
+      From, To : Gtk_Text_Iter;
+   begin
+      Buffer.Get_Start_Iter (From);
+      Buffer.Get_End_Iter (To);
+
+      return Buffer.Get_Text
+        (Start   => From,
+         The_End => To);
+   end Get_Text;
+
+   --------------
+   -- Set_Text --
+   --------------
+
+   procedure Set_Text
+     (Mult_Entry : not null access Gtkada_Multiline_Entry_Record;
+      Text       : UTF8_String)
+   is
+      Buffer : constant Gtk_Text_Buffer := Mult_Entry.Text_View.Get_Buffer;
+   begin
+      Buffer.Set_Text (Text);
+   end Set_Text;
+
+end Gtkada.Multiline_Entry;
---
+++ src/gtkada-multiline_entry.ads
@@ -0,0 +1,78 @@
+------------------------------------------------------------------------------
+--                  GtkAda - Ada95 binding for Gtk+/Gnome                   --
+--                                                                          --
+--                     Copyright (C) 2017, AdaCore                          --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  <description>
+--
+--  This package provides an implementation of multiline entries using
+--  the Gtk_Text_View and the Gtk_Scrolled_Window widgets.
+--
+--  Its purpose is to provide a simpler API and a style similar to the
+--  Gtk_Entry's one by default.
+--
+--  Gtkada_Multiline_Entry widgets can be referenced with the "entry" CSS main
+--  node: they can also be distinguished from the normal entries using the
+--  "mutiline" CSS node.
+--
+--  Example:
+--
+--  .entry.multiline {background-color: black;}
+--
+--  </description>
+--  <group>Numeric/Text Data Entry</group>
+
+with Glib;                use Glib;
+with Gtk.Frame;           use Gtk.Frame;
+with Gtk.Text_View;       use Gtk.Text_View;
+
+package Gtkada.Multiline_Entry is
+
+   type Gtkada_Multiline_Entry_Record is new Gtk_Frame_Record
+   with private;
+   type Gtkada_Multiline_Entry is
+     access all Gtkada_Multiline_Entry_Record'Class;
+
+   procedure Gtk_New
+     (Mult_Entry : out Gtkada_Multiline_Entry);
+   procedure Initialize
+     (Mult_Entry : not null access Gtkada_Multiline_Entry_Record'Class);
+   --  Creates a new multiline entry, with its associated scrolled window.
+
+   function Get_Text
+     (Mult_Entry : not null access Gtkada_Multiline_Entry_Record)
+      return UTF8_String;
+   --  Return the contents of the multiline entry widget, including newline
+   --  characters if any.
+
+   procedure Set_Text
+     (Mult_Entry : not null access Gtkada_Multiline_Entry_Record;
+      Text       : UTF8_String);
+   --  Set the contents of the multiline entry widget.
+
+private
+
+   type Gtkada_Multiline_Entry_Record is new Gtk_Frame_Record
+     with record
+      Text_View : Gtk_Text_View;
+   end record;
+
+end Gtkada.Multiline_Entry;
