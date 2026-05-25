26f85afe44df7cccd0ddc80224568f650240982e
Andry Ogorodnik <ogorodnik@adacore.com>
2026-05-25 19:03:26 +0300

    Bind GtkSorter
    Implements #63

---
+++ src/generated/gtk-sorter.adb
@@ -0,0 +1,235 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--      Copyright (C) 1998-2000 E. Briot, J. Brobecker and A. Charlet       --
+--                     Copyright (C) 2000-2026, AdaCore                     --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma Style_Checks (Off);
+pragma Warnings (Off, "*is already use-visible*");
+with Ada.Unchecked_Conversion;
+with Glib.Type_Conversion_Hooks; use Glib.Type_Conversion_Hooks;
+with Glib.Values;                use Glib.Values;
+with Gtk.Arguments;              use Gtk.Arguments;
+with Gtkada.Bindings;            use Gtkada.Bindings;
+
+package body Gtk.Sorter is
+
+   function Unchecked_To_Gtk_Sorter_Change is new
+   Glib.Values.Unsafe_Enum_Nth (Gtk_Sorter_Change);
+
+   package Type_Conversion_Gtk_Sorter is new Glib.Type_Conversion_Hooks.Hook_Registrator
+     (Get_Type'Access, Gtk_Sorter_Record);
+   pragma Unreferenced (Type_Conversion_Gtk_Sorter);
+
+   -------------
+   -- Changed --
+   -------------
+
+   procedure Changed
+      (Self   : not null access Gtk_Sorter_Record;
+       Change : Gtk_Sorter_Change)
+   is
+      procedure Internal (Self : System.Address; Change : Gtk_Sorter_Change);
+      pragma Import (C, Internal, "gtk_sorter_changed");
+   begin
+      Internal (Get_Object (Self), Change);
+   end Changed;
+
+   -------------
+   -- Compare --
+   -------------
+
+   function Compare
+      (Self  : not null access Gtk_Sorter_Record;
+       Item1 : not null access Glib.Object.GObject_Record'Class;
+       Item2 : not null access Glib.Object.GObject_Record'Class)
+       return Gtk.Enums.Gtk_Ordering
+   is
+      function Internal
+         (Self  : System.Address;
+          Item1 : System.Address;
+          Item2 : System.Address) return Gtk.Enums.Gtk_Ordering;
+      pragma Import (C, Internal, "gtk_sorter_compare");
+   begin
+      return Internal (Get_Object (Self), Get_Object (Item1), Get_Object (Item2));
+   end Compare;
+
+   ---------------
+   -- Get_Order --
+   ---------------
+
+   function Get_Order
+      (Self : not null access Gtk_Sorter_Record) return Gtk_Sorter_Order
+   is
+      function Internal (Self : System.Address) return Gtk_Sorter_Order;
+      pragma Import (C, Internal, "gtk_sorter_get_order");
+   begin
+      return Internal (Get_Object (Self));
+   end Get_Order;
+
+   function Cb_To_Address is new Ada.Unchecked_Conversion
+     (Cb_Gtk_Sorter_Gtk_Sorter_Change_Void, System.Address);
+   function Address_To_Cb is new Ada.Unchecked_Conversion
+     (System.Address, Cb_Gtk_Sorter_Gtk_Sorter_Change_Void);
+
+   function Cb_To_Address is new Ada.Unchecked_Conversion
+     (Cb_GObject_Gtk_Sorter_Change_Void, System.Address);
+   function Address_To_Cb is new Ada.Unchecked_Conversion
+     (System.Address, Cb_GObject_Gtk_Sorter_Change_Void);
+
+   procedure Connect
+      (Object  : access Gtk_Sorter_Record'Class;
+       C_Name  : Glib.Signal_Name;
+       Handler : Cb_Gtk_Sorter_Gtk_Sorter_Change_Void;
+       After   : Boolean);
+
+   procedure Connect_Slot
+      (Object  : access Gtk_Sorter_Record'Class;
+       C_Name  : Glib.Signal_Name;
+       Handler : Cb_GObject_Gtk_Sorter_Change_Void;
+       After   : Boolean;
+       Slot    : access Glib.Object.GObject_Record'Class := null);
+
+   procedure Marsh_GObject_Gtk_Sorter_Change_Void
+      (Closure         : GClosure;
+       Return_Value    : Glib.Values.GValue;
+       N_Params        : Glib.Guint;
+       Params          : Glib.Values.C_GValues;
+       Invocation_Hint : System.Address;
+       User_Data       : System.Address);
+   pragma Convention (C, Marsh_GObject_Gtk_Sorter_Change_Void);
+
+   procedure Marsh_Gtk_Sorter_Gtk_Sorter_Change_Void
+      (Closure         : GClosure;
+       Return_Value    : Glib.Values.GValue;
+       N_Params        : Glib.Guint;
+       Params          : Glib.Values.C_GValues;
+       Invocation_Hint : System.Address;
+       User_Data       : System.Address);
+   pragma Convention (C, Marsh_Gtk_Sorter_Gtk_Sorter_Change_Void);
+
+   -------------
+   -- Connect --
+   -------------
+
+   procedure Connect
+      (Object  : access Gtk_Sorter_Record'Class;
+       C_Name  : Glib.Signal_Name;
+       Handler : Cb_Gtk_Sorter_Gtk_Sorter_Change_Void;
+       After   : Boolean)
+   is
+   begin
+      Unchecked_Do_Signal_Connect
+        (Object      => Object,
+         C_Name      => C_Name,
+         Marshaller  => Marsh_Gtk_Sorter_Gtk_Sorter_Change_Void'Access,
+         Handler     => Cb_To_Address (Handler),--  Set in the closure
+         After       => After);
+   end Connect;
+
+   ------------------
+   -- Connect_Slot --
+   ------------------
+
+   procedure Connect_Slot
+      (Object  : access Gtk_Sorter_Record'Class;
+       C_Name  : Glib.Signal_Name;
+       Handler : Cb_GObject_Gtk_Sorter_Change_Void;
+       After   : Boolean;
+       Slot    : access Glib.Object.GObject_Record'Class := null)
+   is
+   begin
+      Unchecked_Do_Signal_Connect
+        (Object      => Object,
+         C_Name      => C_Name,
+         Marshaller  => Marsh_GObject_Gtk_Sorter_Change_Void'Access,
+         Handler     => Cb_To_Address (Handler),--  Set in the closure
+         Slot_Object => Slot,
+         After       => After);
+   end Connect_Slot;
+
+   ------------------------------------------
+   -- Marsh_GObject_Gtk_Sorter_Change_Void --
+   ------------------------------------------
+
+   procedure Marsh_GObject_Gtk_Sorter_Change_Void
+      (Closure         : GClosure;
+       Return_Value    : Glib.Values.GValue;
+       N_Params        : Glib.Guint;
+       Params          : Glib.Values.C_GValues;
+       Invocation_Hint : System.Address;
+       User_Data       : System.Address)
+   is
+      pragma Unreferenced (Return_Value, N_Params, Invocation_Hint, User_Data);
+      H   : constant Cb_GObject_Gtk_Sorter_Change_Void := Address_To_Cb (Get_Callback (Closure));
+      Obj : constant Glib.Object.GObject := Glib.Object.Convert (Get_Data (Closure));
+   begin
+      H (Obj, Unchecked_To_Gtk_Sorter_Change (Params, 1));
+      exception when E : others => Process_Exception (E);
+   end Marsh_GObject_Gtk_Sorter_Change_Void;
+
+   ---------------------------------------------
+   -- Marsh_Gtk_Sorter_Gtk_Sorter_Change_Void --
+   ---------------------------------------------
+
+   procedure Marsh_Gtk_Sorter_Gtk_Sorter_Change_Void
+      (Closure         : GClosure;
+       Return_Value    : Glib.Values.GValue;
+       N_Params        : Glib.Guint;
+       Params          : Glib.Values.C_GValues;
+       Invocation_Hint : System.Address;
+       User_Data       : System.Address)
+   is
+      pragma Unreferenced (Return_Value, N_Params, Invocation_Hint, User_Data);
+      H   : constant Cb_Gtk_Sorter_Gtk_Sorter_Change_Void := Address_To_Cb (Get_Callback (Closure));
+      Obj : constant Gtk_Sorter := Gtk_Sorter (Unchecked_To_Object (Params, 0));
+   begin
+      H (Obj, Unchecked_To_Gtk_Sorter_Change (Params, 1));
+      exception when E : others => Process_Exception (E);
+   end Marsh_Gtk_Sorter_Gtk_Sorter_Change_Void;
+
+   ----------------
+   -- On_Changed --
+   ----------------
+
+   procedure On_Changed
+      (Self  : not null access Gtk_Sorter_Record;
+       Call  : Cb_Gtk_Sorter_Gtk_Sorter_Change_Void;
+       After : Boolean := False)
+   is
+   begin
+      Connect (Self, "changed" & ASCII.NUL, Call, After);
+   end On_Changed;
+
+   ----------------
+   -- On_Changed --
+   ----------------
+
+   procedure On_Changed
+      (Self  : not null access Gtk_Sorter_Record;
+       Call  : Cb_GObject_Gtk_Sorter_Change_Void;
+       Slot  : not null access Glib.Object.GObject_Record'Class;
+       After : Boolean := False)
+   is
+   begin
+      Connect_Slot (Self, "changed" & ASCII.NUL, Call, After, Slot);
+   end On_Changed;
+
+end Gtk.Sorter;
