9d23b667c8b89b75af3ed16c7a623f389886dd18
Vadim Godunko <godunko@adacore.com>
2026-06-22 21:21:48 +0400

    Bind GdkEvent API

--- src/gdk-event.adb
+++ src/gdk-event.adb
@@ -21,8 +21,12 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
+with Gdk.Seat;
+with Glib.Object; use Glib.Object;
+
 package body Gdk.Event is
 
+   use type Glib.Gboolean;
    use type System.Address;
 
    function Event_Ref (Event : System.Address) return System.Address;
@@ -65,6 +69,256 @@ package body Gdk.Event is
       end return;
    end From_Object;
 
+   ---------------
+   -- Get_Angle --
+   ---------------
+
+   function Get_Angle
+     (Event_1 : Gdk_Event'Class;
+      Event_2 : Gdk_Event'Class;
+      Angle   : out Glib.Gdouble) return Boolean
+   is
+      function Internal
+        (Event_1 : System.Address;
+         Event_2 : System.Address;
+         Angle   : out Glib.Gdouble) return Glib.Gboolean;
+      pragma Import (C, Internal, "gdk_events_get_angle");
+   begin
+      return Internal (Get_Object (Event_1), Get_Object (Event_2), Angle) /= 0;
+   end Get_Angle;
+
+   ----------------
+   -- Get_Center --
+   ----------------
+
+   function Get_Center
+     (Event_1 : Gdk_Event'Class;
+      Event_2 : Gdk_Event'Class;
+      X       : out Glib.Gdouble;
+      Y       : out Glib.Gdouble) return Boolean
+   is
+      function Internal
+        (Event_1 : System.Address;
+         Event_2 : System.Address;
+         X       : out Glib.Gdouble;
+         Y       : out Glib.Gdouble) return Glib.Gboolean;
+      pragma Import (C, Internal, "gdk_events_get_center");
+   begin
+      return Internal
+        (Get_Object (Event_1), Get_Object (Event_2), X, Y) /= 0;
+   end Get_Center;
+
+   ------------------
+   -- Get_Distance --
+   ------------------
+
+   function Get_Distance
+     (Event_1  : Gdk_Event'Class;
+      Event_2  : Gdk_Event'Class;
+      Distance : out Glib.Gdouble) return Boolean
+   is
+      function Internal
+        (Event_1   : System.Address;
+         Event_2   : System.Address;
+         Distance  : out Glib.Gdouble) return Glib.Gboolean;
+      pragma Import (C, Internal, "gdk_events_get_distance");
+   begin
+      return Internal
+        (Get_Object (Event_1), Get_Object (Event_2), Distance) /= 0;
+   end Get_Distance;
+
+   --------------
+   -- Get_Axes --
+   --------------
+
+   --  function Get_Axes
+   --    (Self   : Gdk_Event'Class;
+   --     Axes   : out System.Address;
+   --     N_Axes : out Glib.Guint) return Boolean
+   --  is
+   --     function Internal
+   --       (Self   : System.Address;
+   --        Axes   : access System.Address;
+   --        N_Axes : access Glib.Guint) return Glib.Gboolean;
+   --     pragma Import (C, Internal, "gdk_event_get_axes");
+   --  begin
+   --     return Internal (Get_Object (Self), Axes'Access, N_Axes'Access) /= 0;
+   --  end Get_Axes;
+
+   --------------
+   -- Get_Axis --
+   --------------
+
+   function Get_Axis
+     (Self     : Gdk_Event'Class;
+      Axis_Use : Glib.Gint;
+      Value    : out Glib.Gdouble) return Boolean
+   is
+      function Internal
+        (Self     : System.Address;
+         Axis_Use : Glib.Gint;
+         Value    : out Glib.Gdouble) return Glib.Gboolean;
+      pragma Import (C, Internal, "gdk_event_get_axis");
+   begin
+      return Internal (Get_Object (Self), Axis_Use, Value) /= 0;
+   end Get_Axis;
+
+   ----------------
+   -- Get_Device --
+   ----------------
+
+   function Get_Device (Self : Gdk_Event'Class) return Gdk.Device.Gdk_Device is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gdk_event_get_device");
+      Stub_Gdk_Device : Gdk.Device.Gdk_Device_Record;
+   begin
+      return Gdk.Device.Gdk_Device
+        (Get_User_Data (Internal (Get_Object (Self)), Stub_Gdk_Device));
+   end Get_Device;
+
+   ---------------------
+   -- Get_Device_Tool --
+   ---------------------
+
+   function Get_Device_Tool
+     (Self : Gdk_Event'Class) return Gdk.Device_Tool.Gdk_Device_Tool
+   is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gdk_event_get_device_tool");
+      Stub_Gdk_Device_Tool : Gdk.Device_Tool.Gdk_Device_Tool_Record;
+   begin
+      return Gdk.Device_Tool.Gdk_Device_Tool
+        (Get_User_Data (Internal (Get_Object (Self)), Stub_Gdk_Device_Tool));
+   end Get_Device_Tool;
+
+   -----------------
+   -- Get_Display --
+   -----------------
+
+   function Get_Display (Self : Gdk_Event'Class) return Gdk.Display.Gdk_Display is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gdk_event_get_display");
+      Stub_Gdk_Display : Gdk.Display.Gdk_Display_Record;
+   begin
+      return Gdk.Display.Gdk_Display
+        (Get_User_Data (Internal (Get_Object (Self)), Stub_Gdk_Display));
+   end Get_Display;
+
+   ------------------------
+   -- Get_Event_Sequence --
+   ------------------------
+
+   --  function Get_Event_Sequence (Self : Gdk_Event'Class) return System.Address is
+   --     function Internal (Self : System.Address) return System.Address;
+   --     pragma Import (C, Internal, "gdk_event_get_event_sequence");
+   --  begin
+   --     return Internal (Get_Object (Self));
+   --  end Get_Event_Sequence;
+
+   --------------------
+   -- Get_Event_Type --
+   --------------------
+
+   function Get_Event_Type (Self : Gdk_Event'Class) return Gdk_Event_Type is
+      function Internal (Self : System.Address) return Gdk_Event_Type;
+      pragma Import (C, Internal, "gdk_event_get_event_type");
+   begin
+      return Internal (Get_Object (Self));
+   end Get_Event_Type;
+
+   -----------------
+   -- Get_History --
+   -----------------
+
+   --  function Get_History
+   --    (Self     : Gdk_Event'Class;
+   --     N_Coords : out Glib.Guint) return System.Address
+   --  is
+   --     function Internal
+   --       (Self     : System.Address;
+   --        N_Coords : access Glib.Guint) return System.Address;
+   --     pragma Import (C, Internal, "gdk_event_get_history");
+   --  begin
+   --     return Internal (Get_Object (Self), N_Coords'Access);
+   --  end Get_History;
+
+   ------------------------
+   -- Get_Modifier_State --
+   ------------------------
+
+   --  function Get_Modifier_State (Self : Gdk_Event'Class) return Glib.Guint is
+   --     function Internal (Self : System.Address) return Glib.Guint;
+   --     pragma Import (C, Internal, "gdk_event_get_modifier_state");
+   --  begin
+   --     return Internal (Get_Object (Self));
+   --  end Get_Modifier_State;
+
+   --------------------------
+   -- Get_Pointer_Emulated --
+   --------------------------
+
+   function Get_Pointer_Emulated (Self : Gdk_Event'Class) return Boolean is
+      function Internal (Self : System.Address) return Glib.Gboolean;
+      pragma Import (C, Internal, "gdk_event_get_pointer_emulated");
+   begin
+      return Internal (Get_Object (Self)) /= 0;
+   end Get_Pointer_Emulated;
+
+   ------------------
+   -- Get_Position --
+   ------------------
+
+   function Get_Position
+     (Self : Gdk_Event'Class;
+      X    : out Glib.Gdouble;
+      Y    : out Glib.Gdouble) return Boolean
+   is
+      function Internal
+        (Self : System.Address;
+         X    : out Glib.Gdouble;
+         Y    : out Glib.Gdouble) return Glib.Gboolean;
+      pragma Import (C, Internal, "gdk_event_get_position");
+   begin
+      return Internal (Get_Object (Self), X, Y) /= 0;
+   end Get_Position;
+
+   --------------
+   -- Get_Seat --
+   --------------
+
+   function Get_Seat (Self : Gdk_Event'Class) return Gdk.Gdk_Seat is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gdk_event_get_seat");
+      Stub_Gdk_Seat : Gdk.Seat.Gdk_Seat_Record;
+   begin
+      return Gdk.Gdk_Seat
+        (Get_User_Data (Internal (Get_Object (Self)), Stub_Gdk_Seat));
+   end Get_Seat;
+
+   -----------------
+   -- Get_Surface --
+   -----------------
+
+   function Get_Surface (Self : Gdk_Event'Class) return Gdk.Surface.Gdk_Surface is
+      function Internal (Self : System.Address) return System.Address;
+      pragma Import (C, Internal, "gdk_event_get_surface");
+      Stub_Gdk_Surface : Gdk.Surface.Gdk_Surface_Record;
+   begin
+      return Gdk.Surface.Gdk_Surface
+        (Get_User_Data (Internal (Get_Object (Self)), Stub_Gdk_Surface));
+   end Get_Surface;
+
+   --------------
+   -- Get_Time --
+   --------------
+
+   function Get_Time (Self : Gdk_Event'Class) return Glib.Guint32 is
+      function Internal (Self : System.Address) return Glib.Guint32;
+      pragma Import (C, Internal, "gdk_event_get_time");
+   begin
+      return Internal (Get_Object (Self));
+   end Get_Time;
+
    ----------------
    -- Get_Object --
    ----------------
@@ -82,6 +336,17 @@ package body Gdk.Event is
    begin
       return Self.Data = System.Null_Address;
    end Is_Null;
+
+   ---------------------------
+   -- Triggers_Context_Menu --
+   ---------------------------
+
+   function Triggers_Context_Menu (Self : Gdk_Event'Class) return Boolean is
+      function Internal (Self : System.Address) return Glib.Gboolean;
+      pragma Import (C, Internal, "gdk_event_triggers_context_menu");
+   begin
+      return Internal (Get_Object (Self)) /= 0;
+   end Triggers_Context_Menu;
 
    ----------------
    -- Set_Object --
--- src/gdk-event.ads
+++ src/gdk-event.ads
@@ -24,6 +24,10 @@
 private with Ada.Finalization;
 with System;
 
+with Gdk.Device;
+with Gdk.Device_Tool;
+with Gdk.Display;
+with Gdk.Surface;
 with Glib;
 
 package Gdk.Event is
@@ -31,15 +35,60 @@ package Gdk.Event is
 	type Gdk_Event is tagged private;
 	--  Opaque reference-counted wrapper around GdkEvent*.
 
+	type Gdk_Event_Type is
+	  (Delete,
+		Motion_Notify,
+		Button_Press,
+		Button_Release,
+		Key_Press,
+		Key_Release,
+		Enter_Notify,
+		Leave_Notify,
+		Focus_Change,
+		Proximity_In,
+		Proximity_Out,
+		Drag_Enter,
+		Drag_Leave,
+		Drag_Motion,
+		Drop_Start,
+		Scroll,
+		Grab_Broken,
+		Touch_Begin,
+		Touch_Update,
+		Touch_End,
+		Touch_Cancel,
+		Touchpad_Swipe,
+		Touchpad_Pinch,
+		Pad_Button_Press,
+		Pad_Button_Release,
+		Pad_Ring,
+		Pad_Strip,
+		Pad_Group_Mode,
+		Touchpad_Hold,
+		Pad_Dial,
+		Event_Last);
+	pragma Convention (C, Gdk_Event_Type);
+	--  Specifies the type of the event.
+
 	function Get_Type return Glib.GType;
 	pragma Import (C, Get_Type, "gdk_event_get_type");
+	--  Returns the GType for GdkEvent.
+	--
+	--  @return the GType for GdkEvent.
 
 	function From_Object (Object : System.Address) return Gdk_Event;
 	--  Create an Ada handle from a raw C pointer.
 	--  Always acquires a reference with gdk_event_ref.
+	--
+	--  @param Object raw GdkEvent* pointer.
+	--  @return a reference-counted Gdk_Event handle.
 
 	function Get_Object (Self : Gdk_Event'Class) return System.Address;
 	pragma Inline (Get_Object);
+	--  Returns the underlying GdkEvent* pointer.
+	--
+	--  @param Self a Gdk_Event wrapper.
+	--  @return the wrapped GdkEvent* pointer.
 
 	procedure Set_Object
 	  (Self   : in out Gdk_Event'Class;
@@ -47,9 +96,222 @@ package Gdk.Event is
 	--  Assign a raw C pointer to Self.
 	--  Releases any previous pointer in Self.
 	--  Always acquires a reference on Object.
+	--
+	--  @param Self destination wrapper that receives the new pointer.
+	--  @param Object raw GdkEvent* pointer to assign.
 
 	function Is_Null (Self : Gdk_Event'Class) return Boolean;
 	pragma Inline (Is_Null);
+	--  Returns True if Self does not wrap a GdkEvent*.
+	--
+	--  @param Self a Gdk_Event wrapper.
+	--  @return True if the wrapped pointer is null.
+
+	function Get_Angle
+	  (Event_1 : Gdk_Event'Class;
+		Event_2 : Gdk_Event'Class;
+		Angle   : out Glib.Gdouble) return Boolean;
+	--  Returns the relative angle from @event1 to @event2.
+	--
+	--  The relative angle is the angle between the X axis and the line
+	--  through both events' positions. The rotation direction for positive
+	--  angles is from the positive X axis towards the positive Y axis.
+	--
+	--  This assumes that both events have X/Y information.
+	--  If not, this function returns %FALSE.
+	--
+	--  @param Event_1 first `GdkEvent`.
+	--  @param Event_2 second `GdkEvent`.
+	--  @param Angle
+	--    return location for the relative angle between both events.
+	--  @return %TRUE if the angle could be calculated.
+
+	function Get_Center
+	  (Event_1 : Gdk_Event'Class;
+		Event_2 : Gdk_Event'Class;
+		X       : out Glib.Gdouble;
+		Y       : out Glib.Gdouble) return Boolean;
+	--  Returns the point halfway between the events' positions.
+	--
+	--  This assumes that both events have X/Y information.
+	--  If not, this function returns %FALSE.
+	--
+	--  @param Event_1 first `GdkEvent`.
+	--  @param Event_2 second `GdkEvent`.
+	--  @param X return location for the X coordinate of the center.
+	--  @param Y return location for the Y coordinate of the center.
+	--  @return %TRUE if the center could be calculated.
+
+	function Get_Distance
+	  (Event_1   : Gdk_Event'Class;
+		Event_2   : Gdk_Event'Class;
+		Distance  : out Glib.Gdouble) return Boolean;
+	--  Returns the distance between the event locations.
+	--
+	--  This assumes that both events have X/Y information.
+	--  If not, this function returns %FALSE.
+	--
+	--  @param Event_1 first `GdkEvent`.
+	--  @param Event_2 second `GdkEvent`.
+	--  @param Distance return location for the distance.
+	--  @return %TRUE if the distance could be calculated.
+
+	--  function Get_Axes
+	--    (Self   : Gdk_Event'Class;
+	--  	Axes   : out System.Address;
+	--  	N_Axes : out Glib.Guint) return Boolean;
+	--  Extracts all axis values from an event.
+	--
+	--  To find out which axes are used, use [method@Gdk.DeviceTool.get_axes]
+	--  on the device tool returned by [method@Gdk.Event.get_device_tool].
+	--
+	--  @param Self a `GdkEvent`.
+	--  @param Axes the array of values for all axes.
+	--  @param N_Axes the length of array.
+	--  @return %TRUE on success, otherwise %FALSE.
+
+	function Get_Axis
+	  (Self     : Gdk_Event'Class;
+		Axis_Use : Glib.Gint;
+		Value    : out Glib.Gdouble) return Boolean;
+	--  Extract the axis value for a particular axis use from
+	--  an event structure.
+	--
+	--  To find out which axes are used, use [method@Gdk.DeviceTool.get_axes]
+	--  on the device tool returned by [method@Gdk.Event.get_device_tool].
+	--
+	--  @param Self a `GdkEvent`.
+	--  @param Axis_Use the axis use to look for.
+	--  @param Value location to store the value found.
+	--  @return %TRUE if the specified axis was found, otherwise %FALSE.
+
+	function Get_Device (Self : Gdk_Event'Class) return Gdk.Device.Gdk_Device;
+	--  Returns the device of an event.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return a `GdkDevice`.
+
+	function Get_Device_Tool
+	  (Self : Gdk_Event'Class) return Gdk.Device_Tool.Gdk_Device_Tool;
+	--  Returns a `GdkDeviceTool` representing the tool that
+	--  caused the event.
+	--
+	--  If the was not generated by a device that supports
+	--  different tools (such as a tablet), this function will
+	--  return %NULL.
+	--
+	--  Note: the `GdkDeviceTool` will be constant during
+	--  the application lifetime, if settings must be stored
+	--  persistently across runs, see [method@Gdk.DeviceTool.get_serial].
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return The current device tool.
+
+	function Get_Display (Self : Gdk_Event'Class) return Gdk.Display.Gdk_Display;
+	--  Retrieves the display associated to the @event.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return a `GdkDisplay`.
+
+	--  function Get_Event_Sequence (Self : Gdk_Event'Class) return System.Address;
+	--  Returns the event sequence to which the event belongs.
+	--
+	--  Related touch events are connected in a sequence. Other
+	--  events typically don't have event sequence information.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return the event sequence that the event belongs to.
+
+	function Get_Event_Type (Self : Gdk_Event'Class) return Gdk_Event_Type;
+	--  Retrieves the type of the event.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return a `GdkEvent`Type.
+
+	--  function Get_History
+	--    (Self     : Gdk_Event'Class;
+	--     N_Coords : out Glib.Guint) return System.Address;
+	--  Retrieves the history of the device that @event is for, as a list of
+	--  time and coordinates.
+	--
+	--  The history includes positions that are not delivered as separate events
+	--  to the application because they occurred in the same frame as @event.
+	--
+	--  Note that only motion and scroll events record history, and motion
+	--  events do it only if one of the mouse buttons is down, or the device
+	--  has a tool.
+	--
+	--  @param Self a motion or scroll event.
+	--  @param N_Coords
+	--    Return location for the length of the returned array.
+	--  @return an array of time and coordinates.
+
+	--  function Get_Modifier_State (Self : Gdk_Event'Class) return Glib.Guint;
+	--  Returns the modifier state field of an event.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return
+	--  the modifier state of @event.
+
+	function Get_Pointer_Emulated (Self : Gdk_Event'Class) return Boolean;
+	--  Returns whether this event is an 'emulated' pointer event.
+	--
+	--  Emulated pointer events typically originate from a touch events.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return %TRUE if this event is emulated.
+
+	function Get_Position
+	  (Self : Gdk_Event'Class;
+		X    : out Glib.Gdouble;
+		Y    : out Glib.Gdouble) return Boolean;
+	--  Extract the event surface relative x/y coordinates from an event.
+	--
+	--  This position is in [surface coordinates](coordinates.html).
+	--
+	--  @param Self a `GdkEvent`.
+	--  @param X location to put event surface x coordinate.
+	--  @param Y location to put event surface y coordinate.
+	--  @return whether the positions were set.
+
+	function Get_Seat (Self : Gdk_Event'Class) return Gdk.Gdk_Seat;
+	--  Returns the seat that originated the event.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return a `GdkSeat`.
+
+	function Get_Surface (Self : Gdk_Event'Class) return Gdk.Surface.Gdk_Surface;
+	--  Extracts the surface associated with an event.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return The `GdkSurface` associated with the event.
+
+	function Get_Time (Self : Gdk_Event'Class) return Glib.Guint32;
+	--  Returns the timestamp of @event.
+	--
+	--  Not all events have timestamps. In that case, this function
+	--  returns %GDK_CURRENT_TIME.
+	--
+	--  @param Self a `GdkEvent`.
+	--  @return
+	--  timestamp field from @event.
+
+	function Triggers_Context_Menu (Self : Gdk_Event'Class) return Boolean;
+	--  Returns whether a `GdkEvent` should trigger a context menu,
+	--  according to platform conventions.
+	--
+	--  The right mouse button typically triggers context menus.
+	--  On macOS, Control+left mouse button also triggers.
+	--
+	--  This function should always be used instead of simply checking for
+	--
+	--  ```c
+	--  event-&gt;button == GDK_BUTTON_SECONDARY
+	--  ```
+	--
+	--  @param Self
+	--  a `GdkEvent`, currently only button events are meaningful values.
+	--  @return %TRUE if the event should trigger a context menu.
 
 private
 
